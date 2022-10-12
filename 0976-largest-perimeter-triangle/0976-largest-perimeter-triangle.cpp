2343ce10eeb5c8d860f5c2aaac167dad032dc676
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-12 16:37:48 +0530

    Time: 46 ms (82.24%), Space: 21.9 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,18 +1,30 @@
-class Solution {
-public:
-    int largestPerimeter(vector<int>& nums) {
-       
-        // property of a triangle---->  sum of its two sides must be greater than the third one
-        
-        
-        sort(nums.begin(),nums.end());
-        
-        int sum=0;
-        for(int i=0;i<nums.size()-2;i++){
-            if(nums[i]+nums[i+1]>nums[i+2]){
-                sum=max(sum,nums[i]+nums[i+1]+nums[i+2]);     
+class Solution
+{
+    public:
+       	//     int largestPerimeter(vector<int>& nums) {
+
+       	//        	// property of a triangle---->  sum of its two sides must be greater than the third one
+
+       	//         sort(nums.begin(),nums.end());
+
+       	//         int sum=0;
+       	//         for(int i=0;i < nums.size()-2;i++){
+       	//             if(nums[i]+nums[i+1]>nums[i+2]){
+       	//                 sum=max(sum,nums[i]+nums[i+1]+nums[i+2]);     
+       	//             }
+       	//         }
+       	//      return sum;
+       	//     }
+       	// };
+
+        int largestPerimeter(vector<int> &nums)
+        {
+            sort(nums.begin(), nums.end());
+            for (int i = nums.size() - 3; i >= 0; i--)
+            {
+                if (nums[i] + nums[i + 1] > nums[i + 2])
+                    return nums[i] + nums[i + 1] + nums[i + 2];
             }
+            return 0;
         }
-    return sum;
-    }
 };
