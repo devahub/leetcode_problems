fb31ef34829bb49ed7912c6009ca73b2e9f5bf5d
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-14 01:11:45 +0530

    Time: 1560 ms (32.82%), Space: 253.1 MB (64.39%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,33 +1,72 @@
+class Solution
+{
+    public:
+    
+    // brute force-----> create a vector and push sum into it
+    
+//         ListNode* mergeNodes(ListNode *head)
+//         {
 
-class Solution {
-public:
-    ListNode* mergeNodes(ListNode* head) {
+//             ListNode *cur = head;
+//             vector<int> v;
+//             int sum = 0;
+
+//             while (cur != NULL)
+//             {
+//                 if (cur->val != 0)
+//                 {
+//                     sum += cur->val;
+//                 }
+//                 else
+//                 {
+//                     if (sum != 0)
+//                     {
+//                         v.push_back(sum);
+//                         sum = 0;
+//                     }
+//                 }
+//                 cur = cur->next;
+//             }
+
+//             cur = head;
+//             for (int i = 0; i < v.size() - 1; i++)
+//             {
+//                 cur->val = v[i];
+//                 cur = cur->next;
+//             }
+
+//             cur->val = v[v.size() - 1];
+//             cur->next = NULL;
+//             return head;
+//         }
+// };
+    
+    
+    
+    
+    
+    ListNode* mergeNodes(ListNode *head){
         
-        ListNode* cur=head;
-        vector<int>v;
+        if(head->val==0)head=head->next;
+        
+        ListNode *res=head;
+        ListNode *cur=head;
         int sum=0;
-        
         while(cur!=NULL){
             if(cur->val!=0){
                 sum+=cur->val;
+                cur=cur->next;
             }
             else{
-                if(sum!=0){
-                    v.push_back(sum);
+                res->val=sum;
+                res->next=cur->next;
+                cur=cur->next;
+                res=cur;
                 sum=0;
+               
             }
-            }
-            cur=cur->next;
         }
+       return head; 
         
-        cur=head;
-        for(int i=0;i<v.size()-1;i++){
-            cur->val=v[i];
-            cur=cur->next;
-        }
-        
-        cur->val = v[v.size() - 1];
-        cur->next = NULL; 
-        return head;
     }
 };
