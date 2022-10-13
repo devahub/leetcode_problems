677d2c07495b6b05285a5bbd73a953c51e0b64e3
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-13 22:16:09 +0530

    Time: 13 ms (8.06%), Space: 7.5 MB (18.69%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,25 +1,57 @@
+class Solution
+{
+    public:
 
-class Solution {
-public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        vector<int>v;
-        
-        ListNode* cur=head;
-        while(cur!=NULL){
-            v.push_back(cur->val);
-            cur=cur->next;
-        }
-        
-        reverse(v.begin()+left-1,v.begin()+right);
-        
-        cur=head;
-        int i=0;
-        while(i<v.size()){
-            cur->val=v[i];
-            i++;
-            cur=cur->next;
+       	// vector approach---->
+
+       	//     ListNode* reverseBetween(ListNode* head, int left, int right) {
+       	//         vector < int>v;
+
+       	//         ListNode* cur=head;
+       	//         while(cur!=NULL){
+       	//             v.push_back(cur->val);
+       	//             cur=cur->next;
+       	//         }
+
+       	//         reverse(v.begin()+left-1,v.begin()+right);
+
+       	//         cur=head;
+       	//         int i=0;
+       	//         while(i < v.size()){
+       	//             cur->val=v[i];
+       	//             i++;
+       	//             cur=cur->next;
+
+       	//         }
+       	//         return head;
+       	//     }
+       	// };
+
+    
+    // dummy pointers approach--->
+    
+        ListNode* reverseBetween(ListNode *head, int left, int right)
+        {
+            ListNode *dummy = new ListNode(0);
+            ListNode * cur;
+            ListNode *pre = dummy;
+            dummy->next=head;
             
+            for (int i = 0; i < left - 1; i++)
+            {
+                pre = pre->next;
+            }
+
+            cur = pre->next;
+
+            for (int i = 0; i < right - left; i++)
+            {
+                ListNode *temp = pre->next;
+                pre->next = cur->next;
+                cur->next = cur->next->next;
+                pre->next->next = temp;
+            }
+
+            return dummy->next;
         }
-        return head;
-    }
 };
