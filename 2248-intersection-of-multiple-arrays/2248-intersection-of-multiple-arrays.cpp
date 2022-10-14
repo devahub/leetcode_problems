b777bce432c7ea67188e3914d07408e71a7a0b2f
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-15 02:44:44 +0530

    Time: 7 ms (99.28%), Space: 12.6 MB (49.31%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    vector<int> intersection(vector<vector<int>>& nums) {
-        map<int,int>mp;
-        int n=nums.size();
-        vector<int>v;
+//     vector<int> intersection(vector<vector<int>>& nums) {
+//         map<int,int>mp;
+//         int n=nums.size();
+//         vector<int>v;
+        
+//         for(auto i:nums){
+//             for(auto x:i){
+//                 mp[x]++;
+//                 if(mp[x]==n){
+//                     v.push_back(x);
+//                 }
+//             }
+//         }
+//         sort(v.begin(),v.end());
+//         return v;
+//     }
+// };
+    
+    vector<int> intersection(vector<vector<int>>& nums){
+        vector<int>v(1001);
+        vector<int>res;
         
         for(auto i:nums){
             for(auto x:i){
-                mp[x]++;
-                if(mp[x]==n){
-                    v.push_back(x);
-                }
+                v[x]++;
             }
         }
-        sort(v.begin(),v.end());
-        return v;
+        for(int i=0;i<v.size();i++){
+            if(v[i]==nums.size()){
+                res.push_back(i);
+            }
+        }
+        
+        return res;
     }
 };
