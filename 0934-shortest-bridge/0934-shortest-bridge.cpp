b2621476a855a40c675d9ff7663ad70bc0fab121
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-15 03:43:32 +0530

    Time: 504 ms (13.56%), Space: 37.3 MB (8.47%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -10,76 +10,76 @@ class Solution
        	// 3rd step : I will travel through all its neighbours and make the neighbour visited if it comes under same island. And while marking them visited, I will add them to queue also.
        	// 4th step : Now I will run BFS on the first island and find the nearest bridge to connect nearest '1'.
 
-        queue<pair<int, int>> q; 
+//         queue<pair<int, int>> q; 
     
-    void dfs(vector<vector<int>>& grid, int i, int j) { 
+//     void dfs(vector<vector<int>>& grid, int i, int j) { 
         
-        if (i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j] == 2 or grid[i][j] == 0)
-            return;
+//         if (i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j] == 2 or grid[i][j] == 0)
+//             return;
         
-        grid[i][j] = 2; 
-        q.push({i,j}); 
+//         grid[i][j] = 2; 
+//         q.push({i,j}); 
         
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
-    }
+//         dfs(grid, i+1, j);
+//         dfs(grid, i-1, j);
+//         dfs(grid, i, j+1);
+//         dfs(grid, i, j-1);
+//     }
     
-    int bfs(vector<vector<int>>& grid){ 
+//     int bfs(vector<vector<int>>& grid){ 
         
-        int d = 0; 
-        int mindist = INT_MAX;
+//         int d = 0; 
+//         int mindist = INT_MAX;
         
-        vector<vector<int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+//         vector<vector<int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
         
-        while (!q.empty()){
-            int n = q.size();
+//         while (!q.empty()){
+//             int n = q.size();
             
-            while(n-- > 0){
-                auto a = q.front();
-                q.pop();
+//             while(n-- > 0){
+//                 auto a = q.front();
+//                 q.pop();
 
-                for (int h=0; h<4; h++) {
-                    int x = dir[h][0] + a.first;
-                    int y = dir[h][1] + a.second;
+//                 for (int h=0; h<4; h++) {
+//                     int x = dir[h][0] + a.first;
+//                     int y = dir[h][1] + a.second;
                     
-                    if (x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y] == 1) { 
-                        mindist = min(mindist, d);
-                    }
+//                     if (x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y] == 1) { 
+//                         mindist = min(mindist, d);
+//                     }
 
-                    else if (x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y] == 0) { 
-                        q.push({x, y});
-                        grid[x][y] = 2;
-                    }
-                }
-            }
-            d++; 
-        }
+//                     else if (x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y] == 0) { 
+//                         q.push({x, y});
+//                         grid[x][y] = 2;
+//                     }
+//                 }
+//             }
+//             d++; 
+//         }
         
-        return mindist; 
-    }
+//         return mindist; 
+//     }
     
-    int shortestBridge(vector<vector<int>>& grid) {
+//     int shortestBridge(vector<vector<int>>& grid) {
         
-        bool flag = false;
+//         bool flag = false;
         
-        for (int i=0; i<grid.size(); i++){
-            for (int j=0; j<grid[0].size(); j++){
-                if (grid[i][j] == 1 and !flag){ 
-                    dfs(grid, i, j); 
-                    q.push({i,j});
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag)
-                break;
-        }
+//         for (int i=0; i<grid.size(); i++){
+//             for (int j=0; j<grid[0].size(); j++){
+//                 if (grid[i][j] == 1 and !flag){ 
+//                     dfs(grid, i, j); 
+//                     q.push({i,j});
+//                     flag = true;
+//                     break;
+//                 }
+//             }
+//             if (flag)
+//                 break;
+//         }
         
-        return bfs(grid); 
-    }
-};
+//         return bfs(grid); 
+//     }
+// };
 
 
 // approach----2>>>>>
@@ -90,47 +90,47 @@ class Solution
 
 // class Solution {
 // public:
-//     void dfs(vector<vector<int>> &g,set<pair<int,int>> &is,int i,int j)
-//     {
+    void dfs(vector<vector<int>> &g,set<pair<int,int>> &is,int i,int j)
+    {
          
-//         if(i<0||j<0||i>=g.size()||j>=g[0].size()||g[i][j]!=1||is.count({i,j}))
-//             return;
+        if(i<0||j<0||i>=g.size()||j>=g[0].size()||g[i][j]!=1||is.count({i,j}))
+            return;
         
-//         is.insert({i,j});
-//         g[i][j]=2;
-//         dfs(g,is,i+1,j);
-//         dfs(g,is,i-1,j);
-//         dfs(g,is,i,j+1);
-//         dfs(g,is,i,j-1);
-//     }
+        is.insert({i,j});
+        g[i][j]=2;
+        dfs(g,is,i+1,j);
+        dfs(g,is,i-1,j);
+        dfs(g,is,i,j+1);
+        dfs(g,is,i,j-1);
+    }
     
-//     int shortestBridge(vector<vector<int>>& grid) {
-//         set<pair<int,int>> is1,is2;
+    int shortestBridge(vector<vector<int>>& grid) {
+        set<pair<int,int>> is1,is2;
         
-//         int n=grid.size();
-//         vector<vector<bool>> vis(n,vector<bool>(n,false));
-//         int f=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             for(int j=0;j<n;j++)
-//             {  
-//                 if(grid[i][j]==1&&!vis[i][j])
-//                 {
-//                     if(f==0)
-//                     {   
-//                         dfs(grid,is1,i,j);
-//                         f++;
-//                     }
-//                     else
-//                         dfs(grid,is2,i,j);
-//                  }
-//              }
-//         }
-//         int ans=INT_MAX;
+        int n=grid.size();
+        vector<vector<bool>> vis(n,vector<bool>(n,false));
+        int f=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {  
+                if(grid[i][j]==1&&!vis[i][j])
+                {
+                    if(f==0)
+                    {   
+                        dfs(grid,is1,i,j);
+                        f++;
+                    }
+                    else
+                        dfs(grid,is2,i,j);
+                 }
+             }
+        }
+        int ans=INT_MAX;
      
-//         for(auto i :is1)
-//         for(auto j :is2) 
-//             ans=min(ans,(abs(i.first - j.first) + abs(i.second - j.second)-1));
-//         return ans;
-//     }
-// };
+        for(auto i :is1)
+        for(auto j :is2) 
+            ans=min(ans,(abs(i.first - j.first) + abs(i.second - j.second)-1));
+        return ans;
+    }
+};
