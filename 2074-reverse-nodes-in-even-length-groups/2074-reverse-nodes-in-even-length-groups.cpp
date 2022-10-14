1d3bc0aa7f491fbf7288b4beedcba9d09d4dfd26
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-14 18:44:20 +0530

    Time: 2472 ms (12.68%), Space: 329.6 MB (95.53%) - LeetHub

---
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.cpp
@@ -0,0 +1,56 @@
+class Solution
+{
+    public:
+
+        ListNode* reverse(ListNode *head)
+        {
+            if (!head)
+                return head;
+            ListNode *prev = NULL;
+
+            while (head != NULL)
+            {
+                ListNode *temp = head->next;
+               
+                head->next = prev;
+                prev = head;
+                head = temp;
+            }
+            return prev;
+        }
+
+    ListNode* reverseEvenLengthGroups(ListNode *head) {
+        ListNode* dummy = new ListNode();
+        dummy -> next = head;
+        
+        ListNode* prev=dummy;
+        
+        for(int len=1;len<1e5 && head;len++){
+            ListNode* tail=head;
+            ListNode* nexthead;
+            
+            int j=1;
+            while(j<len && tail!=NULL && tail->next!=NULL)
+            {
+                tail=tail->next;
+                j++;
+            }
+            nexthead=tail->next;
+            
+            
+            if(j%2==0){
+                tail->next=NULL;
+                prev->next=reverse(head);
+                prev=head;
+                head->next=nexthead;
+                head=nexthead;
+             }
+            else{
+                prev=tail;
+                head=nexthead;
+            }
+        }
+        
+    return dummy->next;  
+    }
+};
