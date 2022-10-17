862e6baea8d249e82cb8bc8ecdc28d3e99b119c5
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-17 18:02:34 +0530

    Time: 4 ms (65.37%), Space: 6.6 MB (31.31%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
-    bool checkIfPangram(string sentence) {
-        unordered_map<char,bool>mp;
-        for(auto i:sentence){
-            mp[i]=1;
-        }
-        return mp.size()==26;
-    }
+//     bool checkIfPangram(string sentence) {
+//         unordered_map<char,bool>mp;
+//         for(auto i:sentence){
+//             mp[i]=1;
+//         }
+//         return mp.size()==26;
+//     }
+// };
+    
+    
+    
+     bool checkIfPangram(string sentence){
+         unordered_map<char,int>mp;
+         for(auto i:sentence){
+             mp[i]++;
+         }
+         if(mp.size()==26)return true;
+         return false;
+     }
 };
