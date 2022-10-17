0d6053eb7adb08e830c9b1759556b2835ea8945e
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-18 00:54:59 +0530

    Time: 284 ms (39.79%), Space: 61.1 MB (91.58%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,19 +1,49 @@
 class Solution
 {
     public:
-        int findDuplicate(vector<int> &nums)
-        {
-            // int d = 0;
-            unordered_map<int, int> mp;
-            for (auto i: nums) mp[i]++;
+//         int findDuplicate(vector<int> &nums)
+//         {
+           
+//             unordered_map<int, int> mp;
+//             for (auto i: nums) mp[i]++;
 
-            for (auto i: mp)
-            {
-                if (i.second > 1)
-                {
-                    return i.first;
-                }
-            }
-            return -1;
+//             for (auto i: mp)
+//             {
+//                 if (i.second > 1)
+//                 {
+//                     return i.first;
+//                 }
+//             }
+//             return -1;
+//         }
+// };
+
+
+// without extra space and modifying the array---> take no at first index and then move to that index and and repeat this in test case 1-->
+// 1->3->2->4->2 here we found a cycle so now we will user slow and fast pointer method to find that point 2
+
+
+        int findDuplicate(vector<int> &nums){
+             int slow=nums[0];
+        int fast=nums[0];
+            
+        do
+        {
+           slow=nums[slow];
+           fast=nums[nums[fast]];
+         }
+        while(slow!=fast);
+            
+         fast=nums[0];
+        while(slow!=fast)
+        {
+            fast=nums[fast];
+            slow=nums[slow];
         }
+        return slow;
+            
+            
+            
+        }
+
 };
