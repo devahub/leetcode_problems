770db318ab64e9f9c76a4a1c629f36de712591db
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-17 17:47:31 +0530

    Time: 7 ms (84.94%), Space: 12.7 MB (77.18%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,74 +1,58 @@
 class Solution
 {
     public:
-//         void flatten(TreeNode *root)
-//         {
-//             TreeNode *ans = solve(root);
+
+//     TreeNode* solve(TreeNode* root){
+//         if(root==NULL)return NULL;
+        
+//         if(root->left==NULL && root->right==NULL)return root;
+        
+//         if(root->left!=NULL && root->right==NULL){
+//             TreeNode* templeft=root->left;
+//             root->right=templeft;
+//             root->left=NULL;
+//             return solve(templeft);
 //         }
-
-//     TreeNode* solve(TreeNode *root)
-//     {
-
-//         if (root == NULL) return NULL;
-
-//         if (root->left == NULL and root->right == NULL)
-//             return root;
-
-//         if (root->left != NULL and root->right == NULL)
-//         {
-//             TreeNode *tempLeft = root->left;
-//             root->right = tempLeft;
-//             root->left = NULL;
-//             return solve(tempLeft);
+//          if (root->left == NULL and root->right != NULL)
+//          {
+//              return solve(root->right);
+//          }
+//             else
+//             {
+//             TreeNode* templeft=root->left;
+//             TreeNode* tempright=root->right;
+//             root->right=templeft;
+//             root->left=NULL;
+//             TreeNode* retleft=solve(templeft);
+//             retleft->right=tempright;
+//             return solve(tempright);
 //         }
-
-//         if (root->left == NULL and root->right != NULL)
-//         {
-//             return solve(root->right);
-//         }
-//         else
-//         {
-//             TreeNode *tempRight = root->right;
-//             TreeNode *tempLeft = root->left;
-//             root->right = tempLeft;
-//             root->left = NULL;
-//             TreeNode *retLeft = solve(tempLeft);
-//             retLeft->right = tempRight;
-//             return solve(tempRight);
-//         }
+        
 //     }
+    
+    
+//      void flatten(TreeNode *root){
+//          TreeNode* ans= solve(root);
+//      }
 // };
     
-    TreeNode* solve(TreeNode* root){
-        if(root==NULL)return NULL;
-        
-        if(root->left==NULL && root->right==NULL)return root;
-        
-        if(root->left!=NULL && root->right==NULL){
-            TreeNode* templeft=root->left;
-            root->right=templeft;
-            root->left=NULL;
-            return solve(templeft);
-        }
-         if (root->left == NULL and root->right != NULL)
-         {
-             return solve(root->right);
-         }
-            else
-            {
-            TreeNode* templeft=root->left;
-            TreeNode* tempright=root->right;
-            root->right=templeft;
-            root->left=NULL;
-            TreeNode* retleft=solve(templeft);
-            retleft->right=tempright;
-            return solve(tempright);
+    // morris traversal
+    void flatten(TreeNode *root){
+       TreeNode* curr = root;
+        while (curr!=NULL) {
+            if (curr->left!=NULL) {
+                TreeNode* prev = curr->left;
+                while (prev->right != NULL) 
+                    prev=prev->right;
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
         }
         
     }
+};   
     
     
-     void flatten(TreeNode *root){
-         TreeNode* ans= solve(root);
-     }
-};
+    
