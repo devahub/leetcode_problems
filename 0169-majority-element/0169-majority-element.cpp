e28beb15f143f0a463caefa74a036583722c9055
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-18 23:45:55 +0530

    Time: 45 ms (35.11%), Space: 19.6 MB (14.79%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+//     int majorityElement(vector<int>& nums) {
+//      unordered_map<int,int>mp;
+        
+//         for(auto &i:nums){
+//             mp[i]++;
+//         }
+//         int maxi=INT_MIN;
+//         for(auto i: mp){
+//             if(i.second>mp[maxi])
+//                 maxi=i.first;
+            
+//         }
+//         return maxi;
+//     }
+// };
+    
+    // moore's voting algo
+    
+    int majorityElement(vector<int>& nums) {
+        int ele=0,count=0,n=nums.size();
+        
+        for(int i=0;i<n;i++){
+            if(count==0){
+                ele=nums[i];
+            }
+        if(nums[i]==ele)
+            count++;
+        else count--;
+    } 
+    return ele;
+        
+        
+        
+    }
+};
