87ab3390bb8f834edbd6ac53b2e118f6254ee1f6
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-18 10:11:57 +0530

    Time: 19 ms (43.50%), Space: 6.6 MB (77.40%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,30 +1,59 @@
 class Solution
 {
     public:
-      
+
+       	//         string countAndSay(int n)
+       	//         {
+       	//             string ans = "1";
+       	//             string c;
+
+       	//             while (n != 1)
+       	//             {
+       	//                 c = "";
+       	//                 for (int i = 0; i < ans.size(); i++)
+       	//                 {
+       	//                     int t = 1;
+       	//                     while (i < ans.size() && ans[i] == ans[i + 1])
+       	//                     {
+       	//                         t++;
+       	//                         i++;
+       	//                     }
+       	//                     c = c + to_string(t) + ans[i];
+       	//                 }
+
+       	//                 n--;
+       	//                 ans = c;
+       	//             }
+
+       	//             return ans;
+       	//         }
+       	// };
+
+       	// recurssive approach----->
+
         string countAndSay(int n)
         {
-            string ans = "1";
-            string c;
-
-            while (n != 1)
+            if (n == 1) return "1";
+            string res = "";
+            string s = countAndSay(n - 1);
+            char ch = s[0];
+            int c = 1;
+            for (int i = 1; i < s.size(); i++)
             {
-                c = "";
-                for (int i = 0; i < ans.size(); i++)
+                if (s[i] == ch)
+                    c++;
+                else
                 {
-                    int t = 1;
-                    while (i < ans.size() && ans[i] == ans[i + 1])
-                    {
-                        t++;
-                        i++;
-                    }
-                    c = c + to_string(t) + ans[i];
+                    res += to_string(c);
+                    res += ch;
+                    ch = s[i];
+                    c = 1;
                 }
-
-                n--;
-                ans = c;
             }
 
-            return ans;
+            res += to_string(c);
+            res += ch;
+
+            return res;
         }
 };
