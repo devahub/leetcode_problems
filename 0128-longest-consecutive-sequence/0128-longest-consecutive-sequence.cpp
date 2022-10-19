3e65560413f86118b4800ac0dc0cfdbda60dc8a8
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-20 00:04:09 +0530

    Time: 694 ms (36.05%), Space: 49.7 MB (64.32%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,33 +1,89 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
-        int n=nums.size();
-        if(n==0)
-            return 0;
+    
+    // approach is to put every element in to set and then run a loop and check if ele-1 is present in set if present then continue ans start from a smallest value and count all elements consecutive greater than no.------>striver method
+//     int longestConsecutive(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==0)
+//             return 0;
         
-        unordered_set<int>s;
-        int maxi=INT_MIN;
-        int c=0;
+//         unordered_set<int>s;
+//         int maxi=INT_MIN;
+//         int c=0;
         
-        for(auto i:nums){
-            s.insert(i);
-        }
+//         for(auto i:nums){
+//             s.insert(i);
+//         }
+        
+//         for(int i=0;i<nums.size();i++){
+//             int ele=nums[i];
+//             c=1;
+//             if(s.find(nums[i]-1)!=s.end()){
+//                 continue;
+//             }
+//             while(s.find(ele+1)!=s.end()){
+//                 c+=1;
+//                 ele+=1;
+//             }
+//             maxi=max(maxi,c);
+//             if(maxi==n)
+//                 break;
+//         }
+//         return maxi;
+        
+//     }
+// };
+
+// // brut force will be--->first sort the array and then start from a number and check if next element is coonsecutive or not if yes increase by 1 
+
+
+// // int longestConsecutive(vector<int>&arr) {
+// // if(arr.empty()) return 0;
+// // sort(arr.begin(),arr.end());
+// // int n=arr.size();
+// // int ans=1,prev=arr[0],curr=1;
+// // for(int i=1;i<n;i++){
+// //     if(arr[i]==prev+1){
+// //         curr++;
+// //     }else if(arr[i]!=prev){
+// //         curr=1;
+// //     }
+// //     prev=arr[i];
+// //     ans=max(ans,curr);
+// // }
+// // return ans;
+// // }
+
+
+// map ----> a very interesting approach
+
+        
+    int longestConsecutive(vector<int>& nums) {    
+         unordered_map<int,int>mp;
         
         for(int i=0;i<nums.size();i++){
-            int ele=nums[i];
-            c=1;
-            if(s.find(nums[i]-1)!=s.end()){
-                continue;
+            mp[nums[i]]=1;
+        }     
+        
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i]-1)!=mp.end()){
+                mp[nums[i]]=0;
             }
-            while(s.find(ele+1)!=s.end()){
-                c+=1;
-                ele+=1;
-            }
-            maxi=max(maxi,c);
-            if(maxi==n)
-                break;
         }
-        return maxi;
+            
+        int maxi=0;
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]]==1){
+                int l=1;
+                while(mp.find(nums[i]+l)!=mp.end()){
+                    l++;
+                }
+                maxi=max(maxi,l);
+            }
+        } 
+        
+    return maxi;
+         
         
     }
 };
