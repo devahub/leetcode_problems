ec7a1b47f979ef973b581385bebdea56f5a1d59e
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-23 01:17:32 +0530

    Time: 22 ms (5.99%), Space: 8.4 MB (40.64%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+    public:
+       	// brute force---->vector
+        ListNode* reverseList(ListNode *head)
+        {
+            vector<int> v;
+            ListNode *cur = head;
+            while (cur != NULL)
+            {
+                v.push_back(cur->val);
+                cur = cur->next;
+            }
+            reverse(v.begin(), v.end());
+            cur = head;
+            for (int i =0;i<v.size(); i++)
+            {
+                cur->val = v[i];
+                cur = cur->next;
+            }
+            return head;
+        }
+};
+
+// iterative method----->
+
+//     ListNode* reverseList(ListNode* head) {
+
+//         ListNode* pre=NULL;
+//         ListNode* next=NULL;
+
+//         while(head!=NULL){
+//             next=head->next;
+//             head->next=pre;
+//             pre=head;
+//             head=next;
+//         }
+//         return pre;
+//     }
+// };
+
+// recurssive method------->
+
+//   ListNode* reverseList(ListNode* head){
+
+//       if(head==NULL || head->next==NULL){          	// base case
+//           return head;
+//       }
+
+//       ListNode* newhead=reverseList(head->next);
+
+//       head->next->next=head;
+//       head->next=NULL;
+
+//       return newhead;
+
+//   }
+// };
