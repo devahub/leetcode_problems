af95659d873e456ef9c4a92b0a3c617bcaf6d3be
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-23 23:27:48 +0530

    Time: 11 ms (32.66%), Space: 10.7 MB (32.13%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,84 @@
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+    
+    //  first we are calculating length of linked-list and then again traverse till len-n 
+      
+        
+//         ListNode* temp=head;
+//         int l=0;
+//         int i=1;
+        
+//         while(temp!=NULL){
+//             l++;
+//             temp=temp->next;
+//         }
+        
+//         if(l==n)
+//             return head->next;
+        
+//         for(temp=head;i<l-n;i++){
+//             temp=temp->next;
+//         }
+        
+//         temp->next=temp->next->next;
+        
+//         return head;
+        
+        
+        
+        // approach ---------------------> 2  ---->slow & fast pointer
+        // first we move fast n steps and then we move slow and fast simultaneously by 1 step
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+        
+        while(n--){
+            fast=fast->next;
+        }
+        if(fast==NULL)return head->next;
+        
+        while(fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next;
+            
+        }
+        slow->next=slow->next->next;
+        
+        return head;
+        
+        // approach-----------> 3--- brute force --->vector
+        
+//         vector<int>nums;
+//         vector<int>ans;
+        
+//         while(head!=NULL)
+//         {
+//             nums.push_back(head->val);
+//             head=head->next;
+//         }        
+        
+//         int a =nums.size();
+//         int b = a-n;
+//         for(int i=0;i<b;i++){
+//             ans.push_back(nums[i]);
+            
+//         }
+//         for(int i=b+1; i<nums.size();i++){
+//             ans.push_back(nums[i]);
+//         }
+
+//         ListNode* sol=new ListNode(-1);
+//         ListNode* temp=sol;
+        
+//         for(auto i:ans){
+//             ListNode* cur=new ListNode(i);
+//             temp->next=cur;
+//             temp=cur;
+//         }
+        
+//         return sol->next;     
+        
+    }
+};
