06b031a04b65f538824d9ae986ed89a49cd55a13
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-26 00:59:32 +0530

    Time: 18 ms (21.83%), Space: 13.8 MB (99.84%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,50 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target)return true;
-        }
+    
+    // brute force--->
+//     bool search(vector<int>& nums, int target) {
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==target)return true;
+//         }
+//         return false;
+//     }
+// };
+    
+    
+    // using binary search----->
+    
+     bool search(vector<int>& nums, int target) {
+          int l=0;
+        int h=nums.size()-1;
+            
+         while(l<=h){
+                int mid=l+(h-l)/2;
+                if(target==nums[mid]){
+                    return true;
+                }
+              if((nums[l] == nums[mid]) && (nums[h] == nums[mid]))
+                    {
+                l++;
+                h--;
+                    }
+             
+                else if(nums[mid]>=nums[l]){
+                    if(nums[l]<=target && target<nums[mid])
+                        h=mid-1;
+                    else
+                        l=mid+1;
+                }
+                else {
+                    if(nums[mid]<target && target<=nums[h])
+                        l=mid+1;
+                    else
+                        h=mid-1;
+                }   
+         }
+        
         return false;
-    }
+        
+         
+         
+     }
 };
