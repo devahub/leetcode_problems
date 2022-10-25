25e2015b6431914bc4eb56488b3da4ebf8733337
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-25 20:23:42 +0530

    Time: 110 ms (37.65%), Space: 14.6 MB (32.70%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,61 +1,62 @@
-class Solution {
-public:
-    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
+class Solution
+{
+    public:
+        ListNode* getIntersectionNode(ListNode *heada, ListNode *headb)
+        {
 
-        
-        //      brut force-------> 
-        
-        
-//        ListNode* temp;
-        
-//         while(heada!=NULL){
-//             temp=headb;
-//             while(temp!=NULL){
-//                 if(heada==temp){
-//                     return heada;
-//                 }
-//                 temp=temp->next;
-//             }
-//             heada=heada->next;
-//         }
-//         return NULL;
-        
-       // approach--->2
-//         ListNode* d1=heada;
-//         ListNode* d2=headb;
-        
-//        while(d2!=d1){
-//            if(d1==NULL){
-//                d1=headb;
-//            }
-//            else{
-//                d1=d1->next;
-//            }
-//             if(d2==NULL){
-//                d2=heada;
-//            }
-//            else{
-//                d2=d2->next;
-//            }
-           
-//        }
-//         return d1;
-     
-        
-      // using map ---->
-        
-        unordered_map<ListNode*,int>mp;
-        while(head1!=NULL){
-            mp[head1]++;
-            head1=head1->next;
+           	//      brut force-------> n2 approach
+
+           	//        ListNode* temp;
+
+           	//         while(heada!=NULL){
+           	//             temp=headb;
+           	//             while(temp!=NULL){
+           	//                 if(heada==temp){
+           	//                     return heada;
+           	//                 }
+           	//                 temp=temp->next;
+           	//             }
+           	//             heada=heada->next;
+           	//         }
+           	//         return NULL;
+
+           	// approach--->2
+           	        ListNode* slow=heada;
+           	        ListNode* fast=headb;
+
+           	       while(fast!=slow){
+           	           if(slow==NULL){
+           	               slow=headb;
+           	           }
+           	           else{
+           	              slow=slow->next;
+           	           }
+           	            if(fast==NULL){
+           	               fast=heada;
+           	           }
+           	           else{
+           	               fast=fast->next;
+           	           }
+
+           	       }
+           	        return slow;
+
+           	// using map ---->
+
+            // unordered_map<ListNode*, int> mp;
+            // while (head1 != NULL)
+            // {
+            //     mp[head1]++;
+            //     head1 = head1->next;
+            // }
+            // while (head2 != NULL)
+            // {
+            //     if (mp[head2] > 0)
+            //     {
+            //         return head2;
+            //     }
+            //     head2 = head2->next;
+            // }
+            // return NULL;
         }
-      while(head2!=NULL){
-          if(mp[head2]>0){
-              return head2;
-              
-          }
-          head2=head2->next;
-      }
-        return NULL;
-    }
 };
