457d381716a0658622e7138f081c44dfca88f08c
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-25 23:57:43 +0530

    Time: 20 ms (9.00%), Space: 10.9 MB (76.29%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,16 +1,44 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-       if(nums.size()<=2){
-            return nums.size();
-        }
-        
-       for(int i=0;i<nums.size()-2;i++){
-           if(nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
-               nums.erase(nums.begin()+i);
-               i--;
-           }
-       }
-        return nums.size();
-    }
+class Solution
+{
+    public:
+
+       	// using erase function------->
+       	//     int removeDuplicates(vector<int>& nums) {
+       	//        if(nums.size()<=2){
+       	//             return nums.size();
+       	//         }
+
+       	//        for(int i=0;i < nums.size()-2;i++){
+       	//            if(nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
+       	//                nums.erase(nums.begin()+i);
+       	//                i--;
+       	//            }
+       	//        }
+       	//         return nums.size();
+
+        int removeDuplicates(vector<int> &nums)
+        {
+
+            if (nums.size() <= 2) return nums.size();
+            int c = 1;
+            int j = 1;
+
+            for (int i = 1; i < nums.size(); i++)
+            {
+                if (nums[i - 1] == nums[i])
+                {
+                    c++;
+                }
+                else
+                {
+                    c = 1;
+                }
+                if (c <= 2)
+                {
+                    nums[j++] = nums[i];
+                }
+            }
+
+            return j;
+}
 };
