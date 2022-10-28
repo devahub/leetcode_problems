95d58122b2dae70cffc43ca2ccd07d0692c6df54
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-28 19:18:50 +0530

    Time: 89 ms (8.27%), Space: 17.4 MB (78.14%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+        int findContentChildren(vector<int> &g, vector<int> &s)
+        {
+            sort(g.begin(), g.end());
+            sort(s.begin(), s.end());
+            int c = 0;
+            int i = 0;
+            int j = 0;
+
+            while (i < g.size() && j < s.size())
+            {
+                if (g[i] <= s[j])
+                {
+                    c++;
+                    i++;
+                }
+                j++;
+            }
+
+            return c;
+        }
+};
