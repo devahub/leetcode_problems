601124261b6d969b990b38367364ac74c2500be2
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-10-29 19:16:23 +0530

    Time: 3 ms (97.70%), Space: 10.7 MB (87.42%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+        void solve(vector<int>& can, int target,vector<int>&v,vector<vector<int>>&ans,int index){
+            if(index==can.size()){
+                if(target==0){
+                    ans.push_back(v);
+                }
+                return;
+            }
+            if(can[index]<=target){
+                v.push_back(can[index]);
+                solve(can,target-can[index],v,ans,index);
+                v.pop_back();
+            }
+            // not pick
+            solve(can,target,v,ans,index+1);
+        }
+    
+    
+        vector<vector<int>> combinationSum(vector<int>& can, int target) {
+            vector<vector<int>>ans;
+            vector<int>v;
+            solve(can,target,v,ans,0);
+            return ans;
+            
+            
+            
+            
+        }
+};
