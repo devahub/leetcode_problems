57091df8e66f3b5953b60ae31dc5658ca009e8ba
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-01 17:01:05 +0530

    Time: 43 ms (71.17%), Space: 13.1 MB (80.92%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    // there will be three cases--->
+    
+    int solve(vector<vector<int>>& grid,int i,int j,int n,int m){
+         if(i>=n)
+            return j;
+ 
+        if(grid[i][j]==1){
+            if(j==m-1 || grid[i][j+1]==-1)
+                return -1;
+           
+            if(grid[i][j+1]==1)
+                return solve(grid,i+1,j+1,n,m);
+        }
+        
+        if(grid[i][j]==-1){
+            if(j==0 || grid[i][j-1]==1)
+                return -1;
+            
+            if(grid[i][j-1]==-1)
+                return solve(grid,i+1,j-1,n,m);
+        }
+
+        return -1;
+    }
+    
+    
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<int>v;
+        
+        
+        for(int i=0;i<m;i++){
+          int col=solve(grid,0,i,n,m);
+            v.push_back(col);
+      }
+        return v;
+        
+    }
+};
+    
+    
