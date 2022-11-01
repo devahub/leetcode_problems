50098dcd5590a3796bf541cb4a6337dab6cd0082
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-01 12:40:00 +0530

    Time: 923 ms (88.20%), Space: 158.9 MB (100.00%) - LeetHub

---
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    static bool cmp(pair<string, int>p1, pair<string, int>p2 ){
+          if(p1.second!=p2.second) return p1.second>p2.second; 
+           return p1.first<p2.first;
+    }
+    
+    
+    vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
+        unordered_map<string,vector<pair<string,int>>>mp;
+        int n=ids.size();
+        unordered_map<string,long long>mp1;
+        
+        for(int i=0;i<n;i++){
+            mp[creators[i]].push_back({ids[i], views[i]});
+            mp1[creators[i]]+=views[i];
+        }
+        
+        long long maxi=0;
+        for(auto i:mp1){
+            maxi=max(maxi,i.second);
+        }
+        vector<string>res;
+        for(auto it:mp1){
+            if(it.second==maxi) 
+                res.push_back(it.first);
+        }
+        
+        vector<vector<string>>res1;
+        for(auto x:res){
+            vector<string>v;
+            auto temp= mp[x];
+            sort(temp.begin(), temp.end(), cmp);
+            v.push_back(x);
+            v.push_back(temp[0].first);
+            res1.push_back(v);
+        }
+        return res1;
+        
+    }
+};
