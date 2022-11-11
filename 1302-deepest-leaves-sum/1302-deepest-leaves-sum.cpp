ca136e923071034ae29af56a4817a134b0755563
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-11 23:17:31 +0530

    Time: 262 ms (5.94%), Space: 61.9 MB (24.52%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -5,23 +5,53 @@ public:
     //              then we adding the leaves of that height
     
 
-    int solve(TreeNode* root,int h){
-        if(!root)return 0;
-        if(h==1)return root->val;
-        return solve(root->left,h-1)+solve(root->right,h-1);
+//     int solve(TreeNode* root,int h){
+//         if(!root)return 0;
+//         if(h==1)return root->val;
+//         return solve(root->left,h-1)+solve(root->right,h-1);
         
-    }
+//     }
     
     
     
-    int getdepth(TreeNode* root){
-        if(root==NULL)return 0;
-        return 1+max(getdepth(root->left),getdepth(root->right));
-    }
+//     int getdepth(TreeNode* root){
+//         if(root==NULL)return 0;
+//         return 1+max(getdepth(root->left),getdepth(root->right));
+//     }
     
-      int deepestLeavesSum(TreeNode* root) {
-        int d=getdepth(root);
-        return solve(root,d);
+//       int deepestLeavesSum(TreeNode* root) {
+//         int d=getdepth(root);
+//         return solve(root,d);
+//     }
+// };
+    
+    
+    // using bfs go to last level and return its sum
+    
+    int deepestLeavesSum(TreeNode* root){
+        if(!root)return 0;
+        int sum;
+        queue<TreeNode*>q;
+        q.push(root);
+        
+        while(!q.empty()){
+            sum=0;
+            
+            int size=q.size();
+            
+            for(int i=0;i<size;i++){
+                TreeNode* front = q.front();
+                q.pop();
+        
+                sum += front->val;
+                
+                if(front->left) q.push(front->left);
+                if(front->right) q.push(front->right);
+                
+            }
+        }
+        return sum;
+        
     }
 };
     
