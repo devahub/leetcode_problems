d7a7932988fb67413296939f4422a0d89515d7a6
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-12 23:07:46 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (63.64%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -20,26 +20,42 @@ public:
 //     }
 // };
     
+    
+    //             approach-2---->
+    
+//      int distinctAverages(vector<int>& nums) {
+//         set<float> res;
+        
+//         deque<int> num(nums.begin(), nums.end());
+//         sort(num.begin(), num.end());
+        
+//         while(!num.empty()) {
+//             int first = num.front();
+//             int last = num.back();
+            
+//             num.pop_front();
+//             num.pop_back();
+            
+//             float avg = (float)((first + last) / (float)2);
+            
+//             res.insert(avg);
+//         }
+//         return res.size();
+//     }
+// };
+    
+    
+    // approach----->3  two pointers-->
+    
      int distinctAverages(vector<int>& nums) {
-        set<float> res;
-        
-        deque<int> num(nums.begin(), nums.end());
-        sort(num.begin(), num.end());
-        
-        while(!num.empty()) {
-            int first = num.front();
-            int last = num.back();
-            
-            num.pop_front();
-            num.pop_back();
-            
-            float avg = (float)((first + last) / (float)2);
-            
-            res.insert(avg);
-        }
-        return res.size();
+        sort(nums.begin(),nums.end());
+        set<double> st;
+        int left = 0, right= nums.size() -1 ;
+        while(left < right){
+            double num1 = nums[left++];
+            double num2 =nums[right--];
+            st.insert((num1 + num2)/2);
+        };
+        return st.size();
     }
 };
-    
-    
-    
