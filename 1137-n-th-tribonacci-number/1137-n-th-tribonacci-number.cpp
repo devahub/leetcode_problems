78e19c111ecbb71d3fd7220d3fb64c7b8ef07a42
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-14 12:01:46 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (20.89%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+   
+    
+    
+    // int tribonacci(int n) {
+      // brut solution---->Tle  
+//         if(n==0)return 0;
+//         if(n==1)return 1;
+//         if(n==2)return 1;
+        
+//      return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        
+//     }     
+// };
+int solve(int n,vector<int>&dp){
+    if(dp[n]!=-1)
+        return dp[n];
+ 
+    else if(n==0)
+        return dp[0]=0;
+    
+    else if(n==1 || n==2)
+        return dp[n]=1;
+    
+    else{
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
+    }
+}    
+    
+ int tribonacci(int n) {
+    vector<int>dp(n+1,-1);
+     return solve(n,dp);
+ }
+};
+
+    
+    
