e347d3792de477c1251e91061c7454980e87f761
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-15 09:47:09 +0530

    Time: 74 ms (25.85%), Space: 30.8 MB (93.85%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,25 +1,40 @@
 
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-        if(root==NULL)
-            return 0;
-        int c=0;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-           int n=q.size();
-            c+=n;
-            for(int i=0;i<n;i++){
-                TreeNode * curr=q.front();
-                q.pop();
-                if(curr->left!=NULL)
-                    q.push(curr->left);
-                if(curr->right!=NULL)
-                    q.push(curr->right);
-            }
+    // using level order traversal----->
+    
+//     int countNodes(TreeNode* root) {
+//         if(root==NULL)
+//             return 0;
+//         int c=0;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty()){
+//            int n=q.size();
+//             c+=n;
+//             for(int i=0;i<n;i++){
+//                 TreeNode * curr=q.front();
+//                 q.pop();
+//                 if(curr->left!=NULL)
+//                     q.push(curr->left);
+//                 if(curr->right!=NULL)
+//                     q.push(curr->right);
+//             }
             
-        }
-        return c;
+//         }
+//         return c;
+//     }
+// };
+    
+    
+    
+    
+    
+    // recurssive solution---->
+    
+    int countNodes(TreeNode* root){
+        if(root==NULL)return 0;
+        
+        return countNodes(root->left)+countNodes(root->right)+1;
     }
 };
