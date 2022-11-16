29110f09e8eaf582e9985dbe7ad1b6a786653c01
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-16 18:01:55 +0530

    Time: 72 ms (52.12%), Space: 13.6 MB (15.02%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+// Eg. 
+// 1. Consider n="9"
+// => We need to add 1 for 9 times to get n
+// 2. n = "4201"
+// => We can add    1101
+//                + 1100
+//                + 1000 
+//                + 1000 
+//              -------
+// 			     4201
+    
+    
+    
+    int minPartitions(string n) {
+        int maxi=0;
+        for(int i=0;i<n.size();i++){
+            maxi=max(maxi,n[i]-'0');
+        }
+        return maxi;
+    }
+};
+    
