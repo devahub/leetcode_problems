a28c5ff968f21eb11071819119585f2785bd7472
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-17 20:47:45 +0530

    Time: 192 ms (35.96%), Space: 48.4 MB (79.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,73 +1,59 @@
-class Solution {
-public:
-    
-     // recurssive soln------>
-//    bool solve(int i,vector<int>& nums,int cur){
-      
-//         if(cur<0){
-//             return false;
-//             }
-        
-        
-//         if(i==nums.size()-1){
-//          return true;
-//         }   
-      
-//         if(nums[i]>cur){
-//             return solve(i+1,nums,nums[i]-1);
-//         }
-        
-//         else{
-//             return solve(i+1,nums,cur-1);
-//         }   
-//     }
-    
-//     bool canJump(vector<int>& nums) {
-//         int cur=nums[0];
-//         return solve(0,nums,cur);
-//     }
+class Solution
+{
+    public:
 
-   
-    // greedy solution--------->
-    
-    
-//     bool canJump(vector<int>& nums){
-//         int n=nums.size();
-//         int reach=0;
-        
-//         for(int i=0;i<n;i++){
-//             if(i>reach){
-//                 return false;
-//             }
-//             reach=max(reach,nums[i]+i);
-//         }
-//         return true;
-        
-//     }
-// };
-    
-    
-    bool solve(int i,vector<int>&nums,int cur){
-        
-        if(cur<0){
-            return false;
+       	// greedy solution--------->
+
+       	//     bool canJump(vector<int>& nums){
+       	//         int n=nums.size();
+       	//         int reach=0;
+
+       	//         for(int i=0;i < n;i++){
+       	//             if(i>reach){
+       	//                 return false;
+       	//             }
+       	//             reach=max(reach,nums[i]+i);
+       	//         }
+       	//         return true;
+
+       	//     }
+       	// };
+
+        bool solve(int i, vector<int> &nums, int cur)
+        {
+
+            if (cur < 0)
+            {
+                return false;
+            }
+
+            if (i == nums.size() - 1)
+            {
+                return true;
+            }
+
+            if (nums[i] > cur)
+            {
+                return solve(i + 1, nums, nums[i] - 1);
+            }
+            else
+            {
+                return solve(i + 1, nums, cur - 1);
+            }
         }
-        
-        if(i==nums.size()-1){
-            return true;
-        }
-        
-        if(nums[i]>cur){
-            return solve(i+1,nums,nums[i]-1);
-        }
-        
-        else{
-            return solve(i+1,nums,cur-1);
-        }
-    }
-    
-    bool canJump(vector<int>& nums){
-      return solve(0,nums,nums[0]);    
-        
+
+    bool canJump(vector<int> &nums)
+    {
+        return solve(0, nums, nums[0]);
     }
 };
+
+//         greedy---->
+
+//         int n=nums.size();
+//         int last=n-1;
+//         for(i = n-2; i >= 0; i--){
+//             if(i+nums[i] >= last)
+// 			           last = i;
+//         }
+//        return last==0;
