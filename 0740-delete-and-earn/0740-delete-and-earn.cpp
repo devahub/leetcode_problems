911b3283add3a6d667f86a7343caa09b4b466b01
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-17 16:59:58 +0530

    Time: 168 ms (5.51%), Space: 56.4 MB (5.08%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
   
-    int deleteAndEarn(vector<int>& nums) {
-        vector<int>freq(10001);
-        for(auto i:nums){
-                freq[i]++;
-        }
-        int cp=0;
-        int ep=0;
+//     int deleteAndEarn(vector<int>& nums) {
+//         vector<int>freq(10001);
+//         for(auto i:nums){
+//                 freq[i]++;
+//         }
+//         int cp=0;
+//         int ep=0;
         
-        for(int i=1;i<10001;i++)
-        {
-            int c=ep+(freq[i]*i);
-            ep=max(ep,cp);
-            cp=c;
-        }
-        return max(cp,ep);
-    }
+//         for(int i=1;i<10001;i++)
+//         {
+//             int c=ep+(freq[i]*i);
+//             ep=max(ep,cp);
+//             cp=c;
+//         }
+//         return max(cp,ep);
+//     }
+// };
+
+
+int deleteAndEarn(vector<int>& nums) {
+		vector<int> freq(100001, 0);
+		vector<int> dp(100001, 0);
+
+		for (auto i : nums) freq[i]++;
+		dp[1] = freq[1];
+
+		for (int i=2; i<100001; i++)
+				dp[i] = max(dp[i-2]+i*freq[i], dp[i-1]);
+
+		return dp[100000];
+}
 };
