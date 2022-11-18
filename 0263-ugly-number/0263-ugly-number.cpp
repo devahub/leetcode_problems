9a505ef9a20f29de67add9cd2435c803ed7eda6c
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-18 11:14:30 +0530

    Time: 3 ms (65.96%), Space: 5.8 MB (86.72%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,21 +1,35 @@
-class Solution {
-public:
-    bool isUgly(int n) {
-        //recurssively
-        
-        
-        if(n==0)return false;
-        if(n==1)return true;
-        
-        if(n%2==0)
-            return isUgly(n/2); 
-        if(n%3==0)
-            return isUgly(n/3);
-        if(n%5==0)
-            return isUgly(n/5);
-                
-        else
-             return false;
-        
+class Solution
+{
+    public:
+
+       	//recurssively
+//         bool isUgly(int n)
+//         {
+//             if (n == 0) return false;
+//             if (n == 1) return true;
+
+//             if (n % 2 == 0)
+//                 return isUgly(n / 2);
+//             if (n % 3 == 0)
+//                 return isUgly(n / 3);
+//             if (n % 5 == 0)
+//                 return isUgly(n / 5);
+
+//             else
+//                 return false;
+//         }
+// };
+
+bool isUgly(int n) 
+    {
+        if (n <= 0) return false;
+        while(n > 1)
+        {
+            if (n % 2 == 0) n = n / 2;
+            else if (n % 3 == 0) n = n / 3;
+            else if (n % 5 == 0) n = n / 5;
+            else break;
+        }
+        return (n == 1);
     }
 };
