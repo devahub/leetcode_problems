c102bcdbc703d079e551af5e6ee92937d2d7a60a
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-20 13:54:27 +0530

    Time: 314 ms (100.00%), Space: 133 MB (33.33%) - LeetHub

--- 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -1,38 +1,88 @@
 
 class Solution {
 public:
-    vector<int> solve(TreeNode *root , int q){
-        TreeNode* p = root;
-        int lb = INT_MIN ;
-        int ub =  INT_MAX;
+//     vector<int> solve(TreeNode *root , int q){
+//         TreeNode* p = root;
+//         int lb = INT_MIN ;
+//         int ub =  INT_MAX;
         
-        while(p){
-            if(p->val<q){
-                lb=max(lb,p->val);
-                p=p->right;
-            }
+//         while(p){
+//             if(p->val<q){
+//                 lb=max(lb,p->val);
+//                 p=p->right;
+//             }
             
-            else if(p->val>q){
-                ub=min(ub,p->val);
-                p=p->left;
-            }
+//             else if(p->val>q){
+//                 ub=min(ub,p->val);
+//                 p=p->left;
+//             }
             
-            else{
-                return {p->val , p->val};
-            }
-        }
+//             else{
+//                 return {p->val , p->val};
+//             }
+//         }
         
-        if(lb == INT_MIN) lb = -1;
-        if(ub == INT_MAX) ub = -1;
-        return {lb , ub};  
-    } 
+//         if(lb == INT_MIN) lb = -1;
+//         if(ub == INT_MAX) ub = -1;
+//         return {lb , ub};  
+//     } 
     
-    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
-        vector<vector<int>>ans(queries.size());
-        int i=0;
-        for(auto q:queries){
-            ans[i++]=solve(root,q);
+//     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
+//         vector<vector<int>>ans(queries.size());
+//         int i=0;
+//         for(auto q:queries){
+//             ans[i++]=solve(root,q);
+//         }
+//     return ans;
+//     }
+// };
+
+
+
+
+
+
+// approach---->2
+
+// using binary search and inorder 
+
+ vector<int> v;
+    void printInorder(TreeNode* node)
+    {
+        if (node == NULL)
+            return;
+        printInorder(node->left);
+        v.push_back(node->val);
+        printInorder(node->right);
+    }
+vector<int> binarySearch(int x, int low, int high) {
+    int mini = -1, maxi = -1;
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (v[mid] == x)
+      return {v[mid], v[mid]};
+
+    if (v[mid] < x){
+      low = mid + 1;
+      mini = v[mid];
+    }
+    else{
+      high = mid - 1;
+      maxi = v[mid];
+    }
+  }
+
+  return {mini, maxi};
+}
+    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& q) {
+        printInorder(root);
+        vector<vector<int>> ans;
+        for(int i=0; i<q.size(); i++){
+            ans.push_back(binarySearch(q[i], 0, v.size()-1));
         }
-    return ans;
+        return ans;
     }
 };
+
+
