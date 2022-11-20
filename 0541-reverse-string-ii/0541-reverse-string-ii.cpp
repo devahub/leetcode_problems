bc6c62b64ca7dcb7779c207aeecb79073071ae2f
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-20 21:33:57 +0530

    Time: 15 ms (27.31%), Space: 7.4 MB (37.50%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for(int i=0;i<s.size();i+=2*k){
+            if(i+k<s.size()){
+                reverse(s.begin()+i,s.begin()+i+k);
+            }
+            else{
+                reverse(s.begin()+i,s.end());
+            }
+        }
+        return s;
+    }
+};
