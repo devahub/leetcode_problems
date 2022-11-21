75b9b0fe15e15037f8ce721576c17c10bfcd651c
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-22 02:56:08 +0530

    Time: 11 ms (32.20%), Space: 11.1 MB (28.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,49 +1,54 @@
-class Solution {
-public:
-   // logic---if a sorted array is rotated then either half of the array is sorted 
-        // now check if nums[mid]>=nums[l], the left array is sorted
-        // now if left array is sorted check if target is present in that or not
-        // do the same for nums[mid]<=nums[l]
-        
-//     int search(vector<int>& nums, int target) {
-  
-//         int l=0;
-//         int h=nums.size()-1;
-            
-//          while(l<=h){
-//                 int mid=l+(h-l)/2;
-//                 if(target==nums[mid]){
-//                     return mid;
-//                 }
-//                 else if(nums[mid]>=nums[l]){
-//                     if(nums[l]<=target && target<nums[mid])
-//                         h=mid-1;
-//                     else
-//                         l=mid+1;
-//                 }
-//                 else {
-//                     if(nums[mid]<target && target<=nums[h])
-//                         l=mid+1;
-//                     else
-//                         h=mid-1;
-//                 }   
-//          }
-        
-//         return -1;
-        
-//     }
-// };
-    
-            // brute force----->
-                
-        int search(vector<int>& nums, int target) {
-            
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]==target){
-                    return i;
+class Solution
+{
+    public:
+       	// logic---if a sorted array is rotated then either half of the array is                sorted 
+       	// now check if nums[mid]>=nums[l], the left array is sorted
+       	// now if left array is sorted check if target is present in that or not
+       	// do the same for nums[mid]<=nums[l]
+
+        int search(vector<int> &nums, int target)
+        {
+
+            int l = 0;
+            int h = nums.size() - 1;
+
+            while (l <= h)
+            {
+                int mid = l + (h - l) / 2;
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+                else if (nums[mid] >= nums[l])
+                {
+                    if (nums[l] <= target && target < nums[mid])
+                        h = mid - 1;
+                    else
+                        l = mid + 1;
+                }
+                else
+                {
+                    if (nums[mid] < target && target <= nums[h])
+                        l = mid + 1;
+                    else
+                        h = mid - 1;
                 }
             }
-            
+
             return -1;
         }
 };
+
+// brute force----->
+
+//         int search(vector<int>& nums, int target) {
+
+//             for(int i=0;i < nums.size();i++){
+//                 if(nums[i]==target){
+//                     return i;
+//                 }
+//             }
+
+//             return -1;
+//         }
+// };
