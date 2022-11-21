31b07e381ad6a277a892778055bf74fe74c7813d
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-22 02:51:52 +0530

    Time: 40 ms (75.74%), Space: 22.2 MB (91.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,15 +1,57 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
+    
+    // using map-->
+    
+//     int singleNonDuplicate(vector<int>& nums) {
+//         unordered_map<int,int>mp;
         
-        for(auto i:nums)mp[i]++;
+//         for(auto i:nums)mp[i]++;
         
-        for(auto i:mp){
-            if(i.second==1){
-                return i.first;
-            }
-        }
-        return -1;
-    }
+//         for(auto i:mp){
+//             if(i.second==1){
+//                 return i.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+    
+    
+    // using binary search---->
+    
+     int singleNonDuplicate(vector<int>& nums){
+            int i=0;
+            int j=nums.size()-1;
+         
+            if(nums.size()==1) return nums[0];
+            if(nums[0] != nums[1]) return nums[0];
+            if(nums[j] != nums[j-1]) return nums[j];
+            
+         
+         while(i<=j){
+             int mid=i+(j-i)/2;
+             
+             if(mid%2==0){
+                if(nums[mid] == nums[mid+1])
+                    i = mid+1;
+                else
+                    j = mid-1;  
+             }
+             
+             else if(mid%2!=0){
+                if(nums[mid] == nums[mid+1])
+                    j = mid-1;
+                else
+                    i = mid+1;
+             }
+             
+        if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
+                return nums[mid];
+             
+         }
+        return -1;    
+     }
 };
+    
+    
