0da23d8fe60539ebf4b701f0658642fa72873c30
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-22 12:38:26 +0530

    Time: 466 ms (31.33%), Space: 9.9 MB (26.62%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp){
-        int c=INT_MAX;
+//     int solve(int n,vector<int>&dp){
+//         int c=INT_MAX;
         
-        if(n==0){
-           dp[0]=0;
-            return 0;
-           }
+//         if(n==0){
+//            dp[0]=0;
+//             return 0;
+//            }
         
-         if(dp[n]!=-1){
-            return dp[n];
-            }
+//          if(dp[n]!=-1){
+//             return dp[n];
+//             }
         
-        for(int i=1;i*i<=n;i++){
-            int minp=solve(n-i*i,dp);
-            c=min(c,minp);
+//         for(int i=1;i*i<=n;i++){
+//             int minp=solve(n-i*i,dp);
+//             c=min(c,minp);
+//         }
+        
+//         dp[n]= c+=1;
+        
+//         return c;
+//     }
+    
+    
+    
+//     int numSquares(int n) {
+//         vector<int>dp(n+1,-1);
+//         return solve(n,dp);
+//     }
+// };
+    
+    
+    
+     int solve(int n, vector<int>&dp)
+    {
+        if (n == 0) return 0;
+        if (dp[n] != -1) return dp[n];
+        
+        int mnCount = INT_MAX;
+        for (int num = 1; num <= sqrt(n); num++)
+        {
+            int sqNum = num * num;
+            int currCount = 1 + solve(n - sqNum, dp);
+            mnCount = min(mnCount, currCount);
         }
-        
-        dp[n]= c+=1;
-        
-        return c;
+        return dp[n] = mnCount;
     }
-    
-    
-    
-    int numSquares(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+    int numSquares(int n) 
+    {
+        vector<int>dp(n + 1, -1);
+        return solve(n, dp);
     }
 };
