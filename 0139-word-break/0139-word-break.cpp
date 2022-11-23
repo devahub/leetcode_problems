2d68686a98a8e6d633be169438a453c9b4356ab0
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-23 19:49:13 +0530

    Time: 57 ms (12.88%), Space: 13.9 MB (26.69%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    unordered_map<string, int> map;
+    int dp[305][305];
+    
+    bool solve(string &s, int n, int start){
+        if(start==n) return true;
+        if(dp[start][n]!=-1) return dp[start][n];
+        
+        for(int i=start;i<n;i++){
+            string str = s.substr(start,i-start+1);
+            if(map.find(str)!=map.end()){
+                if(solve(s,n,i+1)) 
+                    return dp[start][n] = true;
+            }
+        }
+        
+        return dp[start][n] = false;
+    }
+    
+    
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        memset(dp,-1,sizeof(dp));
+            
+        for(auto i:wordDict){
+            map[i]++;    
+        }
+        int n=s.size();
+        
+        return solve(s,n,0);
+        
+    }
+};
