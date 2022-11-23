d5026ccf321302b5799c7a5688a0fc548da14738
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-23 21:40:18 +0530

    Time: 47 ms (16.28%), Space: 19.7 MB (81.98%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,67 +2,69 @@ class Solution
 {
     public:
 
-       	    int trap(vector<int>& h) {
-       	        int n=h.size();
-       	        vector < int>vmax(n,0);
-       	        vector < int>vmin(n,0);
-       	        int area=0;
+    // using vector---->
+    
+//        	    int trap(vector<int>& h) {
+//        	        int n=h.size();
+//        	        vector < int>vmax(n,0);
+//        	        vector < int>vmin(n,0);
+//        	        int area=0;
 
-       	       for(int i=1;i < n;i++){
-       	           vmax[i]=max(vmax[i-1],h[i-1]);
-       	       }     
+//        	       for(int i=1;i < n;i++){
+//        	           vmax[i]=max(vmax[i-1],h[i-1]);
+//        	       }     
 
-       	        for(int i=n-2;i>=0;i--){
-       	           vmin[i]=max(vmin[i+1],h[i+1]);
-       	       }    
+//        	        for(int i=n-2;i>=0;i--){
+//        	           vmin[i]=max(vmin[i+1],h[i+1]);
+//        	       }    
 
-       	        for(int i=1;i < n-1;i++){
-       	           area += max(0, min(vmax[i], vmin[i]) - h[i]);
+//        	        for(int i=1;i < n-1;i++){
+//        	           area += max(0, min(vmax[i], vmin[i]) - h[i]);
 
-       	        }
-       	        return area;
-       	    }
-       	};
+//        	        }
+//        	        return area;
+//        	    }
+//        	};
 
        	/// two pointer approach---->
 
-//         int trap(vector<int> &h)
-//         {
-//             int n = h.size();
-//             int l = 0;
-//             int r = n - 1;
-//             int water = 0;
-//             int leftmax = 0;
-//             int rightmax = 0;
+        int trap(vector<int> &h)
+        {
+            int n = h.size();
+            int l = 0;
+            int r = n - 1;
+            int water = 0;
+            int leftmax = 0;
+            int rightmax = 0;
 
-//             while (l < r)
-//             {
-//                 if (h[l] < h[r])
-//                 {
+            while (l < r)
+            {
+                if (h[l] < h[r])
+                {
 
-//                     if (h[l] < leftmax)
-//                     {
-//                         water += leftmax - h[l];
-//                     }
-//                     else
-//                     {
-//                         leftmax = h[l];
-//                     }
-//                     l++;
-//                 }
-//                 else
-//                 {
-//                     if (h[r] < rightmax)
-//                     {
-//                         water += rightmax - h[r];
-//                     }
-//                     else
-//                     {
-//                         rightmax = h[r];
-//                     }
-//                     r--;
-//                 }
-//             }
-//             return water;
-//         }
-// };
+                    if (h[l] < leftmax)
+                    {
+                        water += leftmax - h[l];
+                    }
+                    else
+                    {
+                        leftmax = h[l];
+                    }
+                    l++;
+                }
+                else
+                {
+                    if (h[r] < rightmax)
+                    {
+                        water += rightmax - h[r];
+                    }
+                    else
+                    {
+                        rightmax = h[r];
+                    }
+                    r--;
+                }
+            }
+            return water;
+        }
+};
