3f5ba3d56edac9cd676da4871a954ad2df6a4711
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-24 00:45:47 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (47.84%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -1,31 +1,57 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        if(nums.size() < 3)
-            return 0;
+    
+    // brute force---->
+    
+//     int numberOfArithmeticSlices(vector<int>& nums) {
+//         if(nums.size() < 3)
+//             return 0;
         
-        int cnt = 0, diff;
+//         int cnt = 0, diff;
         
-        for(int i = 0; i<nums.size()-2; ++i)
-        {
-			// storing diff of first 2 elements
-            diff = nums[i+1] - nums[i];
+//         for(int i = 0; i<nums.size()-2; ++i)
+//         {
+//             diff = nums[i+1] - nums[i];
 			
-			// checking for consecutive elements with same difference.
-            for(int j = i+2; j<nums.size(); ++j)
-            {
-				// if we find the same diff of next 2 elements
-				// this means we  find consecutive elements
-				// increase the Count
-                if(nums[j] - nums[j-1] == diff)
-                    ++cnt;
-                else
-				// break as we need to cnt for consecutive diff elements
-                    break;
-            }
-        }
-		// return cnt
-        return cnt;
+//             for(int j = i+2; j<nums.size(); ++j)
+//             {
+			
+//                 if(nums[j] - nums[j-1] == diff)
+//                     ++cnt;
+//                 else
+//                     break;
+//             }
+//         }
+//         return cnt;
 
-    }
+//     }
+// };
+    
+    
+    
+    // using previous diff pointers
+    
+     int numberOfArithmeticSlices(vector<int>& nums){
+         if(nums.size() < 3)
+            return 0;
+         
+         int c=0;
+         int diff=0;
+         int ind=0;
+         int prev_diff = nums[1] - nums[0];
+         
+         for(int i=1;i<nums.size()-1;i++){
+             int diff = nums[i+1] - nums[i];
+             
+             if(diff ==  prev_diff)
+                ind++;
+             
+             else{
+                 prev_diff=diff;
+                 ind=0;
+             }
+             c+=ind;
+         }
+         return c; 
+     }
 };
