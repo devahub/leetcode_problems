f5faf1b5c0d7e22446f59b4ada5f776f3b6fe833
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-23 18:56:38 +0530

    Time: 72 ms (9.20%), Space: 20.3 MB (27.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,38 +1,73 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& board,int row,int col){
-        
-        int c=board[row][col];
-        board[row][col] = '.';                      
-        
-        for(int i=0;i<9;i++){
-            if(c==board[i][col]){               //check for column
-               return false;
-                    }
-           if(c==board[row][i]){                //check for row
-               return false;
-           }
-           if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c){            //check in the 3*3 box
-               return false;
-           }
-        }
-        board[row][col]=c;
-        return true;
-        
-    }
     
-    bool isValidSudoku(vector<vector<char>>& board) {
+    // brute force---->
+    
+//     bool check(vector<vector<char>>& board,int row,int col){
         
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]!='.')
-                {
-                    if(!check(board,i,j)){
-                        return false;
-                    }
-                }
+//         int c=board[row][col];
+//         board[row][col] = '.';                      
+        
+//         for(int i=0;i<9;i++){
+//             if(c==board[i][col]){               //check for column
+//                return false;
+//                     }
+//            if(c==board[row][i]){                //check for row
+//                return false;
+//            }
+//            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c){            //check in the 3*3 box
+//                return false;
+//            }
+//         }
+//         board[row][col]=c;
+//         return true;
+        
+//     }
+    
+//     bool isValidSudoku(vector<vector<char>>& board) {
+        
+//         for(int i=0;i<9;i++){
+//             for(int j=0;j<9;j++){
+//                 if(board[i][j]!='.')
+//                 {
+//                     if(!check(board,i,j)){
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
+    
+    
+    
+    
+    
+    // using set--->
+    
+    bool isValidSudoku(vector<vector<char>>& board){
+        vector<set<int>>rows(9);
+        vector<set<int>>cols(9);
+        vector<set<int>>blocks(9);
+        
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                
+                if (board[i][j] == '.') continue;
+                
+                int curr = board[i][j] - '0';
+                if (rows[i].count(curr) || cols[j].count(curr) ||       blocks[(i/3)*3+j/3].count(curr)) 
+                     return false;
+                
+                rows[i].insert(curr);
+                cols[j].insert(curr);
+                blocks[(i/3)*3+j/3].insert(curr);
             }
         }
+        
         return true;
+        
     }
 };
