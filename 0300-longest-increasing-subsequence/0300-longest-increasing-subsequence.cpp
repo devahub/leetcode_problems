54cc029d6d8211de17d9803498ac27095ab556b7
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-25 03:04:11 +0530

    Time: 1426 ms (15.46%), Space: 292.4 MB (5.60%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    // using recurssion----->Tle
+    
+//     int solve(vector<int>& nums,int i,int prev)
+//     {
+//         if(i>=nums.size()){
+//             return 0;
+//         }
+        
+//         int pick=0;
+//         int npick=solve(nums,i+1,prev);
+        
+//         if(nums[i]>prev){
+//             pick=1+solve(nums,i+1,nums[i]);
+//         }
+        
+        
+//         return max(pick,npick);
+//     }
+    
+//     int lengthOfLIS(vector<int>& nums) {
+//         return solve(nums,0,INT_MIN);
+//     }
+// };
+    
+    
+    
+        // using dp-->memoization---->
+   
+    
+    int solve(int ind , int prev ,vector<int>& arr,vector<vector<int>> &dp){
+        if(ind == arr.size())return 0;
+            
+        if(dp[ind][prev+1] != -1){
+            return dp[ind][prev+1];
+        }
+        
+        int nottake = solve(ind+1,prev,arr,dp);
+        int take =0;
+        if(prev==-1 || arr[ind]>arr[prev]){
+            take = 1+solve(ind+1,ind,arr,dp);
+        }
+        return dp[ind][prev+1]=max(nottake,take);
+    }
+    
+     int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solve(0,-1,nums,dp);
+		}
+};
