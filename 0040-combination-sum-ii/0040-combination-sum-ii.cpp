a9965ebf6971b4f7617518f533c7341dd1fc7d7c
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-25 18:10:00 +0530

    Time: 14 ms (43.23%), Space: 10.4 MB (98.34%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -34,7 +34,7 @@ public:
     
     
     
-    void subset_sum(vector<int>& arr, vector<vector<int>>& ans, vector<int>& v, int i, int sum)
+    void solve(vector<int>& arr, vector<vector<int>>& ans, vector<int>& v, int i, int sum)
 {
         if(i >= arr.size() || sum <= 0)
         {
@@ -45,7 +45,7 @@ public:
         
         // pick it
         v.push_back(arr[i]);
-        subset_sum(arr, ans, v, i+1, sum-arr[i]);
+        solve(arr, ans, v, i+1, sum-arr[i]);
         v.pop_back();
         
         // ignore duplicate elements...
@@ -53,15 +53,17 @@ public:
             i++;
         
         // don't pick it
-        subset_sum(arr, ans, v, i+1, sum);
+        solve(arr, ans, v, i+1, sum);
 }
+    
+    
     
 vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
 {
         vector<vector<int>> ans;
         vector<int> v;
         sort(candidates.begin(), candidates.end());
-        subset_sum(candidates, ans, v, 0, target);
+        solve(candidates, ans, v, 0, target);
         return ans;
 }
 };
