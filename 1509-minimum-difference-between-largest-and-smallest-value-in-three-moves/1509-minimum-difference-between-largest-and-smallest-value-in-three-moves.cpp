7077b904fca84c88507698e12f0ea57857e40da0
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-26 19:57:43 +0530

    Time: 279 ms (15.95%), Space: 35.3 MB (63.19%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,21 +1,49 @@
 class Solution
 {
     public:
-        int minDifference(vector<int> &nums)
-        {
-            int n = nums.size();
-            if (n <= 3)
-            {
-                return 0;
-            }
-            sort(nums.begin(), nums.end());
+//         int minDifference(vector<int> &nums)
+//         {
+//             int n = nums.size();
+//             if (n <= 3)
+//             {
+//                 return 0;
+//             }
+//             sort(nums.begin(), nums.end());
 
-            int mini = INT_MAX;
+//             int mini = INT_MAX;
 
-            for (int i = 0; i < 4; i++) {
-                mini=min(mini,nums[n-1-i]-nums[3-i]);
+//             for (int i = 0; i < 4; i++) {
+//                 mini=min(mini,nums[n-1-i]-nums[3-i]);
                 
-            }
-            return mini;
+//             }
+//             return mini;
+//         }
+// };
+    
+    
+    
+    // same approach---->
+    
+ // 1 kill 3 smallest elements
+ // 2 kill 3 biggest elements
+ // 3 kill 1 biggest elements + 2 smallest elements
+ // 4 kill 2 biggest elements + 1 smallest elements
+    
+     int minDifference(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n<=4){
+            return 0;
         }
+        //1st
+        int res=nums[n-1]-nums[3];
+        //2nd
+        res=min(res,nums[n-4]-nums[0]);
+        //3rd
+        res=min(res,nums[n-2]-nums[2]);
+        //4th
+        res=min(res,nums[n-3]-nums[1]);
+        return res;
+        
+    }
 };
