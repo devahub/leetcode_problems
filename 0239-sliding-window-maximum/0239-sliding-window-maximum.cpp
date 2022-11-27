6514bb8a79ea6aa9396542425fbfad95263c4c34
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-27 13:00:01 +0530

    Time: 337 ms (86.56%), Space: 134.6 MB (81.41%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,38 +1,71 @@
 class Solution {
 public:
     
+    // using list--->
     
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>v;
-        list<int>l;
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         vector<int>v;
+//         list<int>l;
         
-        int i=0;
-        int j=0;
+//         int i=0;
+//         int j=0;
         
-        while(j<nums.size()){
+//         while(j<nums.size()){
             
-            while(l.size()>0 && l.back()<nums[j])
-            {
-                l.pop_back();
-            }
-            l.push_back(nums[j]);
+//             while(l.size()>0 && l.back()<nums[j])
+//             {
+//                 l.pop_back();
+//             }
+//             l.push_back(nums[j]);
             
-         if(j-i+1<k)
-         {
-            j++; 
-         }
-         else if(j-i+1==k){
-                v.push_back(l.front());
-                if (l.front()==nums[i])
-                {
-                    l.pop_front();
-                }
-                i++;
-                j++;
+//          if(j-i+1<k)
+//          {
+//             j++; 
+//          }
+//          else if(j-i+1==k){
+//                 v.push_back(l.front());
+//                 if (l.front()==nums[i])
+//                 {
+//                     l.pop_front();
+//                 }
+//                 i++;
+//                 j++;
+//             } 
+//         }
+//         return v;
+        
+//     }
+// };
+    
+
+    
+    // using deque
+    
+     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+         int n=nums.size();
+         vector<int>ans;
+         deque<int>q;
+         int i=0;
+         int j=0;
+         
+         while(j<n){
+            while(q.size()>0 && q.back()<nums[j]){
+                q.pop_back();
             } 
-        }
-        return v;
-        
-    }
+            q.push_back(nums[j]);
+            
+             if(j-i+1<k)j++;
+             
+             else if(j-i+1 == k){
+                ans.push_back(q.front());
+                 
+                if(nums[i] == q.front())q.pop_front();
+                
+                i++;j++;    
+             }
+         }
+         return ans;
+     }
 };
+    
     
