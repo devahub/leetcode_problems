ce85c95c26a0c020a52eac2bdabb47f41a9a1488
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-27 13:07:02 +0530

    Time: 103 ms (30.91%), Space: 7.7 MB (85.95%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -17,7 +17,7 @@ public:
             
             while(i < n && mp['Q']<=k && mp['W']<=k && mp['E']<= k && mp['R']<=k)
 			{
-				ans = min(ans, j-i+1);      // line is to check and return for min length substring
+				ans = min(ans, j-i+1);  //to check for min length substring
 				mp[s[i++]]++;
 			}
 		}
