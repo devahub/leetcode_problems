b3272a1c96308b44f51759cb25d89f8ba2fddf16
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-28 19:48:47 +0530

    Time: 41 ms (25.32%), Space: 14 MB (97.27%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -4,76 +4,77 @@ public:
         //brute force---->
     
     
-        bool getPath(TreeNode *root,vector<TreeNode *> &res, int x){
+//         bool getPath(TreeNode *root,vector<TreeNode *> &res, int x){
 
-        if(root == NULL){
-            return false;
-        }
+//         if(root == NULL){
+//             return false;
+//         }
 
-        res.push_back(root);
+//         res.push_back(root);
 
-        if(root->val == x) return true;
+//         if(root->val == x) return true;
 
-        if(getPath(root->left,res,x) || getPath(root->right,res,x)) return true;
+//         if(getPath(root->left,res,x) || getPath(root->right,res,x)) return true;
 
-        res.pop_back();
-        return false;
-    }
+//         res.pop_back();
+//         return false;
+//     }
     
     
     
-        TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
-            TreeNode *res;
+//         TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
+//             TreeNode *res;
 
-            if(!root){
-                return {};
-                }
+//             if(!root){
+//                 return {};
+//                 }
         
-            vector<TreeNode *> v1;
-            vector<TreeNode *> v2;
+//             vector<TreeNode *> v1;
+//             vector<TreeNode *> v2;
             
-               getPath(root,v1,p->val);
-               getPath(root,v2,q->val);
+//                getPath(root,v1,p->val);
+//                getPath(root,v2,q->val);
 
-        int n = min(v1.size(),v2.size());
+//         int n = min(v1.size(),v2.size());
 
-        for(int i=0;i<n;i++){
-            if(v1[i] == v2[i]){
-                res = v1[i];
-            }else{
-                break;
-            }
-        }
-        return res;
-    }
-};
+//         for(int i=0;i<n;i++){
+//             if(v1[i] == v2[i]){
+//                 res = v1[i];
+//             }else{
+//                 break;
+//             }
+//         }
+//         return res;
+//     }
+// };
     
     
   
-//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//          if(root==NULL){
-//             return NULL;
-//         }
-//          if(root==p || root==q){
-//             return root;
-//          }
-//         TreeNode* left;
-//         TreeNode* right;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+         if(root==NULL){
+            return NULL;
+        }
+         if(root==p || root==q){
+            return root;
+         }
         
-//         left=lowestCommonAncestor(root->left,p,q);
-//         right=lowestCommonAncestor(root->right,p,q);
+        TreeNode* left;
+        TreeNode* right;
+        
+        left=lowestCommonAncestor(root->left,p,q);
+        right=lowestCommonAncestor(root->right,p,q);
         
         
-//         if(left==NULL){
-//             return right;
-//         }
+        if(left==NULL){
+            return right;
+        }
         
-//         else if(right==NULL){
-//             return left;
-//         }
+        else if(right==NULL){
+            return left;
+        }
         
-//         else{
-//             return root;
-//         }
-//     }
-// };
+        else{
+            return root;
+        }
+    }
+};
