b3b5556430f2b7db6edebad5163403451b3bc063
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-28 18:04:58 +0530

    Time: 123 ms (56.37%), Space: 13.3 MB (61.27%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -41,12 +41,13 @@ public:
     // two pointer approach---->
     
      vector<vector<int>> fourSum(vector<int>& nums, int target){
-         vector<vector<int>> ans;
+            vector<vector<int>> ans;
             sort(nums.begin(),nums.end());
         
         int n = nums.size();
         
         for(int i=0; i<n; ++i){
+            
             long long remain = target - nums[i];
             
             for(int j = i+1; j < n; ++j){
@@ -68,20 +69,24 @@ public:
                         ans.push_back({nums[i], nums[j], nums[left], nums[right]}); 
                         
                        
-                        while(left < right && nums[left] == x) ++left;
+                        while(left < right && nums[left] == x)
+                            ++left;
                         
 
-                        while(left < right && nums[right] == y) --right;
+                        while(left < right && nums[right] == y) 
+                            --right;
                     
                     }         
                 }
                 
         
-                while(j + 1 < n && nums[j + 1] == nums[j]) ++j;
+                while(j + 1 < n && nums[j + 1] == nums[j])  //check duplicate of nums[j]
+                    ++j;
             }
             
          
-            while(i + 1 < n && nums[i + 1] == nums[i]) ++i;
+            while(i + 1 < n && nums[i + 1] == nums[i])  // check duplicate of nums[i]
+                ++i;
         }
         
         return ans;
