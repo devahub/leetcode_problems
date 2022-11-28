4d1590f067a22885cbc6a67df3c8403f7db389f1
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-28 17:10:45 +0530

    Time: 48 ms (80.74%), Space: 12.2 MB (97.56%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+   void dfs(vector<vector<char>>& grid,int i,int j){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+                return;                                                      // boundary checking
+       
+        if(grid[i][j] == '2' || grid[i][j] == '0')
+            return;                                 // return if current position is of water or is                     `                                                   already visited
+       
+       grid[i][j] = '2';                            // mark the current as visited
+       
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+   }
+    
+int numIslands(vector<vector<char>>& grid) {
+        int c=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                        c+=1;
+                }
+            }
+        }
+        return c;
+    }
+};
