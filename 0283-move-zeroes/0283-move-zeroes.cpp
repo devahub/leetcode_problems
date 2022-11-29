e8e4eab3dea4a742db7c73e2645a2ff0882e609a
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-29 15:23:34 +0530

    Time: 47 ms (64.26%), Space: 19.3 MB (14.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,47 +1,47 @@
 class Solution
 {
     public:
-//         void moveZeroes(vector<int> &nums)
-//         {
+        void moveZeroes(vector<int> &nums)
+        {
 
-//             int i = 0, j = 0;
+            int i = 0, j = 0;
 
-//             while (i < nums.size())
-//             {
-//                 if (nums[i] != 0)
-//                 {
-//                     swap(nums[i], nums[j]);
-//                     i++;
-//                     j++;
-//                 }
-//                 else
-//                     i++;
-//             }
-//         }
-// };
-
-
-void moveZeroes(vector<int> &nums){
-    
-    stack<int>s;
-    
-    for(auto i:nums){
-        if(i!=0){
-            s.push(i);
+            while (i < nums.size())
+            {
+                if (nums[i] != 0)
+                {
+                    swap(nums[i], nums[j]);
+                    i++;
+                    j++;
+                }
+                else
+                    i++;
+            }
         }
-    }
-   for(auto i:nums){
-       if(i==0){
-           s.push(i);
-       }
-   }
-   
-    int pos=nums.size()-1;
-    while(!s.empty()){
-        nums[pos]=s.top();
-        s.pop();
-        pos--;
-    }
-    
-}
 };
+
+
+// void moveZeroes(vector<int> &nums){
+    
+//     stack<int>s;
+    
+//     for(auto i:nums){
+//         if(i!=0){
+//             s.push(i);
+//         }
+//     }
+//    for(auto i:nums){
+//        if(i==0){
+//            s.push(i);
+//        }
+//    }
+   
+//     int pos=nums.size()-1;
+//     while(!s.empty()){
+//         nums[pos]=s.top();
+//         s.pop();
+//         pos--;
+//     }
+    
+// }
+// };
