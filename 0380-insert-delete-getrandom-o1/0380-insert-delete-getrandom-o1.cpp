acde17aff86805ef05afa9618e6e91f64fb5236f
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-29 13:35:43 +0530

    Time: 253 ms (85.50%), Space: 97.1 MB (19.16%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,41 +1,82 @@
 class RandomizedSet {
 public:
     
-    unordered_set<int>st;
+    // using set------->
+    
+//     unordered_set<int>st;
+//     RandomizedSet() {
+//         st.clear();
+//     }
+    
+//     bool insert(int val) {
+//         if(st.find(val)!=st.end()){
+//             return false;
+//         }
+//         st.insert(val);
+//         return true;
+//     }
+    
+//     bool remove(int val) {
+//          if(st.find(val)==st.end())
+//             return false;
+//         st.erase(val);
+//         return true;
+//     }
+    
+//     int getRandom() {
+//         vector<int> a;
+//         for(auto i : st)
+//         {
+//             a.push_back(i);
+//         }
+//         int n=a.size();
+//         return a[(rand() % n)]; 
+//     }
+// };
+
+
+    
+    
+    //using map---->
+    
+    
+ private:
+    vector<int>v;
+    unordered_map<int,int>mp;
+    
+    
+public:
     RandomizedSet() {
-        st.clear();
+        
     }
     
     bool insert(int val) {
-        if(st.find(val)!=st.end()){
+        if(mp.find(val)!=mp.end())
             return false;
+        else{
+            v.push_back(val);
+            mp[val]=v.size()-1;
+            return true;
         }
-        st.insert(val);
-        return true;
     }
     
     bool remove(int val) {
-         if(st.find(val)==st.end())
-            return false;
-        st.erase(val);
-        return true;
+         if(mp.find(val)==mp.end())
+             return false;
+        else{
+            int last=v.back();  // back() fetches last element of the array vector
+            v[mp[val]]=v.back(); 
+				               
+            v.pop_back();	  
+            mp[last]=mp[val];	   
+            mp.erase(val);	   
+            return true;
+        }
     }
     
-    int getRandom() {
-        vector<int> a;
-        for(auto i : st)
-        {
-            a.push_back(i);
-        }
-        int n=a.size();
-        return a[(rand() % n)]; 
+    int getRandom()
+    {
+        int n=v.size();
+        return v[rand()%n];
     }
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
