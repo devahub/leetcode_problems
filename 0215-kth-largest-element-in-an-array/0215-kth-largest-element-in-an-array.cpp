2b9091f0f1428fffb7d5a099afd44a9c7b24dbde
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-30 14:45:42 +0530

    Time: 154 ms (76.37%), Space: 47.2 MB (53.34%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,28 +1,41 @@
 class Solution
 {
     public:
-        int findKthLargest(vector<int> &nums, int k)
+    
+    // using sorting---->
+    
+//         int findKthLargest(vector<int> &nums, int k)
+//         {
+
+
+//             sort(nums.begin(), nums.end(), greater<int> ());
+//             for (auto i: nums)
+//             {
+//                 k--;
+
+//                 if (k == 0)
+//                 {
+//                     return i;
+//                 }
+//             }
+//             return -1;
+//         }
+// };
+    
+    
+    
+    // using priority queue----->
+    
+     int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<nums.size();i++)
         {
-
-           	// c++ stl---->
-
-           	//         nth_element(nums.begin(), nums.end()-k, nums.end());
-           	//         return nums[nums.size()-k];
-           	//     }
-           	// };
-
-           	// heap soln
-
-            sort(nums.begin(), nums.end(), greater<int> ());
-            for (auto i: nums)
-            {
-                k--;
-
-                if (k == 0)
+            pq.push(nums[i]);
+                if(pq.size()>k)
                 {
-                    return i;
+                    pq.pop();
                 }
-            }
-            return -1;
         }
+        return pq.top();
+    }
 };
