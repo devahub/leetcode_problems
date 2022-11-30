888fe6699bc28abb0487cebb64d2f60316c37c6b
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-30 14:42:41 +0530

    Time: 12 ms (67.44%), Space: 14.7 MB (81.65%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -5,59 +5,61 @@ class Solution
     
     // recurssive approach---->
     
-//         ListNode* mergeTwoLists(ListNode *list1, ListNode *list2)
-//         {
-            
-//             if (list1 == NULL)
-//                 return list2;
-//             if (list2 == NULL)
-//                 return list1;
-
-//             if (list1->val <= list2->val)
-//             {
-//                 list1->next = mergeTwoLists(list1->next, list2);
-//                 return list1;
-//             }
-            
-//             else
-//             {
-//                 list2->next = mergeTwoLists(list1, list2->next);
-//                 return list2;
-//             }
-//         }
-// };
-
-
-
-
-// brut force will be to store both the list2 into a vector and sort the vector
         ListNode* mergeTwoLists(ListNode *list1, ListNode *list2)
         {
-            vector<int>v;
-            ListNode* cur=list1;
-            while(cur!=NULL){
-                v.push_back(cur->val);
-                cur=cur->next;
+            
+            if (list1 == NULL)
+                return list2;
+            if (list2 == NULL)
+                return list1;
+
+            if (list1->val <= list2->val)
+            {
+                list1->next = mergeTwoLists(list1->next, list2);
+                return list1;
             }
-            cur=list2;
-             while(cur!=NULL){
-                v.push_back(cur->val);
-                cur=cur->next;
+            
+            else
+            {
+                list2->next = mergeTwoLists(list1, list2->next);
+                return list2;
             }
-            
-            sort(v.begin(),v.end());
-            
-            ListNode* ans=new ListNode(-1);
-            ListNode* temp=ans;
-            
-        for(auto it:v){
-            ListNode* curr=new ListNode(it);
-            temp->next=curr;
-            temp=curr;
-        }
-        return ans->next;
-          
-            
         }
 };
+
+
+
+
+// brut force 
+    //will be to store both the list2 into a vector and sort the vector
+
+        
+//     ListNode* mergeTwoLists(ListNode *list1, ListNode *list2)
+//         {
+//             vector<int>v;
+//             ListNode* cur=list1;
+//             while(cur!=NULL){
+//                 v.push_back(cur->val);
+//                 cur=cur->next;
+//             }
+            
+//             cur=list2;
+//              while(cur!=NULL){
+//                 v.push_back(cur->val);
+//                 cur=cur->next;
+//             }
+            
+//             sort(v.begin(),v.end());
+            
+//             ListNode* ans=new ListNode(-1);
+//             ListNode* temp=ans;
+            
+//             for(auto i:v){
+//                 ListNode* curr=new ListNode(i);
+//                 temp->next=curr;
+//                 temp=curr;
+//             }
+//         return ans->next;     
+//         }
+// };
     
