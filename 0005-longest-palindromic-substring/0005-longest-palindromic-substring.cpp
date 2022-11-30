cacf5bda2d3b4d9a18773b81045876c0d1384471
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-11-30 12:52:57 +0530

    Time: 1241 ms (7.07%), Space: 287.4 MB (6.74%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool ispal(string &s,int i,int j,vector<vector<int>>&dp){
+       if(i>=j)
+    {
+          return 1;
+        }
+            if(dp[i][j]!=-1)
+         {
+              return dp[i][j];
+            }
+        if(s[i]==s[j])
+        {
+          return dp[i][j]=ispal(s,i+1,j-1,dp);
+        }
+     return dp[i][j]= 0;
+    }
+
+    
+    // we will noramally check for every index whether the word formed is palindrome or not and find max length word later.
+    
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int maxi=0;
+        string ans;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                
+                if(ispal(s,i,j,dp)){
+                    if(j-i+1>maxi){
+                        maxi=j-i+1;
+                        ans=s.substr(i,j-i+1);
+                }
+                }
+            }
+        }
+    return ans;    
+    }
+};
