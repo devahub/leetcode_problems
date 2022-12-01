e321ced09fe560b1aa04efb6c172ec85d090c4d2
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-01 19:05:40 +0530

    Time: 11 ms (20.12%), Space: 6.7 MB (31.29%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
-    bool halvesAreAlike(string s) {
-        int i=0;
-        int j=s.size()-1;
-        int c=0;
-        int t=0;
+//     bool halvesAreAlike(string s) {
+//         int i=0;
+//         int j=s.size()-1;
+//         int c=0;
+//         int t=0;
         
-        string ch="aeiouAEIOU";
+//         string ch="aeiouAEIOU";
         
-        while(i<j){
-            if(ch.find(s[i])<ch.size()){
-                c++;
+//         while(i<j){
+//             if(ch.find(s[i])<ch.size()){
+//                 c++;
+//             }
+//             if(ch.find(s[j])<ch.size()){
+//                 t++;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return c==t;
+        
+//     }
+// };
+    
+    
+    
+     bool halvesAreAlike(string s) {
+        int n=s.size();
+        int a=0,b=0;
+        vector<char>voy={'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for(int i=0;i<n/2;i++){
+            for(auto x : voy){
+                if(s[i]==x){
+                    a++;
+                    break;
+                }
             }
-            if(ch.find(s[j])<ch.size()){
-                t++;
-            }
-            i++;
-            j--;
         }
-        return c==t;
-        
+        for(int i=n/2;i<n;i++){
+            for(auto x : voy){
+                if(s[i]==x){
+                    b++;
+                    break;
+                }
+            }
+        }
+        return a==b;
     }
 };
