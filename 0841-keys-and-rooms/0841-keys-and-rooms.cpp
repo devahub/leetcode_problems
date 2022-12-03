b9b939d3e9146d7882a243a3f807c3c5ac1bb08f
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-04 01:54:58 +0530

    Time: 15 ms (73.97%), Space: 10.4 MB (79.43%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
-//    void dfs(vector<vector<int>> &graph, int s, vector<bool> &vis){
-//         vis[s] = true;
-//         for(int i = 0; i < graph[s].size(); i++){
-//             if(!vis[graph[s][i]]){
-//                 dfs(graph, graph[s][i], vis);
-//             }
-//         }
-//     }
-    
-//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-//         int n=rooms.size();
-        
-//         vector<bool>vis(n,false);
-        
-//         dfs(rooms,0,vis);
-        
-//         for(int i=0;i<n;i++){
-//             if(!vis[i]){
-//                 return false;
-//             }
-//         }
-//     return true;
-        
-//     }
-// };
+
     
     void dfs(vector<vector<int>>& rooms,vector<bool>&vis,int i){
         vis[i]=true;
@@ -36,12 +12,12 @@ public:
     }
     
     
-    
-     bool canVisitAllRooms(vector<vector<int>>& rooms){
+bool canVisitAllRooms(vector<vector<int>>& rooms){
          int n=rooms.size();
          vector<bool>vis(n,false);
          
          dfs(rooms,vis,0);
+         
          for(int i=0;i<n;i++){
              if(!vis[i]){
                  return false;
