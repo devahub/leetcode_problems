6a433189e81f7cd0d590ba105bf1ad640fb58d5a
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-03 17:25:11 +0530

    Time: 26 ms (54.81%), Space: 8.1 MB (87.24%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,48 +1,80 @@
 class Solution {
 public:
-    // using comparator function---->
     
-     static bool cmp(pair<char,int>&a, pair<char,int>&b)
-    {
-        return a.second>b.second;
-    }
+    // using priority queue
     
     string frequencySort(string s) {
-        unordered_map<char,int>map;
-        string ans;
-        vector<pair<char,int>>v;
-        pair<char,int>p;
-        int n= s.size();
-        for(char x:s)
-        {
-            map[x]++;
+        
+        unordered_map<char,int> freq;          
+        priority_queue<pair<int,char>>pq; 
+        
+        for(char c: s)
+            freq[c]++;
+        for(auto it: freq)
+            pq.push({it.second,it.first});
+        
+        
+        s="";   
+        while(!pq.empty()){
+            s+=string(pq.top().first,pq.top().second); 
+            // will automatically make frequency times the character
+            pq.pop();
         }
-        
-        
-         for(auto it=map.begin();it!=map.end();it++)
-            {
-           v.push_back({it->first,it->second});
-            }
-        
-        sort(v.begin(),v.end(),cmp);
-        
-        for(int i=0;i<v.size();i++)
-        {
-            p=v[i];
-            char val=p.first;
-            int freq=p.second;
-            while(freq)
-            {
-                ans.push_back(val);
-                freq--;
-            }
-        }
-        return ans;
+        return s;
     }
 };
     
     
     
+    
+    
+    
+    // using comparator function---->
+    
+//      static bool cmp(pair<char,int>&a, pair<char,int>&b)
+//     {
+//         return a.second>b.second;
+//     }
+    
+//     string frequencySort(string s) {
+//         unordered_map<char,int>map;
+//         string ans;
+//         vector<pair<char,int>>v;
+//         pair<char,int>p;
+//         int n= s.size();
+//         for(char x:s)
+//         {
+//             map[x]++;
+//         }
+        
+        
+//          for(auto it=map.begin();it!=map.end();it++)
+//             {
+//            v.push_back({it->first,it->second});
+//             }
+        
+//         sort(v.begin(),v.end(),cmp);
+        
+//         for(int i=0;i<v.size();i++)
+//         {
+//             p=v[i];
+//             char val=p.first;
+//             int freq=p.second;
+//             while(freq)
+//             {
+//                 ans.push_back(val);
+//                 freq--;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+    
+    
+    
+    
+    
+    // using without comparator
     
 //     string frequencySort(string s) {
 //         map<char,int>mp;
