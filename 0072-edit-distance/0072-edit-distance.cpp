49ada20594af4ead8ddf24806af8f5a6d27269b7
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-03 19:20:33 +0530

    Time: 145 ms (13.53%), Space: 80.8 MB (10.74%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    int solve(string word1, string word2,int m,int n,vector<vector<int>>&dp){
+        if(m==0){
+            return dp[m][n]=n;
+        }
+        if(n==0){
+            return dp[m][n]=m;
+        }
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+        
+        
+        if(word1[m-1]==word2[n-1]){
+            return dp[m][n]=solve(word1,word2,m-1,n-1,dp);
+        }
+         else{
+            int insertChar = solve(word1, word2, m, n-1, dp);
+            int deleteChar = solve(word1, word2, m-1, n, dp);
+            int replaceChar = solve(word1, word2, m-1, n-1, dp);
+            
+            return dp[m][n] = 1 + min({insertChar, deleteChar, replaceChar});
+        }
+        
+    }
+    
+    
+    int minDistance(string word1, string word2) {
+        int m=word1.size();
+        int n=word2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        
+        return solve(word1,word2,m,n,dp);
+    }
+};
