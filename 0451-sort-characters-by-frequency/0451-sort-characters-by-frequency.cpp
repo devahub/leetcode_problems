614babfc9bcafbd14e4a4aad5193f6eaee70b882
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-03 17:21:43 +0530

    Time: 32 ms (36.03%), Space: 8.4 MB (78.73%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    // using comparator function---->
+    
+     static bool cmp(pair<char,int>&a, pair<char,int>&b)
+    {
+        return a.second>b.second;
+    }
+    
+    string frequencySort(string s) {
+        unordered_map<char,int>map;
+        string ans;
+        vector<pair<char,int>>v;
+        pair<char,int>p;
+        int n= s.size();
+        for(char x:s)
+        {
+            map[x]++;
+        }
+        
+        
+         for(auto it=map.begin();it!=map.end();it++)
+            {
+           v.push_back({it->first,it->second});
+            }
+        
+        sort(v.begin(),v.end(),cmp);
+        
+        for(int i=0;i<v.size();i++)
+        {
+            p=v[i];
+            char val=p.first;
+            int freq=p.second;
+            while(freq)
+            {
+                ans.push_back(val);
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
+    
+    
+    
+    
+//     string frequencySort(string s) {
+//         map<char,int>mp;
+//         vector<pair<int,char>>v;
+       
+        
+//         for(auto i:s){
+//             mp[i]++;
+//         }
+        
+//         for(auto i:mp){
+//             v.push_back({i.second,i.first});
+//         }
+        
+//         sort(v.begin(),v.end(),greater<pair<int,char>>());
+        
+//         s="";
+//         for(auto i:v){
+//             s+=string(i.first,i.second);
+//         }
+//         return s;
+// // string ( count, character )---->create a string containing character repeated count times
+//     }
+// };
