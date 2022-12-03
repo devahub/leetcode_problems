e627586dd313bb1d196ccfb128cca467fd3e6284
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-04 02:35:43 +0530

    Time: 12 ms (68.43%), Space: 10.3 MB (38.18%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     
-    bool checkIfExist(vector<int>& arr) {
-        unordered_map<int,int>mp;
+//     bool checkIfExist(vector<int>& arr) {
+//          unordered_map<int,int>mp;
         
-         for(int i=0;i<arr.size();i++){
-            mp[arr[i]]=i;
-        }
+//          for(int i=0;i<arr.size();i++){
+//             mp[arr[i]]=i;
+//         }
         
+//         for(int i=0;i<arr.size();i++){
+//             if(mp.find(arr[i]*2)!=mp.end() && mp[arr[i]*2] != i){
+//                 return true;
+//             }
+//         }
+//     return false;
+//     }
+// };
+    
+    
+    
+    // more optimal solution----->
+    
+     bool checkIfExist(vector<int>& arr) {
+         
+        unordered_set<int> st;
         for(int i=0;i<arr.size();i++){
-            if(mp.find(arr[i]*2)!=mp.end() && mp[arr[i]*2] != i){
+            if(st.count(2*arr[i])>0 || ((arr[i]%2==0) && st.count(arr[i]/2)>0))
                 return true;
-            }
+            st.insert(arr[i]);
         }
-    return false;
+        return false;
     }
 };
+    
