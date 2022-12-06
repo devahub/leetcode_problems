90c9f61872e5ae226a07631f01d7db2d501bb43a
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-06 12:14:16 +0530

    Time: 4 ms (87.70%), Space: 8.9 MB (17.20%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,24 +4,59 @@ public:
  
     // brute force
     
-    int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) 
-            return 0;
-        if(n == 2)
-            return nums[0] > nums[1] ? 0 : 1;
+//     int findPeakElement(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n == 1) 
+//             return 0;
+//         if(n == 2)
+//             return nums[0] > nums[1] ? 0 : 1;
         
         
-        for(int i=1;i<n-1;i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                return i;
-            }
+//         for(int i=1;i<n-1;i++){
+//             if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+//                 return i;
+//             }
+//         }
+        
+//         if(nums[0] > nums[1])                    //  important lines!!!!!
+//             return 0;
+//         else if(nums[n-1] > nums[n-2]) 
+//             return n-1;
+//         return -1;
+//     }
+// };
+    
+    
+    
+  // using binary search----->  
+    
+     int findPeakElement(vector<int>& nums){
+         int n = nums.size();
+         if(n == 1) 
+             return 0;
+         if(n == 2)
+             return nums[0] > nums[1] ? 0 : 1;
+         int l=1;
+         int h=nums.size()-2;
+         
+         
+         while(l<=h){
+             int mid=l+(h-l)/2;
+             
+                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) 
+                    return mid;
+                else if(nums[mid+1] > nums[mid]) 
+                    l = mid+1;
+                else if(nums[mid-1] >= nums[mid]) 
+                    h = mid-1;
+             
         }
-        
-        if(nums[0] > nums[1]) 
+         
+        if(nums[0] > nums[1])               // important lines !!!!!
             return 0;
         else if(nums[n-1] > nums[n-2]) 
             return n-1;
-        return -1;
-    }
+        return -1;   
+     }
 };
+    
