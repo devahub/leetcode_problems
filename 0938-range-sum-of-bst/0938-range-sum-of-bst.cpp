c4fa2c2ceec1009f9d7bde5cf0a097c570c20c3d
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-07 13:33:37 +0530

    Time: 152 ms (82.16%), Space: 64.8 MB (18.63%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -2,26 +2,50 @@
 class Solution {
 public:
     
-    void solve(TreeNode* root,int low,int high,int& sum){
+    
+    // using preorder---->
+    
+//     void solve(TreeNode* root,int low,int high,int& sum){
         
-        if(!root){
-            return;
-        }
-        if(root->val>=low && root->val<=high){
-            sum+=root->val;
-        }
-        if(root->val>low){
-            solve(root->left,low,high,sum);
-        }
-        if(root->val<high){
-            solve(root->right,low,high,sum);
-        }
+//         if(!root){
+//             return;
+//         }
+//         if(root->val>=low && root->val<=high){
+//             sum+=root->val;
+//         }
+//         if(root->val>low){
+//             solve(root->left,low,high,sum);
+//         }
+//         if(root->val<high){
+//             solve(root->right,low,high,sum);
+//         }
+//     }
+    
+    
+//     int rangeSumBST(TreeNode* root, int low, int high) {
+//         int sum=0;
+//         solve(root,low,high,sum);
+//         return sum;
+//     }
+// };
+    
+    
+    
+    
+    // using inorder------>
+    
+      void inorder(TreeNode* node, int &low, int &high, int &sum)
+    {
+        if(!node) return;
+        inorder(node->left, low, high, sum);
+        if(node->val >= low && node->val <= high) sum += node->val;
+        inorder(node->right, low, high, sum);
     }
-    
-    
-    int rangeSumBST(TreeNode* root, int low, int high) {
+    int rangeSumBST(TreeNode* root, int low, int high) 
+    {
         int sum=0;
-        solve(root,low,high,sum);
-        return sum;
+        inorder(root, low, high, sum);
+        return sum;  
     }
 };
+    
