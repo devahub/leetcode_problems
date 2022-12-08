3178fad35955366ea57c6ddb1aaa265fd4f95ae4
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-08 22:52:52 +0530

    Time: 18 ms (5.83%), Space: 12.9 MB (10.23%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -1,37 +1,63 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    
-    void solve(TreeNode* root,vector<int>&v){
-        if(root==NULL){
-            return ;
-        }
-        
-        if(root->left==NULL && root->right==NULL){
-            v.push_back(root->val);
-        }
-        solve(root->left,v);
-        solve(root->right,v);
-    }
-    
-    
-    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>v1;
-        vector<int>v2;
-        
-        solve(root1,v1);
-        solve(root2,v2);
-        
-        return v1==v2;
-    }
-};
+class Solution
+{
+    public:
+
+       	// recurssive soln----->
+
+        // void solve(TreeNode *root, vector<int> &v)
+        // {
+           	//         if(root==NULL){
+           	//             return ;
+           	//         }
+
+           	//         if(root->left==NULL && root->right==NULL){
+           	//             v.push_back(root->val);
+           	//         }
+           	//         solve(root->left,v);
+           	//         solve(root->right,v);
+           	//     }
+
+           	//     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+           	//         vector < int>v1;
+           	//         vector < int>v2;
+
+           	//         solve(root1,v1);
+           	//         solve(root2,v2);
+
+           	//         return v1==v2;
+           	//     }
+           	// };
+
+            
+            
+            
+            
+            
+            
+            
+           	// iterative approach----->
+
+            vector<int>solve(TreeNode *root)
+            {
+                vector<int>v;
+                stack<TreeNode*> s({ root });
+                while (!s.empty())
+                {
+                    root = s.top();
+                    s.pop();
+                    size_t size = s.size();
+                    if (root->right) 
+                        s.push(root->right);
+                    if (root->left) 
+                        s.push(root->left);
+                    if (s.size() == size) 
+                        v.push_back(root->val);
+                }
+                return v;
+            }
+
+            bool leafSimilar(TreeNode *root1, TreeNode *root2)
+            {
+                return solve(root1) == solve(root2);
+            }
+        };
