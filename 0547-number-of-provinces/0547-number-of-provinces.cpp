add6865f4620d42aa9fa9f4e38cd9783aa439dc5
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-10 13:16:40 +0530

    Time: 63 ms (27.19%), Space: 13.8 MB (71.83%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -2,6 +2,7 @@ class Solution
 {
     public:
 
+    
         void dfs(vector<vector < int>> &mat, int i, vector< bool > &vis)
         {
             vis[i] = true;
@@ -13,8 +14,9 @@ class Solution
                 }
             }
         }
+    
 
-
+    
 int findCircleNum(vector<vector < int>> &mat)
 {
 
@@ -33,49 +35,5 @@ int findCircleNum(vector<vector < int>> &mat)
         }
     }
     return c;
-}
+  }
 };
-
-//         int v=isConnected.size();
-//         vector<int> visited(v,0);
-//         vector<int> adjList[v];
-
-//         for(int i=0;i < v;i++)
-//         {
-//             for(int j=0;j < v;j++)
-//             {
-//                 if(isConnected[i][j]==1 and i!=j)
-//                 {
-//                     adjList[i].push_back(j);
-//                     adjList[j].push_back(i);
-//                 }
-//             }
-//         }
-//         int count=0;
-//         for(int i=0;i < v;i++)
-//         {
-//             if(!visited[i])
-//             {
-//                 count++;
-
-//                 dfs(i,visited,adjList);
-//             }
-//         }
-
-//         return count;
-//     }
-
-//  void dfs(int node,vector<int>& visited,vector<int> adjList[])
-//     {
-//         visited[node]=1;
-//         for(auto it: adjList[node])
-//         {
-//             if(!visited[it])
-//             {
-//                 visited[it]=1;
-//                 dfs(it,visited,adjList);
-//             }
-//         }
-//     }
-
-// };
