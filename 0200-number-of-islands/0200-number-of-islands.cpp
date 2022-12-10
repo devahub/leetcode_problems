f26b58b7681b1e1fac112a1fc2f09593dea4fe44
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-10 12:56:23 +0530

    Time: 59 ms (73.36%), Space: 12.1 MB (97.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    
    void dfs(vector<vector<char>>& grid,int i,int j){
         if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
             return;                                              // boundary checking
@@ -15,8 +17,11 @@ public:
         dfs(grid, i, j+1);
    }
     
+    
+    
 int numIslands(vector<vector<char>>& grid) {
-        int c=0;
+        
+    int c=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
