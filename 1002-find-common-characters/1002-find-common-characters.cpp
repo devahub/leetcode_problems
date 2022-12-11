d9193d6d4ad1dd9ac08aaa26c9868d8b99271db1
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-11 19:29:57 +0530

    Time: 53 ms (10.17%), Space: 11.5 MB (12.46%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-       unordered_map<char, int> mp;
+       unordered_map<char, int> mp1;
        vector<string>v;
         
         for(auto x: words[0]) 
-            mp[x]++;
+            mp1[x]++;
         
         for(int i=1; i<words.size(); i++){
            unordered_map<char, int> temp;
@@ -16,14 +16,14 @@ public:
             for(int i=0;i<26;i++){
                 char ch='a'+i;
                 
-                if(temp.find(ch) != temp.end() && mp.find(ch) != mp.end())
-                    mp[ch] = min(mp[ch], temp[ch]);
-                else if(mp.find(ch) != mp.end()) 
-                    mp.erase(ch);
+                if(temp.find(ch) != temp.end() && mp1.find(ch) != mp1.end())
+                    mp1[ch] = min(mp1[ch], temp[ch]);
+                else if(mp1.find(ch) != mp1.end()) 
+                    mp1.erase(ch);
             }
     }
     
-        for(auto x:mp){
+        for(auto x:mp1){
             string s="";
             s += x.first;
             for(int i=1; i<=x.second; i++) 
