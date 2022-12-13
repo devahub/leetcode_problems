45ab3e036f6db8081d97dcd09f20530055ccbcfc
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-13 23:27:12 +0530

    Time: 22 ms (93.83%), Space: 14.1 MB (49.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -6,35 +6,90 @@ public:
     
     // dfs technique-----> 
     
-    bool iscycle(vector<int>adj[],vector<int>&vis,int id){
+//     bool iscycle(vector<int>adj[],vector<int>&vis,int id){
         
-        if(vis[id]==1)
-            return true;
+//         if(vis[id]==1)
+//             return true;
         
-        if(vis[id]==0){
-            vis[id]=1;
-            for(auto edge : adj[id]){
-                if(iscycle(adj,vis,edge))
-                    return true;
-            }
-        }
+//         if(vis[id]==0){
+//             vis[id]=1;
+//             for(auto edge : adj[id]){
+//                 if(iscycle(adj,vis,edge))
+//                     return true;
+//             }
+//         }
         
-        vis[id] = 2;            // mark it as visited
-        return false;   
-    }
+//         vis[id] = 2;            // mark it as visited
+//         return false;   
+//     }
     
     
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<int> vis(n,0);
-        vector<int> adj[n];
-        for(auto i : pre){
-            adj[i[1]].push_back(i[0]);
-        }
+//     bool canFinish(int n, vector<vector<int>>& pre) {
+//         vector<int> vis(n,0);
+//         vector<int> adj[n];
+//         for(auto i : pre){
+//             adj[i[1]].push_back(i[0]);
+//         }
        
-        for(int i=0;i<n;i++){
-            if(iscycle(adj,vis,i))
-                return false;
+//         for(int i=0;i<n;i++){
+//             if(iscycle(adj,vis,i))
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
+
+    
+    
+    
+    
+    
+    // using topological sort----->bfs
+    
+     bool canFinish(int n, vector<vector<int>>& pre) {
+         if(pre.size()==0) 
+             return true;
+         vector<vector<int>>graph(n);
+         vector<int>indegree(n,0);
+         
+        for(auto i:pre){
+            graph[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
         }
-        return true;
-    }
+         
+         int c=0;
+         queue<int>q;
+         
+         for(int i=0;i<n;i++){
+             if(indegree[i]==0){
+                 q.push(i);
+                 indegree[i]--;
+             }
+         }
+         if(q.size()==0){
+             return false;
+         }
+         
+         while(!q.empty()){
+             int size=q.size();
+             
+             for(int i=0;i<size;i++){
+                 int cur=q.front();
+                 q.pop();
+                 c++;
+                 
+                 for(auto j:graph[cur]){
+                     indegree[j]--;
+                     if(indegree[j]==0){
+                         q.push(j);
+                     }
+                 }        
+             }
+         }
+         
+         if(c==n){
+             return true;
+         }
+         return false;
+     }
 };
