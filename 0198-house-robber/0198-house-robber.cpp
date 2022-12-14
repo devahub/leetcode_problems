8d070b17e140d51fa0a24dc9d5ab3d808f09440d
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-14 12:50:28 +0530

    Time: 3 ms (56.21%), Space: 7.8 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,9 +6,11 @@ public:
 // class Solution {
 // public:
 //     int rob(vector<int>& nums) {
+    
 //         if (nums.size() == 0) return nums[0];
 //         int rob1 = 0;
 //         int rob2 = nums[0];
+    
 //         for (int i = 1; i<nums.size(); i++) {
 //             int rob = max(nums[i]+rob1, rob2);
 //             rob1=rob2;
@@ -21,7 +23,8 @@ public:
 
     
     int solve(vector<int>& nums,vector<int>&dp,int i){
-        if(nums.size()<=i)return 0;
+        if(nums.size()<=i)
+            return 0;
         
         if(dp[i]!=-1)return dp[i];
         
