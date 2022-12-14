0bfa49c24a660274b21b1f417b227f779a97905f
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-15 01:25:57 +0530

    Time: 30 ms (83.40%), Space: 13.2 MB (80.80%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -4,18 +4,18 @@ public:
     // there will be three cases--->
     
     int solve(vector<vector<int>>& grid,int i,int j,int n,int m){
-         if(i>=n)
+        if(i>=n)
             return j;
  
-        if(grid[i][j]==1){
+        if(grid[i][j]==1){                      ///      / case
             if(j==m-1 || grid[i][j+1]==-1)
                 return -1;
            
             if(grid[i][j+1]==1)
-                return solve(grid,i+1,j+1,n,m);
+                return solve(grid,i+1,j+1,n,m);      
         }
         
-        if(grid[i][j]==-1){
+        if(grid[i][j]==-1){                        ///       \ case
             if(j==0 || grid[i][j-1]==1)
                 return -1;
             
@@ -34,8 +34,8 @@ public:
         
         
         for(int i=0;i<m;i++){
-          int col=solve(grid,0,i,n,m);
-            v.push_back(col);
+            int t=solve(grid,0,i,n,m);
+            v.push_back(t);
       }
         return v;
         
