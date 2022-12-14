8594a5fd79719bc602d4988650557534643311f8
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-15 01:34:42 +0530

    Time: 4 ms (76.17%), Space: 13.8 MB (43.92%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+   
+    
+    void solve(int n,int open,int close,string cur,vector<string>&v){
+        
+        if(n*2==cur.size()){
+            v.push_back(cur);
+            return;
+        }
+        
+        if(open<n){
+            solve(n,open+1,close,cur+'(',v);
+        }
+        
+        if(close<open){
+        solve(n,open,close+1,cur+')',v);
+        }
+    }
+    
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string>v;
+        solve(n,0,0,"",v);
+        return v;
+    }
+};
