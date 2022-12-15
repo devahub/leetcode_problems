6ebd6c529f0257df676c0e8bd155eea4a38ffccb
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-15 13:47:48 +0530

    Time: 4 ms (31.08%), Space: 6.4 MB (94.40%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -4,65 +4,65 @@ public:
     
     // using backtracking
     
-//  vector<string>v = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+ vector<string>v = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
-//     void generate(string digits,string temp,int start,vector<string> &ans)
-//     {
-//         if(temp.size()==digits.size())
-//         {
-//             ans.push_back(temp);
-//             return;
-//         }
+    void generate(string digits,string temp,int start,vector<string> &ans)
+    {
+        if(temp.size()==digits.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
         
-//         for(auto i : v[digits[start]-'0'])
-//         {
-//             temp.push_back(i);
-//             generate(digits,temp,start+1,ans);
-//             temp.pop_back();
-//         }
-//     }
+        for(auto i : v[digits[start]-'0'])
+        {
+            temp.push_back(i);
+            generate(digits,temp,start+1,ans);
+            temp.pop_back();
+        }
+    }
     
-// vector<string> letterCombinations(string digits) {
-//       vector<string> ans;
+vector<string> letterCombinations(string digits) {
+      vector<string> ans;
     
-//         if(digits.size()==0)
-//         {
-//             return ans;
-//         }
+        if(digits.size()==0)
+        {
+            return ans;
+        }
     
-//         string temp = "";
-//         generate(digits,temp,0,ans);
-//         return ans;
-//     }
-// };
+        string temp = "";
+        generate(digits,temp,0,ans);
+        return ans;
+    }
+};
     
     
     
     // using bfs----->iterative----->
     
-    vector<string> letterCombinations(string digits) {
-        vector<string> phone = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        if(digits.empty())
-            return {};
+//     vector<string> letterCombinations(string digits) {
+//         vector<string> phone = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+//         if(digits.empty())
+//             return {};
         
-        queue<string> q;
-        vector<string> ans;
+//         queue<string> q;
+//         vector<string> ans;
         
-        q.push("");
+//         q.push("");
 
-        while(!q.empty()){
-            string s = q.front();
-            q.pop();
+//         while(!q.empty()){
+//             string s = q.front();
+//             q.pop();
             
-            if(s.length() == digits.length())
-                ans.push_back(s);
-            else{
-                string temp = phone[digits[s.length()] - '0'];
-                for(auto i : temp)
-                    q.push(s + i);
-            }
-        }
-        return ans;
-    }
-};
+//             if(s.length() == digits.length())
+//                 ans.push_back(s);
+//             else{
+//                 string temp = phone[digits[s.length()] - '0'];
+//                 for(auto i : temp)
+//                     q.push(s + i);
+//             }
+//         }
+//         return ans;
+//     }
+// };
     
