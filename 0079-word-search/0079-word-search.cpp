22eae156b6170a4f249fa650c4a05d41186784b6
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-15 14:25:08 +0530

    Time: 451 ms (64.11%), Space: 8.1 MB (46.55%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,43 +3,43 @@ public:
 
 
 
-    bool dfs(vector<vector<char>>& board, string word,int x,int y,int idx){
+//     bool dfs(vector<vector<char>>& board, string word,int x,int y,int idx){
      
-        if(idx==word.size())
-            return true;            // base condition
+//         if(idx==word.size())
+//             return true;            // base condition
         
-         if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || 
-            board[x][y] == '2' || board[x][y] != word[idx]) 
-         {
-             return false;
-         }
+//          if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || 
+//             board[x][y] == '2' || board[x][y] != word[idx]) 
+//          {
+//              return false;
+//          }
          
-        char c=board[x][y];
-        board[x][y]='2';
-        bool res=  dfs(board,word, x+1, y, idx+1) ||
-                   dfs(board,word, x-1, y, idx+1) ||
-                   dfs(board,word, x, y+1, idx+1) ||
-                   dfs(board,word, x, y-1, idx+1);
-        board[x][y]=c;
+//         char c=board[x][y];
+//         board[x][y]='2';
+//         bool res=  dfs(board,word, x+1, y, idx+1) ||
+//                    dfs(board,word, x-1, y, idx+1) ||
+//                    dfs(board,word, x, y+1, idx+1) ||
+//                    dfs(board,word, x, y-1, idx+1);
+//         board[x][y]=c;
         
-        return res;
+//         return res;
         
-    }
+//     }
     
     
     
-    bool exist(vector<vector<char>>& board, string word) {
+//     bool exist(vector<vector<char>>& board, string word) {
       
-         for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                if(dfs(board,word,i,j,0)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-};
+//          for(int i=0;i<board.size();i++){
+//             for(int j=0;j<board[0].size();j++){
+//                 if(dfs(board,word,i,j,0)){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
     
     
     
@@ -91,3 +91,46 @@ public:
 //         return false;
 //     }
 // };
+    
+    
+    
+    // optimal solution----------->
+    
+    bool exist(vector<vector<char>> &board, string word) {
+       for(int i = 0 ; i < board.size() ; ++i)
+           for(int j = 0 ; j < board[0].size() ; ++j)
+               if(dfs(0 , i , j , board , word))
+                   return true;
+       return false;
+   }
+    
+    
+    
+   bool dfs(int index , int x , int y , vector<vector<char>> &board , string &word)
+   {
+       if(index == word.size()) // word exists in matrix
+           return true;
+       
+       if(x < 0 or y < 0 or x >= board.size() or y >= board[0].size() or board[x][y] == '.')                                                            // boundary check + visited check
+           return false;
+       
+       if(board[x][y] != word[index])
+           return false;
+       
+       char temp = board[x][y];
+       board[x][y] = '.';                                       // marking it visited
+       
+       // Move in 4 directions[UP , DOWN , LEFT , RIGHT]
+       if(dfs(index + 1 , x - 1 , y , board , word) or dfs(index + 1 , x + 1 , y , board , word) or dfs(index + 1 , x , y - 1 , board , word) or dfs(index + 1 , x , y + 1 , board , word))
+           return true;
+           
+       board[x][y] = temp;                                              // backtrack step
+       return false;
+       
+   }
+   
+};
+    
+    
+    
+    
