17f1e348b7127bb03f9fcabbedc71338346a3f99
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-16 22:57:22 +0530

    Time: 114 ms (87.85%), Space: 61.9 MB (27.32%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -6,10 +6,12 @@ public:
     
 
 //     int solve(TreeNode* root,int h){
-//         if(!root)return 0;
-//         if(h==1)return root->val;
-//         return solve(root->left,h-1)+solve(root->right,h-1);
-        
+//         if(!root)
+//              return 0;
+//         if(h==1)
+//              return root->val;
+    
+//         return solve(root->left,h-1)+solve(root->right,h-1);   
 //     }
     
     
@@ -26,27 +28,37 @@ public:
 // };
     
     
+    
+    
+    
+    
+    
+    
+    
     // using bfs go to last level and return its sum
     
     int deepestLeavesSum(TreeNode* root){
-        if(!root)return 0;
+        if(!root)
+            return 0;
         int sum;
         queue<TreeNode*>q;
         q.push(root);
         
         while(!q.empty()){
-            sum=0;   // initialised with 0 so that last it returns the sum when it is last level
+            sum=0; // initialised with 0 so that last it returns the sum when it is last level
             
             int size=q.size();
             
             for(int i=0;i<size;i++){
-                TreeNode* front = q.front();
+                TreeNode* t = q.front();
                 q.pop();
         
-                sum += front->val;
+                sum += t->val;
                 
-                if(front->left) q.push(front->left);
-                if(front->right) q.push(front->right);
+                if(t->left)
+                    q.push(t->left);
+                if(t->right)
+                    q.push(t->right);
                 
             }
         }
