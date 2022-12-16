7fd4d23f69bc1b4a2f84ca4f1d56c3e3c07071ac
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-16 12:01:01 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (54.50%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,37 @@
+class MyQueue {
+public:
+    stack<int>s1;
+    stack<int>s2;
+    
+    MyQueue() {
+         
+    }
+    
+    void push(int x) {
+       while(!s1.empty()){
+           s2.push(s1.top());
+           s1.pop();
+       } 
+        s2.push(x);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+    
+    int pop() {
+        int cur=s1.top();
+        s1.pop();
+        return cur;
+        
+    }
+    
+    int peek() {
+        return s1.top();
+    }
+    
+    bool empty() {
+       return s1.empty();  
+    }
+};
+
