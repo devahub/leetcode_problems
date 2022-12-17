96a1ad71e501e24b42c908b37f55d89df902165e
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-17 19:22:15 +0530

    Time: 16 ms (80.64%), Space: 12.6 MB (81.34%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,34 +1,62 @@
 class Solution {
 public:
+//     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+//         int n=groupSizes.size();
+//         vector<vector<int>>ans;
+//         unordered_map<int,vector<int>>mp;
+        
+//         for(int i=0;i<n;i++){
+//             mp[groupSizes[i]].push_back(i);
+//         }
+        
+//          for(auto j:mp){
+            
+//              int c=0;
+// 			 vector<int> arr=j.second;
+// 			 vector<int> v;
+            
+            
+// 			 for(int i=0;i<arr.size();i++){
+// 				 v.push_back(arr[i]);
+// 				 c++;
+// 				 if(c==j.first){
+// 					 ans.push_back(v);
+// 					 c=0;
+// 					 v.clear();
+// 				 }
+// 			 }
+// 		 }
+        
+        
+        
+//         return ans;
+//     }
+// };
+    
+    
+    
+    
+    
+    
+    
+    
+    // same approach------>
+    
+    
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        int n=groupSizes.size();
-        vector<vector<int>>ans;
-        unordered_map<int,vector<int>>mp;
+        vector<vector<int>> ans;
+        map<int,vector<int>> mp;
         
-        for(int i=0;i<n;i++){
+        for(int i=0;i<groupSizes.size();i++)
+        {
             mp[groupSizes[i]].push_back(i);
+            
+            if(mp[groupSizes[i]].size()==groupSizes[i])
+            {
+                ans.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]] = {};
+            }
         }
-        
-         for(auto j:mp){
-            
-             int c=0;
-			 vector<int> arr=j.second;
-			 vector<int> v;
-            
-            
-			 for(int i=0;i<arr.size();i++){
-				 v.push_back(arr[i]);
-				 c++;
-				 if(c==j.first){
-					 ans.push_back(v);
-					 c=0;
-					 v.clear();
-				 }
-			 }
-		 }
-        
-        
-        
         return ans;
     }
 };
