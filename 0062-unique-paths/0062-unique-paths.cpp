fce49c287a26a695d744d760e09a4cab52c293db
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-17 17:26:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (58.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,11 +14,12 @@ public:
         
         int down=solve(i+1,j,m,n,dp);
         int right=solve(i,j+1,m,n,dp);
+        
         return dp[i][j]=down+right;
     }
     
      int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
          return solve(0,0,m,n,dp);        
     }
 };
