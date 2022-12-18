e8d7dae927a1e71aba48bf3716d1aa4b4571f6ee
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-18 14:40:12 +0530

    Time: 1208 ms (7.51%), Space: 287 MB (7.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    // we will noramally check for every index whether the word formed is palindrome or not and find max length word later.------------>dp solution
+    
     bool ispal(string &s,int i,int j,vector<vector<int>>&dp){
        if(i>=j)
     {
@@ -16,8 +19,6 @@ public:
      return dp[i][j]= 0;
     }
 
-    
-    // we will noramally check for every index whether the word formed is palindrome or not and find max length word later.
     
     string longestPalindrome(string s) {
         int n=s.size();
@@ -40,3 +41,64 @@ public:
     return ans;    
     }
 };
+
+
+
+
+
+
+
+
+
+//  approach ---->2------->
+
+// Approach-1: Use 2 pointers approach.
+// For odd length palindromic substring, we start from same index and check for near characters
+// For even length palindromic substring, start from adjacent index and check for near characters.
+
+// Time Complexity: O(2N^2)
+// Space Complexity: O(1)
+
+
+
+// class Solution {
+// public:
+//     string longestPalindrome(string s) 
+//     {
+//         int lenMax = 0;
+//         string res;
+        
+//         for(int i=0; i<s.length(); i++)
+//         {
+//             string odd = pali_subs(s, i, i); 
+//             long_subs(odd, res, lenMax);
+//             string even = pali_subs(s, i, i+1); 
+//             long_subs(even, res, lenMax);
+//         }
+//         return res;
+//     }
+	
+// 	string pali_subs(string &s, int L, int R)
+//     {
+//         int n=s.size();
+//         while(L>=0 && R<n) 
+//         {
+//             if(s[L]!=s[R]) break;
+//             L--;
+//             R++;
+//         }
+//         return s.substr(L+1, R-1-L); 
+//     }
+    
+//     void long_subs(string &new_s, string &res, int &lenMax)
+//     {
+//         if(new_s.size()>lenMax)
+//         {
+//             res = new_s;
+//             lenMax = new_s.size();
+//         }
+//     }
+// };
+
+
+
