85622a1c83126bfd9493f1de84a7e0e9a8e8d918
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-20 19:50:13 +0530

    Time: 1352 ms (15.10%), Space: 292.4 MB (5.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -27,27 +27,33 @@ public:
     
     
     
+    
+    
         // using dp-->memoization---->
    
     
-    int solve(int ind , int prev ,vector<int>& arr,vector<vector<int>> &dp){
-        if(ind == arr.size())return 0;
+    int solve(int cur , int prev ,vector<int>& arr,vector<vector<int>> &dp){
+        if(cur == arr.size())
+            return 0;
             
-        if(dp[ind][prev+1] != -1){
-            return dp[ind][prev+1];
+        if(dp[cur][prev+1] != -1){
+            return dp[cur][prev+1];
         }
         
-        int nottake = solve(ind+1,prev,arr,dp);
-        int take =0;
-        if(prev==-1 || arr[ind]>arr[prev]){
-            take = 1+solve(ind+1,ind,arr,dp);
+        int nottake = solve(cur+1,prev,arr,dp);
+       
+        int take =0; 
+        if(prev==-1 || arr[cur]>arr[prev]){
+            take = 1+solve(cur+1,cur,arr,dp);
         }
-        return dp[ind][prev+1]=max(nottake,take);
+        return dp[cur][prev+1]=max(nottake,take);
     }
+    
     
      int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+         
         return solve(0,-1,nums,dp);
 		}
 };
