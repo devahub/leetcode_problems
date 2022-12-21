bbf30595b9ddd0a635b5f53a441dd6f36ec09746
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-21 19:47:25 +0530

    Time: 639 ms (62.87%), Space: 155.5 MB (24.12%) - LeetHub

--- 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -1,12 +1,16 @@
 
 class Solution {
 public:
+    
+  // brute force------>  
+    
 //     vector<int> solve(TreeNode *root , int q){
 //         TreeNode* p = root;
 //         int lb = INT_MIN ;
 //         int ub =  INT_MAX;
         
 //         while(p){
+            
 //             if(p->val<q){
 //                 lb=max(lb,p->val);
 //                 p=p->right;
@@ -22,10 +26,15 @@ public:
 //             }
 //         }
         
-//         if(lb == INT_MIN) lb = -1;
-//         if(ub == INT_MAX) ub = -1;
+//         if(lb == INT_MIN) 
+//             lb = -1;
+//         if(ub == INT_MAX) 
+//             ub = -1;
+        
 //         return {lb , ub};  
 //     } 
+    
+    
     
 //     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
 //         vector<vector<int>>ans(queries.size());
@@ -42,21 +51,32 @@ public:
 
 
 
+
+
 // approach---->2
 
 // using binary search and inorder 
 
- vector<int> v;
-    void printInorder(TreeNode* node)
+    
+    
+    vector<int> v;
+    void Inorder(TreeNode* node)                    /// pushing inorder in vector v
     {
         if (node == NULL)
             return;
-        printInorder(node->left);
+        Inorder(node->left);
         v.push_back(node->val);
-        printInorder(node->right);
+        Inorder(node->right);
     }
+    
+    
+    
 vector<int> binarySearch(int x, int low, int high) {
-    int mini = -1, maxi = -1;
+   
+    int mini = -1;
+    int maxi = -1;
+    
+    
   while (low <= high) {
     int mid = low + (high - low) / 2;
 
@@ -67,6 +87,7 @@ vector<int> binarySearch(int x, int low, int high) {
       low = mid + 1;
       mini = v[mid];
     }
+      
     else{
       high = mid - 1;
       maxi = v[mid];
@@ -75,8 +96,10 @@ vector<int> binarySearch(int x, int low, int high) {
 
   return {mini, maxi};
 }
+    
+    
     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& q) {
-        printInorder(root);
+        Inorder(root);
         vector<vector<int>> ans;
         for(int i=0; i<q.size(); i++){
             ans.push_back(binarySearch(q[i], 0, v.size()-1));
