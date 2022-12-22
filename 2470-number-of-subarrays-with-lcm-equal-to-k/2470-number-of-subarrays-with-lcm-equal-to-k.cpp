9f9d8f49164150ae7d5dac3778e80c740ed3407a
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-22 21:47:43 +0530

    Time: 33 ms (86.02%), Space: 9.6 MB (11.46%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -1,37 +1,50 @@
 class Solution
 {
     public:
-        long long gcd(long long int a, long long int b)
-        {
-            if (b == 0)
-                return a;
-            return gcd(b, a % b);
-        }
-
-   	// Function to return LCM of two numbers
-    long long lcm(int a, int b)
-    {
-        return (a / gcd(a, b)) *b;
-    }
-
-    int subarrayLCM(vector<int> &nums, int k)
-    {
-
-        int cnt = 0;
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int c = nums[i];
-            for (int j = i; j < nums.size(); j++)
-            {
-                c = lcm(c, nums[j]);
-
-                if (c == k)
-                {
-                    cnt++;
-                }
+    
+    
+    
+    
+//     long _lcm(long a, long b) {
+//         return (a * b) / __gcd(a, b);
+//     }
+    
+//     int subarrayLCM(vector<int>& nums, int k) {
+//         long count = 0, lcm, n = nums.size();
+        
+//         for(int i = 0; i < n; i++) {
+//             lcm = nums[i];
+//             for(int j = i; j < n; j++) {
+//                 lcm = _lcm(lcm, nums[j]);
+//                 if(lcm == k)
+//                     count++;
+//                 if(lcm > k)
+//                     break;
+//             }
+//         }
+        
+//         return count;
+//     }
+// };
+    
+    
+    
+    
+    
+    
+    
+     int subarrayLCM(vector<int>& nums, int k) {
+        int size=nums.size(),count=0;
+        for(int i=0;i<size;i++){
+        int LCM=nums[i];
+            for(int j=i;j<size;j++){
+                LCM=lcm(LCM,nums[j]);
+                if(LCM==k)
+                    count++;
+                else if(LCM>k)
+                    break;
             }
         }
-        return cnt;
+        return count;
     }
 };
