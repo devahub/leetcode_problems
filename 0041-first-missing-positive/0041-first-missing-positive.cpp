5e1b28c582e63c90ab68531b58526372bc6b78dd
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-23 22:16:01 +0530

    Time: 52 ms (88.61%), Space: 31.5 MB (47.89%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,29 +3,47 @@ public:
     
     // using extra space---->
     
-    int firstMissingPositive(vector<int>& nums) {
+//     int firstMissingPositive(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>v(n,-1);
+        
+//         for(int i=0;i<n;i++){
+//             if(nums[i]>0 && nums[i]<=n){
+//                 v[nums[i]-1]=1;
+//             }
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             if(v[i]==-1){
+//                 return i+1;
+//             }
+//         }
+//         return n+1;
+      
+//     }
+// };
+    
+    
+    
+
+    // without using space----->swapping 
+//     So bring all the elements to their respective position
+// i.e if the element is 3 bring it to 3rd position or 2nd index.
+    
+     int firstMissingPositive(vector<int>& nums) {
+         
         int n=nums.size();
-        vector<int>v(n,-1);
-        
-        for(int i=0;i<n;i++){
-            if(nums[i]>0 && nums[i]<=n){
-                v[nums[i]-1]=1;
-            }
+        for(int i=0 ; i<n ; i++)
+        {
+            while(nums[i]>0 and nums[i]<=n and nums[i]!=nums[nums[i]-1])
+                swap(nums[i],nums[nums[i]-1]);
         }
+         
         
-        for(int i=0;i<n;i++){
-            if(v[i]==-1){
+        for(int i=0 ; i<n ; i++) {
+            if(nums[i] != i+1)
                 return i+1;
-            }
         }
         return n+1;
-      
     }
 };
-    
-    
-    
-    
-    // without using space----->
-    
-    
