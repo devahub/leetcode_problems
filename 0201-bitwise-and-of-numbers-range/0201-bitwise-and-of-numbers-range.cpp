a755439ba455c2a3d5fcff29d697985522c8fe37
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-23 18:15:36 +0530

    Time: 16 ms (70.11%), Space: 6 MB (7.10%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -3,19 +3,46 @@ public:
     
     
     
-    int rangeBitwiseAnd(int left, int right) {
-        if ((left == 0) || (right == 0)) 
-              return 0;
+//     int rangeBitwiseAnd(int left, int right) {
+//         if ((left == 0) || (right == 0)) 
+//               return 0;
         
-        if ((int)log2(left) != (int)log2(right)) 
-              return 0;
+//         if ((int)log2(left) != (int)log2(right)) 
+//               return 0;
         
-        int res=left;
+//         int res=left;
         
-        for(long i=left;i<=right;i++){
-                res&=i;
+//         for(long i=left;i<=right;i++){
+//                 res&=i;
+//         }
+        
+//     return res;  
+//     }
+// };
+    
+    
+    
+    // using bit manipulatation------>
+    
+//       int rangeBitwiseAnd(int left, int right) {
+    
+//           return (right > left) ? (rangeBitwiseAnd(left/2, right/2) << 1) : left;
+    
+//       }
+// };
+
+    
+    
+    
+// another approach----->
+
+int rangeBitwiseAnd(int left, int right) {
+        int c=0;
+        while(left!=right){
+            left>>=1;
+            right>>=1;
+            c++;
         }
-        
-    return res;  
+        return left<<c;
     }
 };
