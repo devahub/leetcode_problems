95e07c69ec753bcf6e6d0dd5ddb7ca9a9db19854
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-25 01:38:37 +0530

    Time: 3 ms (97.64%), Space: 10 MB (65.46%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,16 +1,48 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        int maxi=0;
-        int pr=1;
+//     int maxProduct(vector<int>& nums) {
+//         int maxi=0;
+//         int pr=1;
         
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-                pr=(nums[i]-1)*(nums[j]-1);
-                maxi=max(maxi,pr);
-            }
-        }
+//         for(int i=0;i<nums.size()-1;i++){
+//             for(int j=i+1;j<nums.size();j++){
+//                 pr=(nums[i]-1)*(nums[j]-1);
+//                 maxi=max(maxi,pr);
+//             }
+//         }
         
-        return maxi;
-    }
+//         return maxi;
+//     }
+// };
+    
+    
+    
+    
+    
+    /// using priority queue
+    
+    
+    
+    
+      int maxProduct(vector<int>& nums) {
+            int n=nums.size();
+          priority_queue<int,vector<int>,greater<int>>pq;
+          
+          for(int i=0;i<n;i++){            // we will get largest and second largest number
+              pq.push(nums[i]);
+              if(pq.size()>2){
+                  pq.pop();
+              }
+          }
+          int pr=1;
+          while(!pq.empty()){
+              pr*=pq.top()-1;
+              pq.pop();
+          }
+          return pr;
+          
+                    
+          
+          
+      }
 };
