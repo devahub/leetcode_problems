28c3a4f8bfedcdcca2de0b2b30e339986d75d27a
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-24 14:36:19 +0530

    Time: 62 ms (97.06%), Space: 57 MB (34.52%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.cpp
+++ 0384-shuffle-an-array/0384-shuffle-an-array.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     
+    
     vector<int>original;
     int n;
+    
     Solution(vector<int>& nums) {
         original=nums;
         n=nums.size();
     }
     
+    
+    
     vector<int> reset() {
         return original;
     }
     
+    
      vector<int> shuffle() {
-        vector<int> shuffled = original;
+         
+        vector<int>shuffled=original;
 			
-			int l = n;
-			for(int i = n-1; i>=0; i--) {
-				
-				int j = rand()%l;
-				
-			
-				swap(shuffled[i], shuffled[j]);
-				l--;
-			}
+			random_shuffle(shuffled.begin(),shuffled.end());
 			return shuffled;
     }
 };
