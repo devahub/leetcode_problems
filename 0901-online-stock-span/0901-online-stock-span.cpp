182932f82a14a6886088bcbdf33aac0af5ae9e95
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-27 21:24:56 +0530

    Time: 236 ms (89.84%), Space: 84 MB (98.86%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,45 @@
+class StockSpanner {
+
+// brute force------>TLE
+
+//  vector<int>v;
+//         StockSpanner() {
+        
+//     }
+    
+//     int next(int price) {
+//         v.push_back(price);
+//         int ans=1;
+
+//         for(int i=v.size()-1;i>0;i--){
+//                 if(price>=v[i-1]){
+//                     ans++;
+//                 }
+//                 else{
+//                     break;
+//                 }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+public:
+
+stack<pair<int,int>>s;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int ans=1;
+    while(!s.empty() && s.top().first <= price ){
+            ans += s.top().second;
+            s.pop();
+        }
+        s.push({price,ans});
+        return ans;
+    }
+};
+
