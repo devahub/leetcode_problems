b3dd54d0a366dfcb061d1dafba1d135a6c6001af
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-28 15:18:17 +0530

    Time: 196 ms (74.51%), Space: 96.5 MB (67.85%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
-    bool isMonotonic(vector<int>& nums) {
-        int inc=1;
-        int dec=1;
-        int n=nums.size();
+//     bool isMonotonic(vector<int>& nums) {
+//         int inc=1;
+//         int dec=1;
+//         int n=nums.size();
         
-        for(int i=0;i<n-1;i++){
-            if(nums[i]<=nums[i+1]){
-                inc++;
-            }
-            if(nums[i]>=nums[i+1]){
-                dec++;
+//         for(int i=0;i<n-1;i++){
+//             if(nums[i]<=nums[i+1]){
+//                 inc++;
+//             }
+//             if(nums[i]>=nums[i+1]){
+//                 dec++;
+//             }
+//         }
+        
+//         if(inc==n || dec==n){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+    
+    
+    
+    
+   bool isMonotonic(vector<int>& nums) {
+        bool monoInc=true, monoDec=true;
+		
+		//checking for monotonically increasing
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i-1]> nums[i]){
+                monoInc=false;
             }
         }
-        
-        if(inc==n || dec==n){
-            return true;
+		
+		//check for monotonically decreasing
+        for(int i=1; i<nums.size();i++){
+            if(nums[i-1]<nums[i]){
+                monoDec=false;
+            }
         }
-        return false;
+        return monoInc or monoDec;
     }
 };
