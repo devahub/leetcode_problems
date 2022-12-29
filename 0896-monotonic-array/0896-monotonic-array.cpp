6678497786189e279821b0bf55d73ceff9f13992
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-29 11:46:30 +0530

    Time: 185 ms (81.33%), Space: 96.5 MB (21.91%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
-//     bool isMonotonic(vector<int>& nums) {
-//         int inc=1;
-//         int dec=1;
-//         int n=nums.size();
+    
+    bool isMonotonic(vector<int>& nums) {
+        int inc=1;
+        int dec=1;
+        int n=nums.size();
         
-//         for(int i=0;i<n-1;i++){
-//             if(nums[i]<=nums[i+1]){
-//                 inc++;
-//             }
-//             if(nums[i]>=nums[i+1]){
-//                 dec++;
-//             }
-//         }
-        
-//         if(inc==n || dec==n){
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-    
-    
-    
-    
-   bool isMonotonic(vector<int>& nums) {
-        bool monoInc=true, monoDec=true;
-		
-		//checking for monotonically increasing
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i-1]> nums[i]){
-                monoInc=false;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]<=nums[i+1]){
+                inc++;
+            }
+            if(nums[i]>=nums[i+1]){
+                dec++;
             }
         }
-		
-		//check for monotonically decreasing
-        for(int i=1; i<nums.size();i++){
-            if(nums[i-1]<nums[i]){
-                monoDec=false;
-            }
+        
+        if(inc==n || dec==n){
+            return true;
         }
-        return monoInc or monoDec;
+        return false;
     }
 };
+    
+    
+    
+    
+//    bool isMonotonic(vector<int>& nums) {
+//         if (nums.size()<=2) 
+//             return true;
+//         bool is_increasing= 1;
+//         if (nums[0] > nums.back())
+//             is_increasing=0;
+       
+//         for (int i=0; i<nums.size()-1; i++){
+//             if (is_increasing){
+//                 if (nums[i]>nums[i+1])
+//                     return false;
+//             }
+//             else{
+//                 if (nums[i]<nums[i+1])
+//                     return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
