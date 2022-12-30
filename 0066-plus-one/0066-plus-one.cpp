121503afa26c6b20692eb1fb87e8e8705981cc3e
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-30 19:30:30 +0530

    Time: 7 ms (20.95%), Space: 8.7 MB (87.43%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+    
+    
+    //  reverse traversing 
+    
     vector<int> plusOne(vector<int>& digits) {
         vector<int>v;
         for(int i=digits.size()-1;i>=0;i--)
@@ -17,3 +22,22 @@ public:
         return digits;
     }
 };
+
+//  vector<int> plusOne(vector<int>& digits) 
+//     {
+//         if (digits.back() == 9)
+//         {
+//             digits.pop_back();
+//             digits = plusOne(digits);
+//             digits.push_back(0);
+//         }
+        
+//         else if (digits.empty())
+//             digits.push_back(1);
+
+//         else
+//             digits.back()++;
+        
+//         return digits;
+//     }
+// };
