fbf5606abd9e33b2c8e6b6a023895cfb6e575246
devansh sachan <65270644+devahub@users.noreply.github.com>
2022-12-30 22:00:36 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (69.05%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -3,35 +3,35 @@ public:
     
     // using set-------> not very much optimised--->
     
-     void solve(vector<int>&nums, int i, vector<int>v, set<vector<int>>&st) 
-    {
-        if(i == nums.size()) 
-        {
-            sort(v.begin(), v.end());
-            st.insert(v);
-            return;
-        }
+//      void solve(vector<int>&nums, int i, vector<int>v, set<vector<int>>&st) 
+//     {
+//         if(i == nums.size()) 
+//         {
+//             sort(v.begin(), v.end());
+//             st.insert(v);
+//             return;
+//         }
         
-        v.push_back(nums[i]);
-        solve(nums, i + 1, v, st);
-        v.pop_back();
-        solve(nums, i + 1, v, st);
-    }
+//         v.push_back(nums[i]);
+//         solve(nums, i + 1, v, st);
+//         v.pop_back();
+//         solve(nums, i + 1, v, st);
+//     }
     
-    vector<vector<int>>subsetsWithDup(vector<int>& nums) 
-    {
-        vector<vector<int>>ans;
-        set<vector<int>>st;
-        vector<int>v;
+//     vector<vector<int>>subsetsWithDup(vector<int>& nums) 
+//     {
+//         vector<vector<int>>ans;
+//         set<vector<int>>st;
+//         vector<int>v;
         
-        solve(nums, 0, v, st);
+//         solve(nums, 0, v, st);
         
-        for(auto i : st){
-            ans.push_back(i);
-        }
-        return ans;
-    }
-};
+//         for(auto i : st){
+//             ans.push_back(i);
+//         }
+//         return ans;
+//     }
+// };
     
     
     
@@ -44,26 +44,26 @@ public:
     
    // optimal solution-----> 
     
-//     void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int>&v,int index){
+    void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int>&v,int index){
         
-//         ans.push_back(v);
-//         for(int i=index;i<nums.size();i++){
+        ans.push_back(v);
+        for(int i=index;i<nums.size();i++){
             
-//             if(i>index &&  nums[i]==nums[i-1])
-//                 continue;
-//             v.push_back(nums[i]);
-//             solve(nums,ans,v,i+1);
-//             v.pop_back();
-//         }
+            if(i>index &&  nums[i]==nums[i-1])
+                continue;
+            v.push_back(nums[i]);
+            solve(nums,ans,v,i+1);
+            v.pop_back();
+        }
        
-//     }
+    }
     
     
-//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-//         vector<vector<int>>ans;
-//         vector<int>v;
-//         sort(nums.begin(),nums.end());
-//         solve(nums,ans,v,0);
-//         return ans;
-//     }
-// };
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        solve(nums,ans,v,0);
+        return ans;
+    }
+};
