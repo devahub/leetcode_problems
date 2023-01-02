1d0ce1a37daeb40626e230bdf81baf7973027921
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-02 22:20:40 +0530

    Time: 3 ms (77.86%), Space: 6.3 MB (86.02%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -30,3 +30,59 @@ public:
         return res;
     }
 };
+
+
+
+
+
+
+
+// approach---->2
+
+
+// int add(int &curA, int &curB, int &carry)
+// {
+//         if(curA == 1 && curB == 1 && carry == 1)
+//         {
+//             return 11;
+//         }
+        
+//         // if any of combination values are 1, then return 10
+//         else if((curA == 1 && curB == 1) ||
+//                 (curA == 1 && carry == 1) ||
+//                 (curB == 1 && carry == 1))
+//         {
+//             return 10;
+//         }
+//         else if(curA || curB || carry)
+//         {
+//             return 1;
+//         }
+//         return 0;
+//     }
+    
+// public:
+//     string addBinary(string a, string b) {
+//         int lena = a.size(), lenb = b.size();
+//         int i = lena-1, j = lenb-1, sum = 0, carry = 0;
+//         string res = "";
+        
+//         while(i >= 0 || j >= 0)
+//         {
+//             int curA, curB ;
+            
+//             curA = (i >= 0) ? (a[i--] - '0'): 0;
+//             curB = (j >= 0) ? (b[j--] - '0') : 0;
+            
+//             sum = add(curA, curB, carry);
+//             res = to_string(sum % 10) + res;
+// 			carry = sum / 10;
+//         }
+        
+//         // if carry is there, add it to result
+//         if(carry)
+//             res = to_string(carry) + res;
+		
+//         return res;
+//     }
+// };
