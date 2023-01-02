c649a5036f36bdf70d13b792bbac3bef098b274a
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-02 22:25:42 +0530

    Time: 206 ms (91.81%), Space: 98.5 MB (98.03%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -24,6 +24,9 @@ public:
     
     
     
+    
+    
+    
     // using stack------>
     
     
@@ -45,3 +48,29 @@ public:
           return v;
        }
 };
+
+
+
+
+
+
+
+/// using forward iteration------>
+
+
+// vector<int> dailyTemperatures(vector<int>& t) {
+//     int n = t.size();
+//     vector<int> ans(n,0);
+//     stack<int> st;
+//     for (int i = 0; i < n; i++)
+//     {
+//         while (!st.empty() && t[i] > t[st.top()])
+//         {
+//             ans[st.top()] = i - st.top();
+//             st.pop();
+//         }
+//         st.push(i);
+//     }
+//     return ans;
+// }
+// };
