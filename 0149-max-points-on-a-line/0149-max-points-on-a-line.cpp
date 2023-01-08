86ca1efe0371b38a6b302742475dce8f9e3b98f5
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-08 20:37:44 +0530

    Time: 62 ms (61.06%), Space: 7.5 MB (81.50%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -4,7 +4,7 @@ public:
     
     // brute force---->
     
-// if A, B, and C are three points in the XY-plane, they will lie on a line, i.e., three points are collinear if and only if the slope of AB is equal to the slope of BC.
+// if A, B, and C are three points in the XY-plane, they will lie on a line, i.e., three points are collinear if and only if thej slope of AB is equal to the slope of BC.
     
     int maxPoints(vector<vector<int>>& points) {
         if(points.size()<=2) 
@@ -35,3 +35,59 @@ public:
        return ans; 
     }
 };
+    
+    
+    
+    
+    
+    // using map time complexity will get reduced to n2====>
+    
+    
+    
+//        int maxPoints(vector<vector<int>>& points) {
+        
+//         if(points.size()<=2) 
+//             return points.size(); 
+                                                   
+//         int res = 0;
+//         for(int i = 0; i < points.size(); i++)
+//         {
+//             unordered_map<double, int> mp;
+//             int duplicate = 0;
+//             double slope = 0.0;
+//             for(int j = 0; j < points.size(); j++)
+//             {
+//                 int x1 = points[i][0];
+//                 int x2 = points[j][0];
+//                 int y1 = points[i][1];
+//                 int y2 = points[j][1];    
+                
+//                 // slope = dy/dx.
+//                 int dy = y2 - y1;
+//                 int dx = x2 - x1;
+//                 if(dy == 0 && dx == 0){  
+//                     duplicate++; 
+//                     continue;
+//                 }
+                
+//                 if(dx != 0)
+//                     slope = dy*1.0/dx; // store in double
+//                 else // dx==0 means slope is infinity
+//                     slope = INT_MAX;
+                
+//                 mp[slope]++;
+//             }
+
+//             if(mp.size() == 0)
+//                 res = duplicate;
+//             else
+//             {
+//                 for(auto slope : mp){
+                    
+//                     res = max(res, slope.second + duplicate);
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
