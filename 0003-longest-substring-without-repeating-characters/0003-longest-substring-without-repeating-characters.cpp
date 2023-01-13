c223cef69bda6cdca7694ea6bd01b7da3fbcb37c
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-13 22:02:52 +0530

    Time: 15 ms (80.83%), Space: 8.3 MB (77.20%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -24,22 +24,44 @@ public:
 // };
     
     
+    //   using sliding window and hashmap
     
-     int lengthOfLongestSubstring(string s){
-        int freq[256]={0};
-         int l=0;
-         int r=0;
-         int ans=0;
+//      int lengthOfLongestSubstring(string s){
+//         int freq[256]={0};
+//          int l=0;
+//          int r=0;
+//          int ans=0;
          
-         while(r<s.size()){
-             freq[s[r]]++;
-             while(freq[s[r]]>1){
-                 freq[s[l]]--;
-                 l++;
-             }
-             ans=max(ans,r-l+1);
-             r++;
-         }
-         return ans;
-     }
+//          while(r<s.size()){
+//              freq[s[r]]++;
+//              while(freq[s[r]]>1){
+//                  freq[s[l]]--;
+//                  l++;
+//              }
+//              ans=max(ans,r-l+1);
+//              r++;
+//          }
+//          return ans;
+//      }
+// };
+    
+    
+    
+    
+    
+      int lengthOfLongestSubstring(string s){
+          unordered_map<char,int>mp;
+          int res=0;
+          int start=0;
+          
+          for(int i=0;i<s.size();i++){
+                if(mp.find(s[i])!=mp.end() && mp[s[i]] >=start)
+                {
+                    start=mp[s[i]]+1;
+                }
+            mp[s[i]]=i;
+           res=max(res,i-start+1);   
+          }
+         return res;
+      }
 };
