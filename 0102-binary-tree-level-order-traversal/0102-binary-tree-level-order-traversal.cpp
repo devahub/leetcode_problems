040cc982e7a556cc5194cc9dfe708ec2771b4f22
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-16 17:43:10 +0530

    Time: 8 ms (39.11%), Space: 12.7 MB (33.34%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,35 +1,37 @@
 
 class Solution {
 public:
-//         if(root==NULL)
-//             return {};
+    
+vector<vector<int>> levelOrder(TreeNode* root) {
+        if(root==NULL)
+            return {};
         
-//         vector<vector<int>>res;
-//         queue<TreeNode*>q;
-//         q.push(root);
-//         TreeNode* temp;
+        vector<vector<int>>res;
+        queue<TreeNode*>q;
+        q.push(root);
+        TreeNode* temp;
         
-//         while(!q.empty()){
+        while(!q.empty()){
             
-//             vector<int>v;
-//             int l=q.size();
+            vector<int>v;
+            int l=q.size();
             
-//             for(int i=0;i<l;i++){
-//                 temp=q.front();
-//                 q.pop();
-//                 v.push_back(temp->val);
+            for(int i=0;i<l;i++){
+                temp=q.front();
+                q.pop();
+                v.push_back(temp->val);
                 
-//                 if(temp->left!=NULL)
-//                     q.push(temp->left);
+                if(temp->left!=NULL)
+                    q.push(temp->left);
                 
-//                 if(temp->right!=NULL)
-//                     q.push(temp->right);
-//             }
-//             res.push_back(v);          
-//             }
-//         return res;
-//          }
-// };
+                if(temp->right!=NULL)
+                    q.push(temp->right);
+            }
+            res.push_back(v);          
+            }
+        return res;
+         }
+};
         
         
     
@@ -39,32 +41,32 @@ public:
     
         // recurssive soln
         
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root==NULL)
-            return ans;
-        queue<TreeNode*>q;
-        q.push(root);
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>>ans;
+//         if(root==NULL)
+//             return ans;
+//         queue<TreeNode*>q;
+//         q.push(root);
         
-        while(!q.empty()){
-            int size=q.size();
-            vector<int>v;
-            for(int i=0;i<size;i++){
-                TreeNode *node=q.front();
-                q.pop();
-            if(node->left!=NULL)
-                q.push(node->left);
-            if(node->right!=NULL)
-                q.push(node->right); 
+//         while(!q.empty()){
+//             int size=q.size();
+//             vector<int>v;
+//             for(int i=0;i<size;i++){
+//                 TreeNode *node=q.front();
+//                 q.pop();
+//             if(node->left!=NULL)
+//                 q.push(node->left);
+//             if(node->right!=NULL)
+//                 q.push(node->right); 
                 
-            v.push_back(node->val);
-        }
-            ans.push_back(v);
-        }
-        return ans;
+//             v.push_back(node->val);
+//         }
+//             ans.push_back(v);
+//         }
+//         return ans;
         
-    }
-};
+//     }
+// };
         
         
         
