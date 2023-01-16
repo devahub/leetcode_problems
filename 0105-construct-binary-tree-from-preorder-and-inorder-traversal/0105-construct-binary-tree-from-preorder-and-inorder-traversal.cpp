fa7a91b1eb15762ba5ee53a57eb5249c54f39977
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-16 17:57:03 +0530

    Time: 29 ms (62.50%), Space: 26 MB (80.53%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,9 +1,14 @@
 
 class Solution {
 public:
+    
+    
+    // first find the root from preorder and check where root is located at inorder traversal and now start bulding tree because left side nodes from root in inorder will be part of left of tree;
+    
       TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
         return solve(pre,in,0,in.size()-1);
-    }
+          }
+    
     
     int c=0;
     TreeNode* solve(vector<int>& pre, vector<int>& in,int start,int end)
@@ -14,12 +19,13 @@ public:
         
         TreeNode* x=new TreeNode(pre[c++]);
         int d=0;
-        for(int i=start;i<=end;i++){
+        for(int i=start;i<=end;i++){            // find root in inorder
             if(x->val==in[i]){
                 d=i;
                 break;
             }
         }
+        
         x->left=solve(pre,in,start,d-1);
         x->right=solve(pre,in,d+1,end);
         return x;
