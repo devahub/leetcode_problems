6859ab498393de581c1150a81e2ff598c65fd5f1
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-21 01:09:25 +0530

    Time: 34 ms (66.51%), Space: 27.4 MB (66.01%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
-    vector<int> addToArrayForm(vector<int>& num, int k) {
-        vector<int>ans;
-        int sum=0;
-        int n=num.size()-1;
+//     vector<int> addToArrayForm(vector<int>& num, int k) {
+//         vector<int>ans;
+//         int sum=0;
+//         int n=num.size()-1;
             
-        while(n>=0 || k!=0 || sum>0){
-            if(k!=0){
-                sum+=k%10;
-                k=k/10;
+//         while(n>=0 || k!=0 || sum>0){
+//             if(k!=0){
+//                 sum+=k%10;
+//                 k=k/10;
+//             }
+//             if(n>=0){
+//                 sum+=num[n];
+//                 n--;
+//             }
+//             ans.push_back(sum%10);
+//             sum=sum/10;
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
+    
+    
+    
+    
+    
+    ///  another approach------>
+    
+    
+     vector<int> addToArrayForm(vector<int> num, int K) {
+        
+         for(int i=num.size()-1;i>=0&&K>0;i--)
+            {
+            num[i]+=K;
+            K=num[i]/10;
+            num[i]%=10;
             }
-            if(n>=0){
-                sum+=num[n];
-                n--;
-            }
-            ans.push_back(sum%10);
-            sum=sum/10;
+     
+         while(K>0)             ///  if any k is leaving then we will process it to beginning
+            {
+         num.insert(num.begin(),K%10);
+         K/=10;
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+         
+     return num;
     }
 };
