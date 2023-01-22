a4dc72c65d89fb17441c13d77def29a1c981d419
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-22 23:19:04 +0530

    Time: 60 ms (43.97%), Space: 12.7 MB (72.35%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,19 +1,71 @@
 class Solution {
 public:
-   bool repeatedSubstringPattern(string s) {
-        int n = s.size();
-        string s1 = "", s2 = "";
-        for(int i=1; i<=n/2; i++) {
-            if (n%i == 0) {
-                s1 = s.substr(0, i);
-                s2 = "";
-                for(int j=0; j<n/i; j++) {
-                    s2 += s1;
-                }
+    
+    
+    ///We need to check substrings with length that is divisible by n, then only it is possible to find a repeating substring.
+    // For example, if n=10, we can have repeating substrings of length - 1,2,5 only.
+
+    
+    
+    
+//    bool repeatedSubstringPattern(string s) {
+//         int n = s.size();
+//         string s1 = "", s2 = "";
+       
+//         for(int i=1; i<=n/2; i++) {
+//             if (n%i == 0) {
+//                 s1 = s.substr(0, i);
+//                 s2 = "";
+//                 for(int j=0; j<n/i; j++) {
+//                     s2 += s1;
+//                 }
                 
-                if(s==s2) return true;
-            }
-        }
+//                 if(s==s2) return true;
+//             }
+//         }
+// 		return false;
+//     }
+// };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ///   approach---->2
+    
+    
+    bool repeatedSubstringPattern(string s) {
+	int n = s.size();
+
+	if(n==1) return false;
+
+	vector<int> lps(n, 0);
+
+	int j=0;
+	for(int i=1; i<n; i++) {
+		if (s[i]==s[j]) {
+			lps[i] = j+1;
+			j++;
+		} else {
+			while(j>0) {
+				j = (j>0) ? lps[j-1] : 0;
+				if (s[i]==s[j]) {
+					lps[i] = j+1;
+					j++;
+					break;
+				}
+				if (j==0) lps[i]=0;
+			}
+		}
+	}
+	if (lps[n-1]>0 && n%(n-lps[n-1]) == 0 ) {
+		return true;
+	} else {
 		return false;
-    }
+	}
+}
 };
