992a39477dc9450c0e04baac2a9f54385b0da771
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-22 21:09:37 +0530

    Time: 15 ms (67.77%), Space: 21 MB (59.79%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -5,24 +5,59 @@ public:
 // A balanced binary tree, also referred to as a height-balanced binary tree, is defined as a binary tree in which the height of the left and right subtree of any node differ by not more than 1.
     
     
-    bool ans;
     
-    int solve(TreeNode* root){
-        if(!root)
-            return 0;
+    //// appraoch----->1
+    
+//     bool ans;
+    
+//     int solve(TreeNode* root){
+//         if(!root)
+//             return 0;
         
-        int left=solve(root->left);
-        int right=solve(root->right);
+//         int left=solve(root->left);
+//         int right=solve(root->right);
         
-        if(abs(left-right)>1)
-            ans=false;
+//         if(abs(left-right)>1)
+//             ans=false;
         
-        return max(left,right)+1;
+//         return max(left,right)+1;
+//     }
+    
+//     bool isBalanced(TreeNode* root) {
+//         ans=true;
+//         solve(root);
+//         return ans;
+//     }
+// };
+    
+    
+    
+    
+    
+    
+    ///// approach-->2
+    
+    int height(TreeNode* root){
+        if(root==NULL)return 0;
+        
+        int lefth=height(root->left);
+        int righth=height(root->right);
+        
+        if(lefth==-1 || righth==-1 || abs(lefth-righth)>1){
+            return -1;
+        }
+        
+        return max(lefth,righth)+1;
     }
     
-    bool isBalanced(TreeNode* root) {
-        ans=true;
-        solve(root);
-        return ans;
-    }
+    
+    
+     bool isBalanced(TreeNode* root){
+    
+         if(root==NULL)return true;
+         
+         if(height(root)==-1)return false;
+         
+         return true;
+     }
 };
