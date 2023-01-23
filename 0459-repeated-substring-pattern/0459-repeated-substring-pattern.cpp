986de51e0c5de6145278b58f77ec4de98411a70c
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-23 14:50:24 +0530

    Time: 1474 ms (7.75%), Space: 693.9 MB (5.05%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -13,6 +13,8 @@ public:
 //         string s1 = "", s2 = "";
        
 //         for(int i=1; i<=n/2; i++) {
+            
+            
 //             if (n%i == 0) {
 //                 s1 = s.substr(0, i);
 //                 s2 = "";
@@ -38,34 +40,56 @@ public:
     ///   approach---->2
     
     
-    bool repeatedSubstringPattern(string s) {
-	int n = s.size();
+//     bool repeatedSubstringPattern(string s) {
+// 	int n = s.size();
 
-	if(n==1) return false;
+// 	if(n==1) return false;
 
-	vector<int> lps(n, 0);
+// 	vector<int> lps(n, 0);
 
-	int j=0;
-	for(int i=1; i<n; i++) {
-		if (s[i]==s[j]) {
-			lps[i] = j+1;
-			j++;
-		} else {
-			while(j>0) {
-				j = (j>0) ? lps[j-1] : 0;
-				if (s[i]==s[j]) {
-					lps[i] = j+1;
-					j++;
-					break;
-				}
-				if (j==0) lps[i]=0;
-			}
-		}
-	}
-	if (lps[n-1]>0 && n%(n-lps[n-1]) == 0 ) {
-		return true;
-	} else {
-		return false;
-	}
-}
+// 	int j=0;
+// 	for(int i=1; i<n; i++) {
+// 		if (s[i]==s[j]) {
+// 			lps[i] = j+1;
+// 			j++;
+// 		} else {
+// 			while(j>0) {
+// 				j = (j>0) ? lps[j-1] : 0;
+// 				if (s[i]==s[j]) {
+// 					lps[i] = j+1;
+// 					j++;
+// 					break;
+// 				}
+// 				if (j==0) lps[i]=0;
+// 			}
+// 		}
+// 	}
+// 	if (lps[n-1]>0 && n%(n-lps[n-1]) == 0 ) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+// };
+    
+    
+    
+    
+    
+    
+    
+    ////  approach----->3
+    
+    
+    
+     bool repeatedSubstringPattern(string s) {
+        int n=s.size();
+        string comb=s+s;
+         
+        for(int i=1;i<n;i++){
+            string str=comb.substr(i,n);
+            if(str==s) return true;
+        }
+        return false;
+    }
 };
