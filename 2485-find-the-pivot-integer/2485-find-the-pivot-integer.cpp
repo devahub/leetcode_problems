017f5a67f69cee631900265c7d7f5638f5cebec9
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-23 21:51:09 +0530

    Time: 8 ms (25.25%), Space: 6 MB (14.81%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+         int total=0;
+        for(int i=1;i<=n;i++){
+            total+=i;
+        }
+        // cout<<total<<endl;
+        
+        
+        int sum=0,j;
+        for(j=1;j<=n;j++){
+           sum+=j;
+            if(sum==total-sum+j){
+                return j;
+            }
+        }
+        return -1;
+    }
+};
