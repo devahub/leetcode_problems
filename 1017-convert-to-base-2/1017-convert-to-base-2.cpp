840fe66ac2bbff530be72d168ff4317ec614d1d1
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-25 00:47:00 +0530

    Time: 5 ms (8.43%), Space: 6.1 MB (55.06%) - LeetHub

--- 1017-convert-to-base-2/1017-convert-to-base-2.cpp
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
-    string baseNeg2(int n) {
-        string ans;
-        if(n==0)return "0";
+//     string baseNeg2(int n) {
+//         string ans;
+//         if(n==0)return "0";
         
-        while(n!=0){
-            int temp=n%(-2);
-            n=n/(-2);
+//         while(n!=0){
+//             int temp=n%(-2);
+//             n=n/(-2);
             
-            if(temp<0){
-               temp+=2;
-                n++;
-            }
-         ans.insert(ans.begin(),temp+'0');   
+//             if(temp<0){
+//                temp+=2;
+//                 n++;
+//             }
+//          ans.insert(ans.begin(),temp+'0');   
+//         }
+//         return ans;
+//     }
+// };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     string baseNeg2(int n) {
+        string ans="";
+        if(n==0)
+            ans+=('0');
+        int r;
+        while(n){
+            r=n%(-2);
+            if(r<0){
+                r+=2;
+                n=n/(-2)+1;
+            }else
+                n/=(-2);
+            ans+=(r+'0');
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
