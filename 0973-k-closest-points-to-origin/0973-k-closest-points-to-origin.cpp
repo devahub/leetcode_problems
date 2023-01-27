a0cfc8e0252a6293c0b02296ec8a745f163e06f4
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-27 17:42:32 +0530

    Time: 222 ms (84.48%), Space: 63.2 MB (76.68%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -3,15 +3,20 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
      
         vector<vector<int>>ans;
-        if(points.size()==0)return ans;
+        
+        if(points.size()==0)
+            return ans;
         
         priority_queue<pair<int,pair<int,int>>>pq;
         
         for(int i=0;i<points.size();i++){
-             int d=points[i][0]*points[i][0]+points[i][1]*points[i][1];
-           if(pq.size()<k){
+             
+           int d=points[i][0]*points[i][0]+points[i][1]*points[i][1];
+           
+            if(pq.size()<k){
             pq.push({d,{points[i][0],points[i][1]}});
            }
+            
             else{
                 if(d<pq.top().first)
                 {
@@ -24,10 +29,25 @@ public:
                  ans.push_back({pq.top().second.first,pq.top().second.second});
             pq.pop();
          }
-           return ans;     
-        
-        
-        
-        
+           return ans;       
     }
 };
+
+
+
+
+
+///     approach---->2
+
+//   vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+//         vector<vector<int>> answer;
+//         vector<pair<double,vector<int>>> distances;
+//         for(auto i:points){
+//             double distance=sqrt(pow(i[0],2)+pow(i[1],2));
+//             distances.push_back(make_pair(distance,i));
+//         }
+//         sort(distances.begin(),distances.end());
+//         for(int i=0;i<K;i++)answer.push_back(distances[i].second);
+//         return answer;
+//     }
+// };
