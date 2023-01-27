976c0ff7b092d3c8102b742e1bcabc18c6274fc4
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-27 17:02:11 +0530

    Time: 39 ms (45.23%), Space: 28.5 MB (40.86%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-//     vector<int> addToArrayForm(vector<int>& num, int k) {
-//         vector<int>ans;
-//         int sum=0;
-//         int n=num.size()-1;
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int>ans;
+        int sum=0;
+        int n=num.size()-1;
             
-//         while(n>=0 || k!=0 || sum>0){
-//             if(k!=0){
-//                 sum+=k%10;
-//                 k=k/10;
-//             }
-//             if(n>=0){
-//                 sum+=num[n];
-//                 n--;
-//             }
-//             ans.push_back(sum%10);
-//             sum=sum/10;
-//         }
-//         reverse(ans.begin(),ans.end());
-//         return ans;
-//     }
-// };
+        while(n>=0 || k!=0 || sum>0){
+            if(k!=0){
+                sum+=k%10;
+                k=k/10;
+            }
+            if(n>=0){
+                sum+=num[n];
+                n--;
+            }
+            ans.push_back(sum%10);
+            sum=sum/10;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
     
     
     
@@ -29,21 +29,21 @@ public:
     ///  another approach------>
     
     
-     vector<int> addToArrayForm(vector<int> num, int K) {
+//      vector<int> addToArrayForm(vector<int> num, int K) {
         
-         for(int i=num.size()-1;i>=0&&K>0;i--)
-            {
-            num[i]+=K;
-            K=num[i]/10;
-            num[i]%=10;
-            }
+//          for(int i=num.size()-1;i>=0&&K>0;i--)
+//             {
+//             num[i]+=K;
+//             K=num[i]/10;
+//             num[i]%=10;
+//             }
      
-         while(K>0)             ///  if any k is leaving then we will process it to beginning
-            {
-         num.insert(num.begin(),K%10);
-         K/=10;
-        }
+//          while(K>0)             ///  if any k is leaving then we will process it to beginning
+//             {
+//          num.insert(num.begin(),K%10);
+//          K/=10;
+//         }
          
-     return num;
-    }
-};
+//      return num;
+//     }
+// };
