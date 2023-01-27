03ede3ca87b43f14e96c9e18b5aa7727bf490f34
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-27 17:23:00 +0530

    Time: 3 ms (91.48%), Space: 11.3 MB (37.97%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,26 +1,34 @@
-class Solution {
-public:
-    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
-      int n=mat.size();
-   if(mat==target){
-       return true;
-   }
-  int c=3;
-  
-    while(c--){
-     for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            swap(mat[i][j],mat[j][i]);
-        }
-      }
-      for(int i=0;i<n;i++){
-         reverse(mat[i].begin(),mat[i].end());
-        }
-       if(mat==target){
-       return true;
-         }
-    }
+class Solution
+{
+    public:
+        bool findRotation(vector<vector < int>> &mat, vector< vector< int>> &target)
+        {
+            int n = mat.size();
+            if (mat == target)
+            {
+                return true;
+            }
+            int c = 3;
 
-  return false;
-    }
+            while (c--)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i; j < n; j++)
+                    {
+                        swap(mat[i][j], mat[j][i]);
+                    }
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    reverse(mat[i].begin(), mat[i].end());
+                }
+                if (mat == target)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 };
