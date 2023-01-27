725f02a51f2df26b8537dc99ebf8992e582e0008
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-01-27 17:24:15 +0530

    Time: 7 ms (58.46%), Space: 11.1 MB (92.75%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,34 +1,62 @@
 class Solution
 {
     public:
-        bool findRotation(vector<vector < int>> &mat, vector< vector< int>> &target)
-        {
-            int n = mat.size();
-            if (mat == target)
-            {
-                return true;
-            }
-            int c = 3;
+//         bool findRotation(vector<vector < int>> &mat, vector< vector< int>> &target)
+//         {
+//             int n = mat.size();
+//             if (mat == target)
+//             {
+//                 return true;
+//             }
+//             int c = 3;
 
-            while (c--)
-            {
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = i; j < n; j++)
-                    {
-                        swap(mat[i][j], mat[j][i]);
-                    }
-                }
-                for (int i = 0; i < n; i++)
-                {
-                    reverse(mat[i].begin(), mat[i].end());
-                }
-                if (mat == target)
-                {
-                    return true;
+//             while (c--)
+//             {
+//                 for (int i = 0; i < n; i++)
+//                 {
+//                     for (int j = i; j < n; j++)
+//                     {
+//                         swap(mat[i][j], mat[j][i]);
+//                     }
+//                 }
+//                 for (int i = 0; i < n; i++)
+//                 {
+//                     reverse(mat[i].begin(), mat[i].end());
+//                 }
+//                 if (mat == target)
+//                 {
+//                     return true;
+//                 }
+//             }
+
+//             return false;
+//         }
+// };
+
+    
+    
+    
+    
+    
+    ///   approach----->2
+    
+    
+     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        if(mat==target) return true;
+        int n=mat.size();
+        int cnt=3;
+        while(cnt--){
+            for (int i = 0; i < n / 2; i++) {
+                for (int j = i; j < n - i - 1; j++) {
+                    int temp = mat[i][j];
+                    mat[i][j] = mat[n - 1 - j][i];
+                    mat[n - 1 - j][i] = mat[n - 1 - i][n - 1 - j];
+                    mat[n - 1 - i][n - 1 - j] = mat[j][n - 1 - i];
+                    mat[j][n - 1 - i] = temp;
                 }
             }
-
-            return false;
+            if(mat==target) return true;
         }
+        return false;
+    }
 };
