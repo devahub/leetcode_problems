2a40e1d327ce2dfe11f9333035191629e0f16830
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-02-03 15:59:55 +0530

    Time: 16 ms (54.72%), Space: 10.7 MB (41.51%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,65 +1,6 @@
-// When we convert the string into ZigZag, we can divide each group of characters into periods, with size of 
-//        2 * numRows - 2
-// You can see that the first and last row has increment of periodSize = 6,
-// The second row has two increments, which is alternating between 4 and 2.
-// The third row also has two increments, which is alternating between 2 and 4.
-
-// We can draw the following conclusion: there are two increments for each row i, such that
-
-// int inc1 = periodSize - (i * 2);
-// int inc2 = periodSize - inc1;
-
-
 class Solution {
 public:
-//     string convert(string s, int num) {
-        
-//         if(num==1){
-//             return s;
-//         }
-        
-    
-//         string res;
-//         int periodSize=2*num-2;
-        
-//         for(int i=0;i<num;i++){
-//             int inc1 = periodSize - (i * 2);
-// 		    int inc2 = periodSize - inc1;
-// 		    int j = i;
-            
-//             while(j < s.length()) {
-// 			if (inc1 != 0 && j < s.length()) { 
-// 				res += s[j];
-// 				j += inc1;
-// 			}
-
-// 			if (inc2 != 0 && j < s.length()) { 
-// 				res += s[j];
-// 				j += inc2;
-// 			}   
-// 		}
-// 	}
-
-// 	return res;
-        
-//     }
-// };
-
-
-
-
-
-
-
-
-
-
-
-////approach--->2
-
-
-
-/*n=numRows
+  /*n=numRows
 
 Δ=2n-2    1                           2n-1                         4n-3
 Δ=        2                     2n-2  2n                    4n-4   4n-2
@@ -81,11 +22,11 @@ public:
     
         while(i<n){
             
-            for(int j =0; j<numRows && i<n; j++ ){
+            for(int j =0; j<numRows && i<n; j++ ){    /// horizontal moves
                 v[j].push_back(s[i++]);
             }
             
-            for(int j =numRows-2; j>=1 && i<n; j--){
+            for(int j =numRows-2; j>=1 && i<n; j--){            //diagonal moves
                 v[j].push_back(s[i++]);
             }
         }
@@ -98,9 +39,3 @@ public:
         return res;
     }
 };
-
-
-
-
-
-
