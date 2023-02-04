49b69fb68cd6c33b33e064d4731928b8ed7bd919
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-02-04 21:17:22 +0530

    Time: 10 ms (61.66%), Space: 7.4 MB (32.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,41 +1,7 @@
-class Solution
-{
-    public:
-       	//     bool checkInclusion(string s1, string s2) {
-       	//         if(s1.size() > s2.size())
-       	//             return false;
-
-       	//         vector < int>count1(26,0), count2(26,0);
-
-       	//         int i;
-       	//         for(i = 0; i < s1.size(); i++) {
-       	//            	// compute the frequency of each char for the first window
-       	//             count1[s1[i] - 'a']++;
-       	//             count2[s2[i] - 'a']++;
-       	//         }
-
-       	//         if(count1 == count2) return true;
-
-       	//        	// search the remaining windows
-       	//         int start = 0, end = i;
-       	//         while(end < s2.size()){
-       	//        	//remove char at 'start' index and add char at 'end' index
-       	//             --count2[s2[start] - 'a'];
-       	//             ++count2[s2[end] - 'a'];
-
-       	//             if(count1 == count2) return true;
-       	//             end++; start++;
-       	//         }
-       	//         return false;
-
-       	//     }
-       	// };
-
-       	// using map-----> same thing
-
-        bool checkInclusion(string s1, string s2)
-        {
-            int n1 = s1.size();
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+           int n1 = s1.size();
             int n2 = s2.size();
 
             if (n1 > n2) return false;
@@ -47,6 +13,7 @@ class Solution
                 mp[i]++;
             }
 
+        
             int i = 0;
             int j = 0;
             int c = mp.size();
@@ -86,5 +53,5 @@ class Solution
                 }
             }
             return false;
-        }
+    }
 };
