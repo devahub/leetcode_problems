44465662186df2b296b8ab005f3aa24f46798954
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-02-25 13:03:36 +0530

    Time: 155 ms (29.63%), Space: 94.8 MB (6.60%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -24,16 +24,41 @@ public:
     //   [7,1,5,3,6,4] 
     
     
-      int maxProfit(vector<int>& prices){
-          int maxi=0;
-          int mini=prices[0];
+//       int maxProfit(vector<int>& prices){
+//           int maxi=0;
+//           int mini=prices[0];
           
-          for(int i=1;i<prices.size();i++){
-              mini=min(mini,prices[i]);
-              maxi=max(maxi,prices[i]-mini);
+//           for(int i=1;i<prices.size();i++){
+//               mini=min(mini,prices[i]);
+//               maxi=max(maxi,prices[i]-mini);
               
-          }
-                return maxi;
+//           }
+//                 return maxi;
          
-      }
+//       }
+// };
+    
+    
+    
+    
+    
+    
+    ////  using stack----->
+    
+    
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        stack<int>st;
+        for(int i = 0 ; i < prices.size() ; i++){
+            if(st.empty()) st.push(prices[i]);
+            else{
+                if(prices[i] > st.top()) ans = max(ans,prices[i]-st.top());
+                else{
+                    while(!st.empty() and st.top()>prices[i])st.pop();
+                    st.push(prices[i]);
+                }
+            }
+        }
+        return ans;
+    }
 };
