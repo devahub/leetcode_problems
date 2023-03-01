33de0122bd770d4041d351a070cd2d893b2bc470
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-03-01 23:44:19 +0530

    Time: 204 ms (59.24%), Space: 66 MB (38.21%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,39 +1,21 @@
-class Solution
-{
-    public:
-
-       	// using merge sort---->
-
-        void merge(vector<int> &nums, int low, int mid, int high)
-        {
-            if (low >= high) return;
-            int l = low, r = mid + 1, k = 0, size = high - low + 1;
-            vector<int> sorted(size, 0);
-            
-            while (l <= mid and r <= high)
-                sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
-            while (l <= mid)
-                sorted[k++] = nums[l++];
-            while (r <= high)
-                sorted[k++] = nums[r++];
-            for (k = 0; k < size; k++)
-                nums[k + low] = sorted[k];
+class Solution {
+public:
+    
+    
+    
+    // There is an obvious way to do sorting with priority queues: Take the items that you want to sort, and insert them into the priority queue (using the item itself as its own priority). Then remove items from the priority queue until it is empty. The items will come off the queue in order from largest to smallest.
+    
+    vector<int> sortArray(vector<int>& nums) {
+     priority_queue<int,vector<int>,greater<int>>pq;
+        
+        for(auto i:nums){
+            pq.push(i);
         }
-
-    void mergeSort(vector<int> &nums, int l, int r)
-    {
-        if (l >= r)	//remember to put the equal to sign
-            return;
-
-        int mid = l + (r - l) / 2;
-        mergeSort(nums, l, mid);
-        mergeSort(nums, mid + 1, r);
-        merge(nums, l, mid, r);
-    }
-
-    vector<int> sortArray(vector<int> &nums)
-    {
-        mergeSort(nums, 0, nums.size() - 1);
+        
+        for(int i=0;i<nums.size();i++){
+            nums[i]=pq.top();
+            pq.pop();
+        }
         return nums;
     }
 };
