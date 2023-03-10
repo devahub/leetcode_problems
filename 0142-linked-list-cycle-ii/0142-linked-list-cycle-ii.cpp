0ed634e3636cceb875cd7d33fe03cb0d9940abd5
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-03-10 23:40:25 +0530

    Time: 4 ms (95.65%), Space: 7.5 MB (84.99%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,47 +8,50 @@
  */
 class Solution {
 public:
-  //    ListNode *detectCycle(ListNode *head) {
-//         
-
-//         if(head==NULL ||  head->next==NULL)
-//             return NULL;
+     ListNode *detectCycle(ListNode *head) {
         
-//         while(fast!=NULL && fast->next!=NULL){
-//             slow=slow->next;
-//             fast=fast->next->next;
+        ListNode* slow=head;
+         ListNode* fast=head;
+        if(head==NULL ||  head->next==NULL)
+            return NULL;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
             
-//             if(slow==fast)
-//                 break;                  // cycle found
-//         }
+            if(slow==fast)
+                break;                  // cycle found
+        }
         
-//         if(slow!=fast)
-//             return NULL;                    // cycle not found
+        if(slow!=fast)
+            return NULL;                    // cycle not found
         
-// // Alternatively, we know now for a fact that there is a cycle, so we reset turtle to be head (and notice we do NOT touch hare), run again our race and finally return the value of turtle, which will also reveal the beginning of the cycle!
-//         slow=head;
-//         while(slow!=fast){                      
-//             slow=slow->next;
-//             fast=fast->next;
+// Alternatively, we know now for a fact that there is a cycle, so we reset turtle to be head (and notice we do NOT touch hare), run again our race and finally return the value of turtle, which will also reveal the beginning of the cycle!
+        slow=head;
+        while(slow!=fast){                      
+            slow=slow->next;
+            fast=fast->next;
             
-//         }
-//         return slow;
-//     }
-// };
+        }
+        return slow;
+    }
+};
     
     
     // using hashmap--->
-     ListNode *detectCycle(ListNode *head){
-        unordered_map<ListNode*,bool>mp;
+
+//      ListNode *detectCycle(ListNode *head){
+
+//         unordered_map<ListNode*,bool>mp;
          
-         while(head!=NULL){             // if the number is already marked true return that number
-             if(mp[head]==true){           
-                 return head;
-             }
+//          while(head!=NULL){     // if the number is already marked true return that number
+//              if(mp[head]==true){           
+//                  return head;
+//              }
              
-             mp[head]=true;             // if not then mark true
-             head=head->next;
-         }
-         return NULL;
-     }
-};
+//              mp[head]=true;             // if not then mark true
+//              head=head->next;
+//          }
+//          return NULL;
+//      }
+// };
