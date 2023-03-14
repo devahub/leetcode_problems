b43a4b43b5edbad50d76271d1ae7f50e687a9b74
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-03-14 23:15:19 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (12.22%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -12,29 +12,63 @@
 class Solution {
 public:
     
-    void solve(TreeNode* root,int temp,int &sum){
-        if(root==NULL){
+//     void solve(TreeNode* root,int temp,int &sum){
+//         if(root==NULL){
+//             return;
+//         }
+        
+//          temp=temp*10+root->val;
+        
+//         if(root->left==NULL && root->right==NULL){
+//             sum+=temp;
+//         }
+        
+//         solve(root->left,temp,sum);
+//         solve(root->right,temp,sum);
+        
+        
+//     }
+    
+    
+//     int sumNumbers(TreeNode* root) {
+//         if(root==NULL)return 0;
+        
+//         int sum=0;
+//         solve(root,0,sum);
+//         return sum;
+//     }
+// };
+    
+    
+    
+    
+    
+    
+    
+    
+    vector<string> answer;
+    void preorder(TreeNode* root, string str){
+        if(root == NULL){
             return;
         }
-        
-         temp=temp*10+root->val;
-        
-        if(root->left==NULL && root->right==NULL){
-            sum+=temp;
+        if(root -> left == NULL and root -> right == NULL){
+            str = str+to_string(root -> val);
+            answer.push_back(str);
+            return;
         }
-        
-        solve(root->left,temp,sum);
-        solve(root->right,temp,sum);
-        
-        
+        str = str+to_string(root -> val);
+        preorder(root -> left,str);
+        preorder(root -> right,str);
     }
-    
-    
     int sumNumbers(TreeNode* root) {
-        if(root==NULL)return 0;
-        
-        int sum=0;
-        solve(root,0,sum);
+        string str;
+        preorder(root,str);
+        int sum = 0;
+        for(int i = 0 ; i < answer.size(); i++){
+            // reverse(answer[i].begin(), answer[i].end());
+            int x = stoi(answer[i]);
+            sum+=x;
+        }
         return sum;
     }
 };
