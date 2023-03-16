2f7737e6adc7df14b53433d1b8a162d324cc46de
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-03-17 00:38:36 +0530

    Time: 28 ms (36.75%), Space: 16.4 MB (23.34%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,36 +1,70 @@
-class MinStack {
-public:
-      vector<int>v;
+// class MinStack {
+// public:
+//       vector<int>v;
     
-    MinStack() {
-        v.clear();
-    }
+//     MinStack() {
+//         v.clear();
+//     }
 
     
   
     
-    void push(int val) {
-        v.push_back(val);
+//     void push(int val) {
+//         v.push_back(val);
+//     }
+    
+//     void pop() {
+//         v.erase(v.end() - 1);
+//     }
+    
+//     int top() {
+//         return v[v.size()-1];
+//     }
+    
+//     int getMin() {
+//         return *min_element(v.begin(),v.end());
+//     }
+// };
+
+
+
+//  approach--2
+
+
+class MinStack {
+public:
+    stack<int>s1, s2;
+    
+    void push(int x) {
+        if(s1.empty()){
+            s2.push(x);
+        }
+        else if(x <= s2.top()){
+            s2.push(x);
+        }
+        s1.push(x);
     }
     
     void pop() {
-        v.erase(v.end() - 1);
+        if(s1.empty())return;
+        if(s1.top() == s2.top()){
+            s2.pop();
+        }
+        s1.pop();
     }
     
     int top() {
-        return v[v.size()-1];
+        if(s1.empty())return -1;
+        return s1.top();
     }
     
     int getMin() {
-        return *min_element(v.begin(),v.end());
+        if(s1.empty())return -1;
+        return s2.top();
     }
 };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+
+
+
+
