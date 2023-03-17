8fa3a112158997ca8afffa11f14d1ec9a7842507
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-03-17 18:03:59 +0530

    Time: 4 ms (33.90%), Space: 7 MB (39.11%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,28 +1,64 @@
 class MyStack {
 public:
     /** Initialize your data structure here. */
-    vector<int> stack;
-    MyStack() {}
+//     vector<int> stack;
+//     MyStack() {}
     
-    /** Push element x onto stack. */
+//     /** Push element x onto stack. */
+//     void push(int x) {
+//         stack.push_back(x);
+//     }
+    
+//     /** Removes the element on top of the stack and returns that element. */
+//     int pop() {
+//         int ele = stack.back();
+//         stack.pop_back();
+//         return ele;
+//     }
+    
+//     /** Get the top element. */
+//     int top() {
+//         return stack.back();
+//     }
+    
+//     /** Returns whether the stack is empty. */
+//     bool empty() {
+//         return stack.size() == 0;
+//     }
+// };
+    
+    
+    
+    
+    
+    //  2 approach---->
+    
+    queue<int>q1;
+    queue<int>q2;
+    
+        MyStack() {
+      
+    }
+    
     void push(int x) {
-        stack.push_back(x);
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());    q1.pop();
+        }
+        swap(q1, q2);
     }
     
-    /** Removes the element on top of the stack and returns that element. */
     int pop() {
-        int ele = stack.back();
-        stack.pop_back();
-        return ele;
+        int result = top();
+        q1.pop();
+        return result;
     }
     
-    /** Get the top element. */
     int top() {
-        return stack.back();
+        return q1.front();
     }
     
-    /** Returns whether the stack is empty. */
     bool empty() {
-        return stack.size() == 0;
+        return q1.empty();
     }
 };
