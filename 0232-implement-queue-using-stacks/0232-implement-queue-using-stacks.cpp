e482eb69b3d27d499269ac0469a63570629a5291
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-03-17 20:09:33 +0530

    Time: 5 ms (11.16%), Space: 7.1 MB (43.94%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,37 +1,74 @@
 class MyQueue {
 public:
-   stack<int>s1;
-    stack<int>s2;
     
-    MyQueue() {
+    
+    
+//    stack<int>s1;
+//     stack<int>s2;
+    
+//     MyQueue() {
          
-    }
+//     }
     
-    void push(int x) {
-       while(!s1.empty()){
-           s2.push(s1.top());
-           s1.pop();
-       } 
-        s2.push(x);
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
-    }
+//     void push(int x) {
+//        while(!s1.empty()){
+//            s2.push(s1.top());
+//            s1.pop();
+//        } 
+//         s2.push(x);
+//         while(!s2.empty()){
+//             s1.push(s2.top());
+//             s2.pop();
+//         }
+//     }
     
-    int pop() {
-        int cur=s1.top();
-        s1.pop();
-        return cur;
+//     int pop() {
+//         int cur=s1.top();
+//         s1.pop();
+//         return cur;
+        
+//     }
+    
+//     int peek() {
+//         return s1.top();
+//     }
+    
+//     bool empty() {
+//        return s1.empty();  
+//     }
+// };
+
+
+    
+    
+    
+    // approach-2
+    
+    
+    vector<int>vp;
+
+    MyQueue() {
         
     }
     
+    void push(int x) {
+   vp.push_back(x);    
+    }
+    
+    int pop() {
+        int x= vp[0];
+        vp.erase(vp.begin(),vp.begin()+1);
+        
+       return x;
+    }
+    
     int peek() {
-        return s1.top();
+        return vp[0];
     }
     
     bool empty() {
-       return s1.empty();  
+       if(vp.size() == 0)
+           return true;
+        return false;
     }
 };
-
