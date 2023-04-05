599fba6367740e6dde24da1a0e4616a86155b7e0
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-04-05 23:54:33 +0530

    Time: 39 ms (25.09%), Space: 10.7 MB (32.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,60 @@
 class Solution {
 public:
+//     int lengthOfLongestSubstring(string s) {
+//         int arr[256]={0};
+//         int l=0;;
+//         int r=0;
+//         int ans=0;
+        
+        
+        
+//         while(r<s.size()){
+//             arr[s[r]]++;
+//              while(arr[s[r]]>1)    //if the occurence become more than 1 means the char is repeated
+//             { 
+//                 arr[s[l]]--;   //reduce the occurence of temp as it might be present ahead also in the string
+//                 l++;       
+//             }
+            
+//             ans=max(ans,r-l+1);
+//             r++;
+            
+//         }
+        
+        
+        
+        
+//         return ans;
+//     }
+// };
+    
+    
+    
+    
+    
+    
+    
+    
+    
     int lengthOfLongestSubstring(string s) {
-        int arr[256]={0};
-        int l=0;;
+     unordered_set<char>st;
+        int l=0;
         int r=0;
-        int ans=0;
+        int maxi=0;
         
-        
-        
-        while(r<s.size()){
-            arr[s[r]]++;
-             while(arr[s[r]]>1)    //if the occurence become more than 1 means the char is repeated
-            { 
-                arr[s[l]]--;   //reduce the occurence of temp as it might be present ahead also in the string
-                l++;       
+        while(l<s.size() && r<s.size()){
+            if(st.find(s[r])==st.end()){
+                st.insert(s[r]);
+                int n=st.size();
+                maxi=max(maxi,n);
+                r++;
             }
-            
-            ans=max(ans,r-l+1);
-            r++;
-            
+            else{
+                st.erase(s[l]);
+                l++;
+            }
         }
         
-        
-        
-        
-        return ans;
+        return maxi;
     }
 };
