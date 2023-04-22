1025a0511c05c450ff26a3d40999fad1ce824f20
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-04-22 12:14:08 +0530

    Time: 92 ms (76.84%), Space: 59.1 MB (8.07%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-
-    
-      int maxArea(vector<int>& height){
-          int l=0;
-          int h=height.size()-1;
-          int maxi=0;
-          
-          while(l<h){
-              int lh=height[l];
+    int maxArea(vector<int>& height) {
+      
+        int l=0;
+        int h=height.size()-1;
+        int maxi=0;
+        
+        while(l<h){
+            int lh=height[l];
               int rh=height[h];
               
               maxi=max(maxi,min(lh,rh)*(h-l));
@@ -18,9 +17,10 @@ public:
               }
               else{
                   h--;
-              }
-              
-          }
-         return maxi;
-      }
+        }
+    }
+        
+        return maxi;
+        
+    }
 };
