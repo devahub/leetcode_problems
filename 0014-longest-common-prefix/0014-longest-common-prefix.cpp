1020d08cdf1420db383b31b3995422b318ab4a2f
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-04-23 14:59:01 +0530

    Time: 4 ms (71.70%), Space: 9 MB (99.94%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+     string ans="";
+        
+        for(int i=0;i<s[0].size();i++){
+            for(int j=1;j<s.size();j++){
+                if(s[j][i]!=s[0][i]){
+                    return ans;
+                }
+            }
+             ans+=s[0][i];
+        }
+        
+        
+        return ans;
+    }
+};
