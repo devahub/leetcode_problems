593acd9872ce25d29ec3802a4686ed5ad246d926
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-04-25 14:46:14 +0530

    Time: 214 ms (52.06%), Space: 29.8 MB (47.20%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    
-    // using two pointers------>
-    
     vector<vector<int>> threeSum(vector<int>& nums) {
+        
         sort(nums.begin(),nums.end());
         vector<vector<int>>res;
+        
         
         for(int i=0;i<nums.size()-2;i++){
             if(i==0 || i>0 && nums[i]!=nums[i-1])
@@ -13,7 +12,7 @@ public:
             int low=i+1;
             int hi=nums.size()-1;
             int sum=0-nums[i];
-                
+            
             while(low<hi){
                 if(nums[low]+nums[hi]==sum){
                     vector<int>v;
@@ -37,52 +36,10 @@ public:
                     hi--;
                 }
             }
+            }
         }
-    }
         
-    return res;    
+        
+        return res;
     }
 };
-    
-    
-    
-    
-    
-    
-    // using hashmap------>
-    
-//     vector<vector<int>> threeSum(vector<int>& nums) {
-//         sort(nums.begin() , nums.end());     
-//         if(nums.size() < 3){              
-//             return {};
-//         }
-//         if(nums[0] > 0){                    
-//             return {};
-//         }
-        
-//         unordered_map<int , int>mp;
-//         for(int i = 0 ; i < nums.size() ; ++i){    
-//             mp[nums[i]] = i;
-//         }
-//         vector<vector<int>> ans;
-        
-//         for(int i = 0 ; i < nums.size() - 2 ; ++i){     
-//             if(nums[i] > 0){    
-//                 break;
-//                   }
-            
-//             for(int j = i + 1 ; j < nums.size() - 1 ; ++j){     
-//                 int required = -1*(nums[i] + nums[j]);   
-//                 if(mp.count(required) && mp.find(required)->second > j){
-//                     ans.push_back({nums[i] , nums[j] , required});
-//                 }
-//                 j = mp.find(nums[j])->second;
-//             }
-//             i =mp.find(nums[i])->second;     
-//         }
-        
-//         return ans;  
-//     }
-// };
-    
-    
