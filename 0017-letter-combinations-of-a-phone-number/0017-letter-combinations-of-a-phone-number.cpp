64a25c38d2ae7d35955f3e14bf77562b6ad2682e
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-04-27 20:56:46 +0530

    Time: 2 ms (43.45%), Space: 6.5 MB (80.82%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
-   
     
-    // using backtracking
-    
- vector<string>v = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+   vector<string>v = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
     void generate(string digits,string temp,int start,vector<string> &ans)
     {
@@ -35,34 +32,3 @@ vector<string> letterCombinations(string digits) {
         return ans;
     }
 };
-    
-    
-    
-    // using bfs----->iterative----->
-    
-//     vector<string> letterCombinations(string digits) {
-//         vector<string> phone = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-//         if(digits.empty())
-//             return {};
-        
-//         queue<string> q;
-//         vector<string> ans;
-        
-//         q.push("");
-
-//         while(!q.empty()){
-//             string s = q.front();
-//             q.pop();
-            
-//             if(s.length() == digits.length())
-//                 ans.push_back(s);
-//             else{
-//                 string temp = phone[digits[s.length()] - '0'];
-//                 for(auto i : temp)
-//                     q.push(s + i);
-//             }
-//         }
-//         return ans;
-//     }
-// };
-    
