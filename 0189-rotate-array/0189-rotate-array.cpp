e03f3be8c3f9b8c02d426f1b19e18ebfde57cefe
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-05-07 01:32:31 +0530

    Time: 33 ms (55.43%), Space: 24.9 MB (94.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,31 +1,5 @@
-class Solution
-{
-    public:
-
-       	// rotate all elements by 1 positon k times--->TLE 
-
-//         void rotateby1(vector<int> &nums)
-//         {
-//             int n = nums.size();
-//             int temp = nums[n - 1];
-            
-//             for(int i=n-1;i>0;i--){
-//                 nums[i]=nums[i-1];
-//             }
-//             nums[0]=temp;
-//         }
-
-//     void rotate(vector<int> &nums, int k)
-//     {
-//         for (int i = 0; i < k; i++)
-//         {
-//             rotateby1(nums);
-//         }
-//     }
-// };
-
-// 2 nd approach---->
-
+class Solution {
+public:
     void reverse(vector<int>&nums,int i,int j)
     {
         int l=i;
@@ -49,16 +23,3 @@ class Solution
         reverse(nums,0,nums.size()-1);
       }
 };
-
-//      1234567----->partition into two halves such that using k=k%nums.size()
-
-//          reverse 1234   reverse 567
-//             4321765
-//      now reverse this whole   5671234
-
-/// without making reverse function--->
-
-// k = k%nums.size();
-// reverse(nums.begin(), nums.end());
-// reverse(nums.begin(), nums.begin()+k);
-// reverse(nums.begin()+k, nums.end());
