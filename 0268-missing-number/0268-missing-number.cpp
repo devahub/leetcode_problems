72d68c315ce74e8723d7c0b6ad2b712e74307577
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-05-08 13:42:28 +0530

    Time: 64 ms (5.00%), Space: 25.7 MB (5.22%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,40 +1,19 @@
-class Solution
-{
-    public:
-        int missingNumber(vector<int> &nums)
-        {
-           	// approach 1 hashmap
-
-           	//         map<int,int>mp;
-
-           	//         for(auto i:nums){
-           	//             mp[i]++;
-
-           	//             }
-           	//         for(int j=0;;j++){
-           	//             if(mp[j]==0){
-           	//                 return j;
-           	//             }
-           	//         }
-           	//         return -1;
-
-           	// approach-2---->
-
-            int n = nums.size();
-            sort(nums.begin(), nums.end());
-
-            int ans = n;
-
-            for (int i = 0; i < n; i++)
-            {
-
-                if (nums[i] != i)
-                {
-                    ans = i;
-                    break;
-                }
-            }
-
-            return ans;
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        map<int,int>mp;
+        
+        for(auto i:nums){
+            mp[i]++;
         }
+        
+        
+        
+        for(int i=0;;i++){
+            if(mp[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
 };
