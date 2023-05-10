3a50155cfa25a33bde65611a399ade78621a6e09
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-05-11 04:06:15 +0530

    Time: 22 ms (49.80%), Space: 19.6 MB (75.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-//     int majorityElement(vector<int>& nums) {
-//      unordered_map<int,int>mp;
+    
+    
+       int majorityElement(vector<int>& nums) {
+     unordered_map<int,int>mp;
         
-//         for(auto &i:nums){
-//             mp[i]++;
-//         }
-//         int maxi=INT_MIN;
-//         for(auto i: mp){
-//             if(i.second>mp[maxi])
-//                 maxi=i.first;
+        for(auto &i:nums){
+            mp[i]++;
+        }
+        int maxi=INT_MIN;
+        for(auto i: mp){
+            if(i.second>mp[maxi])
+                maxi=i.first;
             
-//         }
-//         return maxi;
+        }
+        return maxi;
 //     }
 // };
     
     // moore's voting algo
     
-    int majorityElement(vector<int>& nums) {
-        int ele=0,count=0,n=nums.size();
+//     int majorityElement(vector<int>& nums) {
+//         int ele=0,count=0,n=nums.size();
         
-        for(int i=0;i<n;i++){
-            if(count==0){
-                ele=nums[i];
-            }
-        if(nums[i]==ele)
-            count++;
-        else count--;
-    } 
-    return ele;
-        
-        
-        
+//         for(int i=0;i<n;i++){
+//             if(count==0){
+//                 ele=nums[i];
+//             }
+//         if(nums[i]==ele)
+//             count++;
+//         else count--;
+//     } 
+//     return ele;
     }
 };
