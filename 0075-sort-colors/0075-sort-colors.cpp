d3ba48ec53ad4e6c9491bf40aa11e5a1fdc10719
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-05-11 04:01:43 +0530

    Time: 6 ms (23.60%), Space: 8.2 MB (90.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,56 +1,28 @@
-class Solution
-{
-    public:
-       // void sortColors(vector<int> &nums)
-       // {
-           	//        int lo=0;
-           	//         int hi=nums.size()-1;
-           	//             int i=0;
-           	//         while(i<=hi){
-           	//         switch(nums[i]){
-           	//             case 0:
-           	//                 swap(nums[lo++],nums[i++]);
-           	//                break;
-
-           	//             case 1:
-           	//                 i++;
-           	//                 break;
-
-           	//             case 2:
-           	//                 swap(nums[hi--],nums[i]);
-           	//                 break;
-           	//         }    
-           	//         }   
-           	//     }
-            //  };
-    
-    
-
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0;
+        int h=nums.size()-1;
+        int i=0;
             
+        while(i<=h){
             
-        void sortColors(vector<int> &nums)
-        { 
-            int low = 0;
-            int high = nums.size() - 1;
-            int mid = 0;
-
-            while (mid <= high)
-            {
-                if (nums[mid] == 0)
-                {
-                    swap(nums[mid], nums[low]);
-                    low++;
-                    mid++;
-                }
-                else if (nums[mid] == 1)
-                {
-                    mid++;
-                }
-                else
-                {
-                    swap(nums[mid], nums[high]);
-                    high--;
-                }
+            if(nums[i]==0){
+                swap(nums[i],nums[l]);
+                l++;
+                i++;
             }
+            
+            else if(nums[i]==1){
+                i++;
+            }
+            else {
+                swap(nums[i],nums[h]);
+                h--;
+            }
+            
         }
+        
+    
+    }
 };
