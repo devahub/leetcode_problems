9f4dbe01a3f62d409fb37db11f4681512ea118ef
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-05-21 03:28:54 +0530

    Time: 107 ms (44.82%), Space: 46.9 MB (30.18%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-
-    // brut force---->
+     // brut force---->
+    
+  // brut force---->
     
 //     int subarraySum(vector<int>& nums, int k)
 //     {
@@ -23,29 +24,23 @@ public:
 // };
     
     
+    
+    
+     int subarraySum(vector<int>& nums, int k) {
+        int c = 0;
+        int currSum = 0;
+        int n = nums.size();
         
-    
-    
-// we are using same prefix sum concept but reducing the time complexityto o(n)
-// you can do it by taking prefix sum array but in o(n2) time
-    
-int subarraySum(vector<int>& nums, int k)
-{
-    int c=0;
-    int sum=0;
-    
-    unordered_map<int,int>mp;
-    for(int i=0;i<nums.size();i++){
-        sum+=nums[i];
+         if(n==0) return 0;
+        unordered_map<int, int> mp;
         
-        if(sum==k){   
-            c++;
+         for(int i=0;i<n;i++){
+            currSum += nums[i];
+            
+            if(currSum == k) c++;
+            if(mp[currSum-k]!=0) c += mp[currSum-k];
+            mp[currSum]++;
         }
-        if(mp.find(sum-k)!=mp.end()){
-            c+=mp[sum-k];
-        }
-        mp[sum]++;
+        return c;
     }
-    return c;
-}
 };
