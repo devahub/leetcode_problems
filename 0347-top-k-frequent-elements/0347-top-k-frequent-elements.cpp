431b2c3169021fda8e2f38f67b491caa589a6f51
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-05-23 01:46:10 +0530

    Time: 15 ms (82.68%), Space: 15.6 MB (7.10%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,22 +1,49 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        priority_queue<pair<int,int>>pq;
-        vector<int>res;
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int,int>mp;
+//         priority_queue<pair<int,int>>pq;
+//         vector<int>res;
         
         
-        for(auto i:nums)mp[i]++;
+//         for(auto i:nums)mp[i]++;
         
-        for(auto [a,b]:mp){
-            pq.push({b,a});
+//         for(auto [a,b]:mp){
+//             pq.push({b,a});
+//         }
+        
+//         while(k!=0){
+//             res.push_back(pq.top().second);
+//             pq.pop();
+//             k--;
+//         }
+//         return res;
+//     }
+// };
+    
+    
+    
+    
+     vector<int> topKFrequent(vector<int>& nums, int k) {
+         vector<int> v;
+        if(nums.empty()) return v;
+        unordered_map<int, int> mp;
+        vector<vector<int>> buckets(nums.size()+1);
+
+         
+        for(auto n: nums) mp[n]++;
+        
+        
+        for(auto& pair: mp) buckets[pair.second].push_back(pair.first);
+        for(int i = nums.size(); i; --i)
+        {
+            for(int j = 0; j < buckets[i].size(); ++j)
+            {
+                v.push_back(buckets[i][j]);
+                if(v.size() == k) return v;
+            }
         }
+        return v;
         
-        while(k!=0){
-            res.push_back(pq.top().second);
-            pq.pop();
-            k--;
-        }
-        return res;
     }
 };
