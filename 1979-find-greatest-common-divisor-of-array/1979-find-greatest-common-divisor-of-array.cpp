6c11c99e7b5fbab62738d0359e8ca99ffcb4e0d2
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-05-30 14:46:00 +0530

    Time: 10 ms (28.32%), Space: 12.4 MB (93.06%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
     
-     int gcdnum(int a,int b)
-        {
-        if(b==0)                // base case
-            return a;
-        return gcdnum(b,a%b);
-         }
+//      int gcdnum(int a,int b)
+//         {
+//         if(b==0)                // base case
+//             return a;
+//         return gcdnum(b,a%b);
+//          }
     
     
-    int findGCD(vector<int>& nums) {
+//     int findGCD(vector<int>& nums) {
+//         int mini=INT_MAX;
+//         int maxi=INT_MIN;
+        
+//        for(auto i:nums){
+//            mini=min(mini,i);
+//            maxi=max(maxi,i);
+//        }
+//         return gcdnum(mini,maxi);
+//     }
+// };
+    
+    
+    
+    
+    
+//  2nd appraoch----->
+    
+    
+    
+      int findGCD(vector<int>& nums) {
         int mini=INT_MAX;
         int maxi=INT_MIN;
         
-       for(auto i:nums){
-           mini=min(mini,i);
-           maxi=max(maxi,i);
-       }
-        return gcdnum(mini,maxi);
+        for(auto i:nums){
+            mini=min(mini,i);
+            maxi=max(maxi,i);
+        }
+        
+        for(int i=mini;i>=1;i--){
+            if(mini%i==0 && maxi%i==0){
+                return i;
+            }
+        }
+        return -1;
     }
 };
