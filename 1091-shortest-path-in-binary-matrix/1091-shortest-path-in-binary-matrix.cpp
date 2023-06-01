66b10468c7287da2760b96b974f23a207b98b1e5
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-06-02 01:32:15 +0530

    Time: 80 ms (53.68%), Space: 19.6 MB (77.00%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,88 +3,32 @@ public:
     
     
     
-    
-//     int dirx[8]={0, 0, 1, -1, 1, 1, -1, -1};
-//     int diry[8]={1, -1, 0, 0, -1, 1, -1, 1};
-        
-//     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-//         int n=grid.size();
-//         if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0)
-// 		{
-// 			return -1;
-// 		}
-     
-        
-//         int size=0;
-//         int steps=0;
-//         queue<pair<int,int>>q;
-//         q.push({0,0});
-        
-//         while(!q.empty()){
-//             steps++;
-//             size=q.size();
-//             while(size--){
-                
-//             int row=q.front().first;
-//             int col=q.front().second;
-//             q.pop();
-                
-//         if(row==n-1 && col==n-1){
-//             return steps;
-//         }
-        
-//         for(int i=0;i<8;i++){
-//             int newx=dirx[i]+row;
-//             int newy=diry[i]+col;
-            
-//         if (newx >= 0 && newx < n && newy >= 0 && newy < n && grid[newx][newy] == 0)
-//         {
-//             grid[newx][newy] = 1;
-// 			   q.push({newx, newy});
-//             }
-//         }  
-//     }
-// }
-//         return -1;
-        
-//     }
-// };
-    
-    
-    
-    
-    
-    
-    
-    // same approach using visited matrix--->
-    
-    
-   bool isValid(int r, int c, int n, vector<vector<int>> &grid, vector<vector<bool>> &visited)
+     bool isValid(int r, int c, int n, vector<vector<int>> &grid, vector<vector<bool>> &visited)
     {
         return (r>=0 && c>=0 && r<n && c<n && grid[r][c]==0 && !visited[r][c]);
     }
     
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) 
-    {
+    
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        queue<pair<int, int>> q; 
+        queue<pair<int,int>>q;
         int ans=0;
+        
         vector<vector<bool>> visited(n, vector<bool> (n, false)); 
         int size;
         
-        if(!grid[0][0]) 
-        {
+        
+        if(!grid[0][0]){
             q.push({0,0});
-            visited[0][0] = true;
+            visited[0][0]=true;
         }
         
-        while(!q.empty())
-        {
-            size = q.size();
+        while(!q.empty()){
+            
+            size=q.size();
             ans++;
             
-            
-            for(int cnt=0; cnt<size; cnt++) 
+           for(int cnt=0; cnt<size; cnt++) 
             {
                 auto node = q.front(); 
                 q.pop();
@@ -108,14 +52,10 @@ public:
                 }
             }
         }
+        
+        
+        
+        
         return -1;
-            
     }
 };
-    
-    
-    
-    
-    
-    
-  
