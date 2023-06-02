cb3dfaefd937092dad897b07fea32cd48f22a9a6
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-06-02 13:35:30 +0530

    Time: 4 ms (41.19%), Space: 6.3 MB (83.19%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1=word1.size();
+        int n2=word2.size();
+        string res="";
+       int d=max(n1,n2);
+        
+        for(int i=0;i<d;i++){
+           if(n1){
+               res+=word1[i];
+               n1--;
+           }
+            if(n2){
+                res+=word2[i];
+                n2--;
+            }
+            
+        }
+        
+        return res;
+    }
+};
