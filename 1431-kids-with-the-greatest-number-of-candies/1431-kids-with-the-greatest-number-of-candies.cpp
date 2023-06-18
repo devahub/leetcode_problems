f9327d03f6a0072e13396a6d51993d5156a6f73b
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-06-19 00:54:09 +0530

    Time: 4 ms (63.80%), Space: 8.8 MB (76.85%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -2,22 +2,45 @@ class Solution {
 public:
     
     
-    vector<bool> kidsWithCandies(vector<int>& candies, int extra) {
-        int maxi=INT_MIN;
-        vector<bool>v;
+//     vector<bool> kidsWithCandies(vector<int>& candies, int extra) {
+//         int maxi=INT_MIN;
+//         vector<bool>v;
         
-        for(auto i:candies){
-            maxi=max(maxi,i);
-        }
+//         for(auto i:candies){
+//             maxi=max(maxi,i);
+//         }
         
-        for(int i=0;i<candies.size();i++){
-            if((candies[i]+extra)>=maxi){
-                v.push_back(true);
+//         for(int i=0;i<candies.size();i++){
+//             if((candies[i]+extra)>=maxi){
+//                 v.push_back(true);
+//             }
+//             else{
+//                 v.push_back(false);
+//             }
+//         }
+//         return v;
+//     }
+// };
+    
+    
+    
+    
+    
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> ans;
+        int n=extraCandies;
+        vector<int>a;
+        a=candies;
+        sort(a.begin(),a.end());
+        int y=candies.size();
+        int x=a[y-1];
+        for(int i=0; i<candies.size(); i++){
+            if(candies[i]+n>=x){
+                ans.push_back(true);
             }
-            else{
-                v.push_back(false);
-            }
+            else
+                ans.push_back(false);
         }
-        return v;
+        return ans;
     }
 };
