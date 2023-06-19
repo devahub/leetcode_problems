ff117a5a2a01b06fd83ffa942b8f1767aeb98404
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-06-19 20:19:36 +0530

    Time: 19 ms (69.04%), Space: 20.4 MB (27.19%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+    
+    
+    
     bool canPlaceFlowers(vector<int>& flower, int n) {
         int c=0;
+        
         if (n == 0) 
             return true;
         if (flower.size() == 0) 
             return false;
         
-        flower.push_back(0);         // append last index as 0
-        flower.insert(flower.begin(),0);   // insert 0 at first index
+            flower.push_back(0);         // append last index as 0
+            flower.insert(flower.begin(),0);   // insert 0 at first index
 
         for(int i=1;i<flower.size()-1;i++){
             if(c<=n && flower[i]==0 && flower[i+1]==0 && flower[i-1]==0){
