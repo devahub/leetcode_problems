43292533a8d9e827a67905d237565d2bd5ba98ea
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-06-19 20:59:37 +0530

    Time: 3 ms (97.17%), Space: 7.8 MB (43.02%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -18,23 +18,62 @@ public:
 
 // vector soln--->
 
-string reverseVowels(string s){
-    vector<int>v;
-    string temp="";
+// string reverseVowels(string s){
+//     vector<int>v;
+//     string temp="";
 
-    for(int i=0;i<s.size();i++){
-        if(s[i] == 'a' || s[i] == 'e'|| s[i] == 'i'|| s[i] == 'o'|| s[i] == 'u'
-             || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+//     for(int i=0;i<s.size();i++){
+//         if(s[i] == 'a' || s[i] == 'e'|| s[i] == 'i'|| s[i] == 'o'|| s[i] == 'u'
+//              || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+//             {
+//                 temp+=s[i];
+//                 v.push_back(i);
+//             }
+//     }
+//     reverse(temp.begin(),temp.end());
+
+//     for(int i=0;i<v.size();i++){
+//         s[v[i]]=temp[i];
+//     }
+// return s;
+// }
+// };
+    
+    
+    
+    
+    
+    
+    
+    // two pointer
+    
+      string reverseVowels(string s) 
+    {
+        int i = 0;
+        int j = s.size()-1;
+        
+        while(i<j)
+        {
+            bool flag1 = false, flag2 = false;   // to check if we are pointing to a vowel or not
+            
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+            flag1 = true;
+            
+            if(s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u' || s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O' || s[j]=='U')
+            flag2 = true;
+            
+            if(flag1 && flag2)   // if both are pointing to vowels just swap them
             {
-                temp+=s[i];
-                v.push_back(i);
+                swap(s[i],s[j]);
+                i++;j--; 
             }
-    }
-    reverse(temp.begin(),temp.end());
-
-    for(int i=0;i<v.size();i++){
-        s[v[i]]=temp[i];
-    }
-return s;
-}
+            
+            if(!flag1)    // if i is not pointing to a vowel, move the pointer forward
+            i++;
+            if(!flag2)    // if j is not pointing to a vowel, move the pointer backwards
+            j--;
+        }
+        
+        return s;
+    } 
 };
