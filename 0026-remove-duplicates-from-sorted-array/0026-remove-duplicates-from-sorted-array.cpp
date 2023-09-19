958848e6f0328a666366c44023e458e6e98eafb5
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-09-20 01:46:05 +0530

    Time: 7 ms (79.34%), Space: 18.8 MB (16.38%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
-   // two pointer---->optimal solution
-    
-    
     int removeDuplicates(vector<int>& nums) {
-        
-        int i=0;
+          int i=0;
         for(int j=1;j<nums.size();j++){
             if(nums[i]!=nums[j]){
                 i++;
@@ -16,21 +12,3 @@ public:
         
     }
 };
-
-
-    // brut solution using hashing---->
-    
-// int removeDuplicates(vector<int>& nums) {
-//     vector<int>ans;
-    
-//     unordered_map<int,int>mp;
-//     for(int i=0;i<nums.size();i++){
-//         if(mp[nums[i]]==0){
-//             ans.push_back(nums[i]);
-//               mp[nums[i]]++;
-//         }
-//      }
-//         nums=ans;
-//         return ans.size();
-//     }
-// };
