5065f50f8b1d19f0614560910c8613130870982c
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-09-20 01:46:54 +0530

    Time: 7 ms (79.34%), Space: 19.7 MB (7.93%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
+//     int removeDuplicates(vector<int>& nums) {
+//           int i=0;
+//         for(int j=1;j<nums.size();j++){
+//             if(nums[i]!=nums[j]){
+//                 i++;
+//                 nums[i]=nums[j];
+//             }
+//         }
+//         return i+1;
+        
+//     }
+// };
+    
+    
+    
+    
     int removeDuplicates(vector<int>& nums) {
-          int i=0;
-        for(int j=1;j<nums.size();j++){
-            if(nums[i]!=nums[j]){
-                i++;
-                nums[i]=nums[j];
-            }
+        
+         vector<int>ans;
+    
+    unordered_map<int,int>mp;
+    for(int i=0;i<nums.size();i++){
+        if(mp[nums[i]]==0){
+            ans.push_back(nums[i]);
+              mp[nums[i]]++;
         }
-        return i+1;
+     }
+        nums=ans;
+        return ans.size();
         
     }
 };
