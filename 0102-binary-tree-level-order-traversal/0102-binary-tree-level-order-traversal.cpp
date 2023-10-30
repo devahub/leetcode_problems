9a3ec47b8156370aae04cf137d0df8fdffb0f447
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-10-30 19:19:04 +0530

    Time: 0 ms (100.00%), Space: 15.3 MB (5.86%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -13,41 +13,62 @@ class Solution {
 public:
     
     
-    vector<vector<int>> levelOrder(TreeNode* root) {
-         if(root==NULL){
-           return {};
-       } 
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//          if(root==NULL){
+//            return {};
+//        } 
         
-        vector<vector<int>>ans;
+//         vector<vector<int>>ans;
         
-        queue<TreeNode*>q;
-        q.push(root);
+//         queue<TreeNode*>q;
+//         q.push(root);
         
-        int len;
-        TreeNode* temp;
+//         int len;
+//         TreeNode* temp;
         
-        while(!q.empty()){
-            len=q.size();
-            vector<int>v;
+//         while(!q.empty()){
+//             len=q.size();
+//             vector<int>v;
             
-            for(int i=0;i<len;i++){
+//             for(int i=0;i<len;i++){
                 
-            temp=q.front();
-            q.pop();
-            v.push_back(temp->val);
+//             temp=q.front();
+//             q.pop();
+//             v.push_back(temp->val);
         
-            if(temp->left)q.push(temp->left);
-            if(temp->right)q.push(temp->right);
+//             if(temp->left)q.push(temp->left);
+//             if(temp->right)q.push(temp->right);
                 
                 
-            }
-            ans.push_back(v);
+//             }
+//             ans.push_back(v);
             
             
             
             
-        }
+//         }
     
-        return ans;
+//         return ans;
+//     }
+// };
+    
+    
+    
+    
+    
+   vector<vector<int>> answer;
+    void DFS_levelorder(TreeNode* root,int level) 
+    {
+        if(!root) return;  
+        if(level==answer.size())  
+            answer.push_back(vector<int> ()); 
+        
+        answer[level].push_back(root->val); 
+        DFS_levelorder(root->left,level+1);  
+        DFS_levelorder(root->right,level+1);  
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        DFS_levelorder(root,0); 
+        return answer;  
     }
 };
