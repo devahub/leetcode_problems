b232367206002eacc1a3961180391dcc9fdbe3ee
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-10-30 22:11:13 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (17.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
-   // int fib(int n) { 
-       // if(n<=1)                                          // brut- recurssion
-       //     return n;
-       //  int ans=fib(n-1)+fib(n-2);
-       //  return ans;
-        
-        
-//     int dp[31]={0};
-//     int fib(int n) { 
-        
-//         if(n <= 1)
-// 		    return n;
-// 	    if(dp[n]!=0)
-// 		return dp[n];
-        
-//         return dp[n]=fib(n-1)+fib(n-2);
-        
-//     }
-// };
-    
-    
-    
-    int fib(int n) {
-        if(n==0)return 0;
-        if(n==1)return 1;
-        return fib(n-1)+fib(n-2);
-        
+   int fib(int n) {
+        if(n==0 || n==1) return n;
+        int prev2=0;
+        int prev=1;
+        for(int i=2;i<=n;i++){
+            int curr_i = prev2+prev;
+            prev2=prev;
+            prev=curr_i;
+        }
+        return prev;
     }
 };
