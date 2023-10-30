55616cf4cd99fe3a68afc54063d76ad02065d236
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-10-30 19:33:51 +0530

    Time: 14 ms (22.40%), Space: 21.4 MB (21.20%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,63 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     
     
-// A balanced binary tree, also referred to as a height-balanced binary tree, is defined as a binary tree in which the height of the left and right subtree of any node differ by not more than 1.
+    bool ans;
     
-    
-    
-    //// appraoch----->1
-    
-//     bool ans;
-    
-//     int solve(TreeNode* root){
-//         if(!root)
-//             return 0;
+    int solve(TreeNode* root){
+        if(!root)
+            return 0;
         
-//         int left=solve(root->left);
-//         int right=solve(root->right);
+        int left=solve(root->left);
+        int right=solve(root->right);
         
-//         if(abs(left-right)>1)
-//             ans=false;
+        if(abs(left-right)>1)
+            ans=false;
         
-//         return max(left,right)+1;
-//     }
-    
-//     bool isBalanced(TreeNode* root) {
-//         ans=true;
-//         solve(root);
-//         return ans;
-//     }
-// };
-    
-    
-    
-    
-    
-    
-    ///// approach-->2
-    
-    int height(TreeNode* root){
-        if(root==NULL)return 0;
-        
-        int lefth=height(root->left);
-        int righth=height(root->right);
-        
-        if(lefth==-1 || righth==-1 || abs(lefth-righth)>1){
-            return -1;
-        }
-        
-        return max(lefth,righth)+1;
+        return max(left,right)+1;
     }
     
-    
-    
-     bool isBalanced(TreeNode* root){
-    
-         if(root==NULL)return true;
-         
-         if(height(root)==-1)return false;
-         
-         return true;
-     }
+    bool isBalanced(TreeNode* root) {
+        ans=true;
+        solve(root);
+        return ans;
+    }
 };
+    
