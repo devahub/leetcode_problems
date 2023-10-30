d2596ac349c46c8878e76d654748fd54ef8d7123
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-10-31 00:33:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (64.57%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -2,37 +2,24 @@ class Solution {
 public:
     
     
-    // brute force--->tle usig recurssion
-    
-//     int numTrees(int n) {
-//         if(n<=1)return 1;
-        
-//         int ans=0;
-//         for(int i=1;i<=n;i++){
-//             ans+=numTrees(i-1)*numTrees(n-i);
-//         }
-//         return ans;
-//     }
-// };
-    
-        int solve(int n,vector<int>&dp) {
+    int solve(int n,vector<int>&dp) {
         if(n<=1)return 1;
         
         if(dp[n]!=-1)return dp[n];
             
         int ans=0;
+        
         for(int i=1;i<=n;i++){
             ans+=solve(i-1,dp)*solve(n-i,dp);
         }
         return dp[n]=ans;
     }
     
-    int numTrees(int n){
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+    
+    
+    
+    int numTrees(int n) {
+     vector<int>dp(n+1,-1);
+     return solve(n,dp);   
     }
-    
-    
 };
-    
-    
