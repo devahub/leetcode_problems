0b0aa5a4a4150ddc5711cb6970becf26edc2a7a0
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-10-30 18:48:22 +0530

    Time: 6 ms (40.04%), Space: 14.2 MB (13.67%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,72 +1,53 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     
-vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root==NULL)
-            return {};
+    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+         if(root==NULL){
+           return {};
+       } 
         
-        vector<vector<int>>res;
+        vector<vector<int>>ans;
+        
         queue<TreeNode*>q;
         q.push(root);
+        
+        int len;
         TreeNode* temp;
         
         while(!q.empty()){
-            
+            len=q.size();
             vector<int>v;
-            int l=q.size();
             
-            for(int i=0;i<l;i++){
-                temp=q.front();
-                q.pop();
-                v.push_back(temp->val);
+            for(int i=0;i<len;i++){
                 
-                if(temp->left!=NULL)
-                    q.push(temp->left);
+            temp=q.front();
+            q.pop();
+            v.push_back(temp->val);
+        
+            if(temp->left)q.push(temp->left);
+            if(temp->right)q.push(temp->right);
                 
-                if(temp->right!=NULL)
-                    q.push(temp->right);
+                
             }
-            res.push_back(v);          
-            }
-        return res;
-         }
+            ans.push_back(v);
+            
+            
+            
+            
+        }
+    
+        return ans;
+    }
 };
-        
-        
-    
-    
-    
-    
-    
-        // recurssive soln
-        
-//     vector<vector<int>> levelOrder(TreeNode* root) {
-//         vector<vector<int>>ans;
-//         if(root==NULL)
-//             return ans;
-//         queue<TreeNode*>q;
-//         q.push(root);
-        
-//         while(!q.empty()){
-//             int size=q.size();
-//             vector<int>v;
-//             for(int i=0;i<size;i++){
-//                 TreeNode *node=q.front();
-//                 q.pop();
-//             if(node->left!=NULL)
-//                 q.push(node->left);
-//             if(node->right!=NULL)
-//                 q.push(node->right); 
-                
-//             v.push_back(node->val);
-//         }
-//             ans.push_back(v);
-//         }
-//         return ans;
-        
-//     }
-// };
-        
-        
-        
