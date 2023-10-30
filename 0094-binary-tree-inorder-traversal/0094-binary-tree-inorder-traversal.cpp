aa6f3c00899a2d1110f82a1f995c635177cce3cf
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-10-30 18:32:47 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (18.11%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,44 +1,59 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     
-    // using recurssion---->
     
-//     void solve(TreeNode* root,vector<int>&v){
-//     if(root==NULL){
-//         return;
-//     }    
-//         solve(root->left,v);
-//         v.push_back(root->val);
-//         solve(root->right,v);
-//     }
+//      void helper(TreeNode* root,vector<int>&ans){
+         
+        
+//          if(root==NULL){
+//              return;
+//          }
+         
+//          helper(root->left,ans);
+//          ans.push_back(root->val);
+//          helper(root->right,ans);
+         
+//      }
+    
     
 //     vector<int> inorderTraversal(TreeNode* root) {
-//         vector<int>v;
-//         solve(root,v);
-//         return v;
+//         vector<int>ans;
+//         helper(root,ans);
+//         return ans;
 //     }
 // };
     
-    // iterative approach--->important 
     
-    vector<int> inorderTraversal(TreeNode* root){
-        stack<TreeNode*>st;
-        vector<int>v;
+    
+    vector<int> v;
+    
+    vector<int> inorderTraversal(TreeNode* root) {
         
-        while(root || !st.empty()){
+        stack<TreeNode*> s;
+        
+        while(root||!s.empty()){
             if(root!=NULL){
-                st.push(root);
+                s.push(root);
                 root=root->left;
             }
             else{
-                root=st.top();
-                st.pop();
+                root=s.top();
+                s.pop();
                 v.push_back(root->val);
                 root=root->right;
             }
-        }
-        
-        return v;
+          }
+         return v;
     }
 };
