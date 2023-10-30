ed0d150702b8ccbad52e4ea555234fd40f874c22
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-10-30 20:08:07 +0530

    Time: 5 ms (17.04%), Space: 10.5 MB (21.59%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,11 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-   bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL)
-            return true;
-        if(p==NULL || q==NULL)
-            return false;
+    
+    
+    
+    bool isSameTree(TreeNode* p, TreeNode* q) {
         
+        if(p==NULL || q==NULL){
+            return p==q;
+        }    
         
         if(p->val!=q->val)
             return false;
@@ -13,36 +26,5 @@ public:
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
         
         
-        
     }
 };
-        
-        // iterative approach--->
-//     bool isSameTree(TreeNode* p, TreeNode* q) {     
-//         queue<TreeNode*>que;
-        
-//         que.push(p);
-//         que.push(q);
-        
-//         while(!que.empty()){
-            
-//             TreeNode* i=que.front();
-//             que.pop();
-            
-//             TreeNode* j=que.front();
-//             que.pop();
-//             if(i==NULL && j==NULL)continue;
-//             if(i==NULL || j==NULL)return false;
-            
-//             if(i->val!=j->val){
-//             return false;                
-//             }
-//             que.push(i->left);
-//               que.push(j->left);
-//               que.push(i->right);
-//               que.push(j->right);
-            
-//         }
-//         return true;
-//     }
-// };
