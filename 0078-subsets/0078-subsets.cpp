ad96abf6b48937f98049b2ee9d236f1b8f318bb1
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-11-02 01:59:31 +0530

    Time: 0 ms (100.00%), Space: 16.4 MB (23.28%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,87 +1,27 @@
 class Solution {
 public:
     
+    vector<vector<int>>ans;
 
-
-//     vector<vector<int>>ans;
-//     void solve(vector<int>& nums,int i,vector<int>temp){
+        void solve(vector<int>& nums,int i,vector<int>temp){
         
-//        if(i==nums.size()){
-//            ans.push_back(temp);
-//            return;
-//        }
+       if(i==nums.size()){
+           ans.push_back(temp);
+           return;
+       }
         
-//         solve(nums,i+1,temp);
-//         temp.push_back(nums[i]);
-//         solve(nums,i+1,temp);   
+        solve(nums,i+1,temp);
+        temp.push_back(nums[i]);
+        solve(nums,i+1,temp);   
         
-//  }
-  
-//    vector<vector<int>> subsets(vector<int>& nums) {
-//         vector<int>temp;
-//         solve(nums,0,temp);
-//         return ans;
-            
-//     }
-// };
+ }    
     
     
     
     
-    
-    
-    
-    
-    
-    
-    // iterative soln--->
-    //use two for loops and complete  iterations for every value of i 
-    
-//       vector<vector<int>> subsets(vector<int>& nums){
-//           int n=nums.size();
-//           vector<vector<int>>ans;
-//           ans.push_back({});
-          
-//           for(int i=0;i<n;i++){
-//               int size=ans.size();
-              
-//               for(int j=0;j<size;j++){
-//                   vector<int>temp=ans[j];      // get the size
-//                   temp.push_back(nums[i]);
-//                   ans.push_back(temp);
-                  
-//               }
-//           }
-          
-//           return ans;
-//       }
-// };
-
-
-    
-    
-    
-    
-    
-    
-    // recurssive solution-----> backtracking solution---->
-    
-    
-    void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int>& v,int index){
-        ans.push_back(v);
-        
-        for(int i=index;i<nums.size();i++){
-            v.push_back(nums[i]);
-            solve(nums,ans,v,i+1);
-            v.pop_back();
-        }
-        
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>temp;
+        solve(nums,0,temp);   
+        return ans;
     }
-    
-  vector<vector<int>> subsets(vector<int>& nums){
-      vector<vector<int>>ans;
-      vector<int>v;
-      solve(nums,ans,v,0);
-      return ans; 
-  }
 };
