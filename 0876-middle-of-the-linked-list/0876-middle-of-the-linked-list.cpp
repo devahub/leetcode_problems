a4e299f9cff2fe46acbca691640fd8078b5eaaff
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-11-02 14:17:11 +0530

    Time: 3 ms (45.84%), Space: 7.4 MB (50.21%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -5,12 +5,15 @@ public:
     
    ListNode* middleNode(ListNode* head)  {
         
-        ListNode  *slow=head,*fast=head;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        
         while(fast!=NULL && fast->next!=NULL)
         {
             slow=slow->next;
             fast=fast->next->next;
         }
-        return slow;
+       
+    return slow;
     }
 };
