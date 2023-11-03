2ef3f87f1dd192539a3d6edb0235ed0bccf5a1b4
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-11-03 11:44:49 +0530

    Time: 43 ms (25.22%), Space: 14.9 MB (41.48%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,31 +1,16 @@
-class Solution
-{
-    public:
-        ListNode* getIntersectionNode(ListNode *heada, ListNode *headb)
-        {
 
-           	//      brut force-------> n2 approach
-
-           	//        ListNode* temp;
-
-           	//         while(heada!=NULL){
-           	//             temp=headb;
-           	//             while(temp!=NULL){
-           	//                 if(heada==temp){
-           	//                     return heada;
-           	//                 }
-           	//                 temp=temp->next;
-           	//             }
-           	//             heada=heada->next;
-           	//         }
-           	//         return NULL;
-
-           	// approach--->2
-           	        ListNode* slow=heada;
-           	        ListNode* fast=headb;
-
-           	       while(fast!=slow){
-           	           if(slow==NULL){
+class Solution {
+public:
+    
+    
+    
+    ListNode *getIntersectionNode(ListNode *heada, ListNode *headb) {
+        ListNode* slow=heada;
+        ListNode* fast=headb;
+        
+        
+        while(fast!=slow){
+                if(slow==NULL){
            	               slow=headb;
            	           }
            	           else{
@@ -37,26 +22,7 @@ class Solution
            	           else{
            	               fast=fast->next;
            	           }
-
-           	       }
-           	        return slow;
-
-           	// using map ---->
-
-            // unordered_map<ListNode*, int> mp;
-            // while (head1 != NULL)
-            // {
-            //     mp[head1]++;
-            //     head1 = head1->next;
-            // }
-            // while (head2 != NULL)
-            // {
-            //     if (mp[head2] > 0)
-            //     {
-            //         return head2;
-            //     }
-            //     head2 = head2->next;
-            // }
-            // return NULL;
         }
+        return slow;
+    }
 };
