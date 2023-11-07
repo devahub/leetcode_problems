b4a8a77e02251857c30f0cda414f5117d5357d3c
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-11-07 12:57:18 +0530

    Time: 0 ms (100.00%), Space: 8 MB (76.40%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,17 +2,54 @@ class Solution {
 public:
     
     
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>>ans;
         
-        sort(nums.begin(),nums.end());
+//         sort(nums.begin(),nums.end());
         
-        do{
-            ans.push_back(nums);
+//         do{
+//             ans.push_back(nums);
+//         }
+//          while(next_permutation(nums.begin(),nums.end()));
+        
+//         return ans;
+        
+//     }
+// };
+    
+    
+    
+    
+    
+    
+    // 2nd approach====>
+    
+    
+    
+    void solve(vector<int> &nums, int index, vector<vector< int>> &ans)
+    {
+         if (index == nums.size())                       //base case
+            {
+                ans.push_back(nums);
+                return;
+            }
+            
+        
+        for(int i=index;i<nums.size();i++){
+            
+                swap(nums[index], nums[i]);
+                solve(nums, index + 1, ans);
+                swap(nums[index], nums[i]);
+            
+            
         }
-         while(next_permutation(nums.begin(),nums.end()));
-        
-        return ans;
         
     }
+    
+     vector<vector<int>> permute(vector<int>& nums){
+         vector<vector<int>>ans;
+         solve(nums,0,ans);
+         return ans;
+         
+     }
 };
