aed0b39e490ce6144b9fd5c60928beca4c5167a8
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-11-07 12:51:37 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (76.40%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,42 +1,18 @@
-class Solution
-{
-    public:
+class Solution {
+public:
     
-       	//    vector<vector < int>> permute(vector<int>& nums) {
-       	//        vector<vector < int>>v;
-       	//         sort(nums.begin(),nums.end());
-
-       	//         do{
-       	//             v.push_back(nums);
-       	//         }
-
-       	//         while(next_permutation(nums.begin(),nums.end()));
-       	//         return v;
-       	//     }
-       	// };
-
-       	// 2nd appproach----->
-
-        void solve(vector<int> &nums, int index, vector<vector< int>> &ans)
-        {
-            if (index == nums.size())                       //base case
-            {
-                ans.push_back(nums);
-                return;
-            }
-
-            for (int i = index; i < nums.size(); i++)
-            {
-                swap(nums[index], nums[i]);
-                solve(nums, index + 1, ans);
-                swap(nums[index], nums[i]);
-            }
-        }
-
-    vector<vector < int>> permute(vector<int> &nums)
-    {
+    
+    vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        solve(nums, 0, ans);
+        
+        sort(nums.begin(),nums.end());
+        
+        do{
+            ans.push_back(nums);
+        }
+         while(next_permutation(nums.begin(),nums.end()));
+        
         return ans;
+        
     }
 };
