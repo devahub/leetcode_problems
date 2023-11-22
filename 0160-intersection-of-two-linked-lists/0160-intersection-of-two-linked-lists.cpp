5a5ab42a820fff29fe260640376acc05e0d82067
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-11-23 04:19:50 +0530

    Time: 38 ms (55.13%), Space: 14.8 MB (81.85%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -4,25 +4,67 @@ public:
     
     
     
-    ListNode *getIntersectionNode(ListNode *heada, ListNode *headb) {
-        ListNode* slow=heada;
-        ListNode* fast=headb;
+//     ListNode *getIntersectionNode(ListNode *heada, ListNode *headb) {
+//         ListNode* slow=heada;
+//         ListNode* fast=headb;
         
         
-        while(fast!=slow){
-                if(slow==NULL){
-           	               slow=headb;
-           	           }
-           	           else{
-           	              slow=slow->next;
-           	           }
-           	            if(fast==NULL){
-           	               fast=heada;
-           	           }
-           	           else{
-           	               fast=fast->next;
-           	           }
-        }
-        return slow;
-    }
+//         while(fast!=slow){
+//                 if(slow==NULL){
+//            	               slow=headb;
+//            	           }
+//            	           else{
+//            	              slow=slow->next;
+//            	           }
+//            	            if(fast==NULL){
+//            	               fast=heada;
+//            	           }
+//            	           else{
+//            	               fast=fast->next;
+//            	           }
+//         }
+//         return slow;
+//     }
+// };
+
+    
+    
+    /// length differn soln
+    
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		int n = 0;
+		int m = 0;
+		ListNode* ptr1 = headA;
+		ListNode* ptr2 = headB;
+		while(ptr1 != NULL){
+			n++;
+			ptr1 = ptr1 -> next;
+		}
+		while(ptr2 != NULL){
+			m++;
+			ptr2 = ptr2 -> next;
+		}
+		int t = abs(n - m);
+		if(n > m){
+			while(t){
+				headA = headA -> next;
+				t--;
+			}
+		}
+		else{
+			while(t){
+				headB = headB -> next;
+				t--;
+			}
+		}
+		while(headA != NULL and headB != NULL){
+			if(headA == headB){
+				return headA;
+			}
+			headA = headA -> next;
+			headB = headB -> next;
+		}
+		return NULL;
+	}
 };
+
