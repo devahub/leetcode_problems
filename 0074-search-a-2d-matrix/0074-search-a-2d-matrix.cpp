b48ac2aea7648ad2fb3c09e6c1fd8e2920bf2b96
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-12-07 22:36:23 +0530

    Time: 3 ms (71.53%), Space: 9.9 MB (53.10%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -24,30 +24,30 @@ class Solution
 
        	// binary search technique--------->do binary search in each row
 
-       	//         bool searchMatrix(vector<vector < int>> &mat, int target)
-       	//         {
+       	        bool searchMatrix(vector<vector < int>> &mat, int target)
+       	        {
 
-       	//             int m = mat.size();
-       	//             int n = mat[0].size();
+       	            int m = mat.size();
+       	            int n = mat[0].size();
 
-       	//             for (int row = 0; row < m; row++)
-       	//             {
-       	//                 int i = 0;
-       	//                 int j = n - 1;
+       	            for (int row = 0; row < m; row++)
+       	            {
+       	                int i = 0;
+       	                int j = n - 1;
 
-       	//                 while (i <= j)
-       	//                 {
-       	//                     int mid = i + (j - i) / 2;
-       	//                     if (mat[row][mid] == target) 
-        //                             return true;
-       	//                     else if (mat[row][mid] > target)
-        //                             j = mid - 1;
-       	//                     else i = mid + 1;
-       	//                 }
-       	//             }
-       	//             return false;
-       	//         }
-       	// };
+       	                while (i <= j)
+       	                {
+       	                    int mid = i + (j - i) / 2;
+       	                    if (mat[row][mid] == target) 
+                                    return true;
+       	                    else if (mat[row][mid] > target)
+                                    j = mid - 1;
+       	                    else i = mid + 1;
+       	                }
+       	            }
+       	            return false;
+       	        }
+       	};
 
     
     
@@ -65,29 +65,29 @@ class Solution
     
     
     
-        bool searchMatrix(vector<vector < int>> &mat, int target)
-        {
-            int m = mat[0].size();
-            int n = mat.size();
-            int j = m - 1;
-            int i = 0;
+//         bool searchMatrix(vector<vector < int>> &mat, int target)
+//         {
+//             int m = mat[0].size();
+//             int n = mat.size();
+//             int j = m - 1;
+//             int i = 0;
 
-            while (i < n && j >= 0)
-            {
-                if (mat[i][j] == target)
-                {
-                    return true;
-                }
-                else if (mat[i][j] > target)
-                {
-                    j--;
-                }
-                else
-                {
-                    i++;
-                }
-            }
+//             while (i < n && j >= 0)
+//             {
+//                 if (mat[i][j] == target)
+//                 {
+//                     return true;
+//                 }
+//                 else if (mat[i][j] > target)
+//                 {
+//                     j--;
+//                 }
+//                 else
+//                 {
+//                     i++;
+//                 }
+//             }
 
-            return false;
-        }
-};
+//             return false;
+//         }
+// };
