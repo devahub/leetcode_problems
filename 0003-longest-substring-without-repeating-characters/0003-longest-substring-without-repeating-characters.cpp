04feb0c2b0406dd332126119f8a9ae711f40eeb1
devansh sachan <65270644+devahub@users.noreply.github.com>
2023-12-08 00:31:48 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (89.10%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,60 +1,60 @@
 class Solution {
 public:
-//     int lengthOfLongestSubstring(string s) {
-//         int arr[256]={0};
-//         int l=0;;
-//         int r=0;
-//         int ans=0;
-        
-        
-        
-//         while(r<s.size()){
-//             arr[s[r]]++;
-//              while(arr[s[r]]>1)    //if the occurence become more than 1 means the char is repeated
-//             { 
-//                 arr[s[l]]--;   //reduce the occurence of temp as it might be present ahead also in the string
-//                 l++;       
-//             }
-            
-//             ans=max(ans,r-l+1);
-//             r++;
-            
-//         }
-        
-        
-        
-        
-//         return ans;
-//     }
-// };
-    
-    
-    
-    
-    
-    
-    
-    
-    
     int lengthOfLongestSubstring(string s) {
-     unordered_set<char>st;
-        int l=0;
+        int arr[256]={0};
+        int l=0;;
         int r=0;
-        int maxi=0;
+        int ans=0;
         
-        while(l<s.size() && r<s.size()){
-            if(st.find(s[r])==st.end()){
-                st.insert(s[r]);
-                int n=st.size();
-                maxi=max(maxi,n);
-                r++;
+        
+        
+        while(r<s.size()){
+            arr[s[r]]++;
+             while(arr[s[r]]>1)   //if the occurence become more than 1 means the char is repeated
+            { 
+                arr[s[l]]--;   //reduce the occurence of temp as it might be present ahead also in the string
+                l++;       
             }
-            else{
-                st.erase(s[l]);
-                l++;
-            }
+            
+            ans=max(ans,r-l+1);
+            r++;
+            
         }
         
-        return maxi;
+        
+        
+        
+        return ans;
     }
 };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     int lengthOfLongestSubstring(string s) {
+//      unordered_set<char>st;
+//         int l=0;
+//         int r=0;
+//         int maxi=0;
+        
+//         while(l<s.size() && r<s.size()){
+//             if(st.find(s[r])==st.end()){
+//                 st.insert(s[r]);
+//                 int n=st.size();
+//                 maxi=max(maxi,n);
+//                 r++;
+//             }
+//             else{
+//                 st.erase(s[l]);
+//                 l++;
+//             }
+//         }
+        
+//         return maxi;
+//     }
+// };
