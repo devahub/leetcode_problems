9614fbdac8b31d84da874ac1df9803dc9a7f062b
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-02-15 02:28:08 +0530

    Time: 6 ms (80.53%), Space: 21.2 MB (6.57%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-//     int removeDuplicates(vector<int>& nums) {
-//           int i=0;
-//         for(int j=1;j<nums.size();j++){
-//             if(nums[i]!=nums[j]){
-//                 i++;
-//                 nums[i]=nums[j];
-//             }
-//         }
-//         return i+1;
-        
-//     }
-// };
-    
-    
-    
+ 
     
     int removeDuplicates(vector<int>& nums) {
-        
-         vector<int>ans;
-    
-    unordered_map<int,int>mp;
-    for(int i=0;i<nums.size();i++){
-        if(mp[nums[i]]==0){
-            ans.push_back(nums[i]);
-              mp[nums[i]]++;
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
         }
-     }
-        nums=ans;
-        return ans.size();
+        return i+1;
         
     }
 };
+    
+    
+   
+    
+//     int removeDuplicates(vector<int>& nums) {    
+    
+//     vector<int>ans;
+//     unordered_map<int,int>mp;
+//     for(int i=0;i<nums.size();i++){
+//         if(mp[nums[i]]==0){
+//             ans.push_back(nums[i]);
+//               mp[nums[i]]++;
+//         }
+//      }
+//         nums=ans;
+//         return ans.size();
+        
+//     }
+// };
