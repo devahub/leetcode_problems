0efe3c5b5a0bfa171a2ab7d2d19c3e8dcbdea26e
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-02-20 01:36:08 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (19.56%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     
     
-//     bool isPowerOfTwo(int n) {
-//         if(n==1)return true;
-//         if(n<1)return false;
+    bool isPowerOfTwo(int n) {
+        if(n==1)return true;
+        if(n<1)return false;
         
-//         if(n%2!=0)
-//             return false;
+        if(n%2!=0)
+            return false;
         
-//         return isPowerOfTwo(n/2);
-//     }
-// };
-    
-    
-    
-    
-     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        while(n%2==0) n/=2;
-        return n==1;
+        return isPowerOfTwo(n/2);
     }
 };
+    
+    
+    
+    
+//      bool isPowerOfTwo(int n) {
+//         if(n==0) return false;
+//         while(n%2==0) n/=2;
+//         return n==1;
+//     }
+// };
