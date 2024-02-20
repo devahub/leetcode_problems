88ee843e7be5182c1f0dfa45e6e37c6b5e0b9721
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-02-21 00:33:00 +0530

    Time: 104 ms (43.74%), Space: 64.7 MB (31.66%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,18 +2,36 @@ class Solution {
 public:
     
     
-    int findDuplicate(vector<int>& nums) {
-                    unordered_map<int, int> mp;
-       	            for (auto i: nums) mp[i]++;
+//     int findDuplicate(vector<int>& nums) {
+//                     unordered_map<int, int> mp;
+//        	            for (auto i: nums) mp[i]++;
 
-       	            for (auto i: mp)
-       	            {
-       	                if (i.second > 1)
-       	                {
-       	                    return i.first;
-       	                }
-       	            }
-       	            return -1;
+//        	            for (auto i: mp)
+//        	            {
+//        	                if (i.second > 1)
+//        	                {
+//        	                    return i.first;
+//        	                }
+//        	            }
+//        	            return -1;
         
+//     }
+// };
+    
+    
+       // Using Sorting
+    // TC: O(nlogn)
+   int findDuplicate(vector<int>& nums)  {
+        // sort the numbers and find the number which has duplicate
+        // by checking the one which has the same at next position
+        sort(nums.begin(), nums.end());
+        
+        int duplicate = 0;
+        for(int i = 0; i < nums.size() - 1; i++)
+            if(nums[i] == nums[i+1]) {
+                duplicate = nums[i];
+                break;
+            }
+        return duplicate;
     }
 };
