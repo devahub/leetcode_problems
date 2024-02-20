ae3f974288b43d49892f43d5f85ccd9db442dfa7
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-02-21 01:10:55 +0530

    Time: 11 ms (93.48%), Space: 26.4 MB (43.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,27 +2,49 @@ class Solution {
 public:
     
     
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ans(n);
-        vector<int>left(n);
-        vector<int>right(n);
+    
+    
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>ans(n);
+//         vector<int>left(n);
+//         vector<int>right(n);
         
-          left[0] = 1;
-        for(int i = 1; i < nums.size(); i++){
-            left[i] = left[i - 1] * nums[i - 1];
-        }
+//           left[0] = 1;
+//         for(int i = 1; i < nums.size(); i++){
+//             left[i] = left[i - 1] * nums[i - 1];
+//         }
         
-        right[n - 1] = 1;
-        for (int j = n - 2; j >= 0; j--) {
-            right[j] = right[j + 1] * nums[j + 1];
-        }
+//         right[n - 1] = 1;
+//         for (int j = n - 2; j >= 0; j--) {
+//             right[j] = right[j + 1] * nums[j + 1];
+//         }
         
-        for (int k = 0; k < n; k++) {
-            ans[k]= left[k] * right[k];
-        }
+//         for (int k = 0; k < n; k++) {
+//             ans[k]= left[k] * right[k];
+//         }
        
-        return ans;
+//         return ans;
+//     }
+// };
+
+    
+   ///  using dp  
+    
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> output(n);
+        output[0] = 1;
+        for(int i=1; i<n; i++){
+            output[i] = output[i-1] * nums[i-1];
+        }
+        
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
     }
 };
 
