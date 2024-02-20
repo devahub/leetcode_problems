a1177fea49949f2075f4011c6fef92295161af90
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-02-20 23:58:31 +0530

    Time: 49 ms (5.09%), Space: 27.9 MB (5.76%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,45 +3,45 @@ public:
     
     // using hashmap----->
     
-//     int missingNumber(vector<int>& nums) {
-//         map<int,int>mp;
+    int missingNumber(vector<int>& nums) {
+        map<int,int>mp;
         
-//         for(auto i:nums){
-//             mp[i]++;
-//         }
+        for(auto i:nums){
+            mp[i]++;
+        }
         
         
         
-//         for(int i=0;;i++){
-//             if(mp[i]==0){
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-// };
-    
-    
-    
-    
-    
-     int missingNumber(vector<int>& nums){
-               
-           int n = nums.size();
-            sort(nums.begin(), nums.end());
-
-            int ans = n;
-
-            for (int i = 0; i < n; i++)
-            {
-
-                if (nums[i] != i)
-                {
-                    ans = i;
-                    break;
-                }
+        for(int i=0;;i++){
+            if(mp[i]==0){
+                return i;
             }
-
-            return ans;
-     }
+        }
+        return -1;
+    }
 };
+    
+    
+    
+    
+    
+//      int missingNumber(vector<int>& nums){
+               
+//            int n = nums.size();
+//             sort(nums.begin(), nums.end());
+
+//             int ans = n;
+
+//             for (int i = 0; i < n; i++)
+//             {
+
+//                 if (nums[i] != i)
+//                 {
+//                     ans = i;
+//                     break;
+//                 }
+//             }
+
+//             return ans;
+//      }
+// };
