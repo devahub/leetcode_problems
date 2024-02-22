c66e293268aa851051101376b380ef446cd1d961
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-02-23 00:42:30 +0530

    Time: 121 ms (84.95%), Space: 65.7 MB (50.28%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,53 +1,23 @@
-class Solution
-{
-    public:
-       	///        the judge will have indigree 'n-1' and outdigree '0'...thats it!!
-
-       	//         int findJudge(int n, vector<vector < int>> &trust)
-       	//         {
-       	//             vector<int> in (n + 1, 0);
-       	//             vector<int> out(n + 1, 0);
-
-       	//             for (auto &i: trust)
-       	//             {
-       	//                 in[i[1]]++;
-       	//                 out[i[0]]++;
-       	//             }
-
-       	//             for (int i = 1; i <= n; i++)
-       	//             {
-       	//                 if (out[i] == 0 && in[i] == n - 1)
-       	//                 {
-       	//                     return i;
-       	//                 }
-       	//             }
-       	//             return -1;
-       	//         }
-       	// };
-
-       	// using map----->
-
-        int findJudge(int n, vector<vector < int>> &trust)
-        {
-            if (n == 1) return 1;
-            map<int, int> m;
-            map<int, int> x;
-            
-            for (int i = 0; i < trust.size(); i++)
-            {
-                m[trust[i][0]]++;
-            }
-            
-            for (int i = 0; i < trust.size(); i++)
-            {
-                x[trust[i][1]]++;
-            }
-            
-            for (int i = 0; i <= n; i++)
-            {
-                if (m[i] == 0 && x[i] == n - 1)
-                    return i;
-            }
-            return -1;
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if (n == 1) return 1;
+        unordered_map<int,int>mp;
+        unordered_map<int,int>np;
+        
+        for(int i=0;i<trust.size();i++){
+            mp[trust[i][0]]++;
         }
+        for(int i=0;i<trust.size();i++){
+            np[trust[i][1]]++;
+        }
+        
+        for(int i=0;i<=n;i++)
+        {
+            if(mp[i]==0 && np[i]==n-1){
+             return i;   
+            }
+        }        
+        return -1;
+    }
 };
