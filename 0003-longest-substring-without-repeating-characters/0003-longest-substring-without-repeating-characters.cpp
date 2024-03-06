b568f2ef3da2b0ede197968abadef4a30e165e1a
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-03-07 02:25:49 +0530

    Time: 5 ms (87.58%), Space: 9.9 MB (75.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    
     int lengthOfLongestSubstring(string s) {
         int arr[256]={0};
         int l=0;;
@@ -9,8 +11,10 @@ public:
         
         
         while(r<s.size()){
+            
             arr[s[r]]++;
-             while(arr[s[r]]>1)   //if the occurence become more than 1 means the char is repeated
+            
+            while(arr[s[r]]>1)   //if the occurence become more than 1 means the char is repeated
             { 
                 arr[s[l]]--;   //reduce the occurence of temp as it might be present ahead also in the string
                 l++;       
