3bb95480dbe7eaa746ba19668bb3b3e2a36ebd94
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-03-12 12:17:21 +0530

    Time: 0 ms (100.00%), Space: 8 MB (45.64%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -4,31 +4,57 @@ public:
     ////      
     
     
-    string customSortString(string order, string s) {
-        unordered_map<char,int>mp;
-        string res="";
+//     string customSortString(string order, string s) {
+//         unordered_map<char,int>mp;
+//         string res="";
         
-        for(auto i:s){
+//         for(auto i:s){
+//             mp[i]++;
+//         }
+        
+        
+        
+//        for(auto i:order){
+//            if(mp.find(i)!=mp.end()){
+//                 res.append(mp[i], i);
+//                mp.erase(i);
+//            }
+           
+           
+           
+//        }
+//          for(auto &it : mp){
+//             res.append(it.second,it.first);
+//         }
+        
+        
+//         return res;
+        
+//     }
+// };
+    
+    
+    
+    
+    
+    
+// same method====>
+    
+    
+    string customSortString(string order, string str) 
+    {
+        unordered_map<char,int> mp;
+        for(auto i:str)
             mp[i]++;
+        string res;
+        for(auto i:order)
+        {
+            res+=string(mp[i],i);
+            mp[i]=0;
         }
-        
-        
-        
-       for(auto i:order){
-           if(mp.find(i)!=mp.end()){
-                res.append(mp[i], i);
-               mp.erase(i);
-           }
-           
-           
-           
-       }
-         for(auto &it : mp){
-            res.append(it.second,it.first);
+        for (auto i : mp) {
+            res += string(i.second, i.first);
         }
-        
-        
         return res;
-        
     }
 };
