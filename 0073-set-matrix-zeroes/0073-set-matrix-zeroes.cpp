3990da6d49a7d9934f3915faa7f1181dae3ade59
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-04-11 01:06:25 +0530

    Time: 7 ms (83.65%), Space: 16.3 MB (30.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -36,35 +36,74 @@ public:
     
    // without space=====>
     
-      void setZeroes(vector<vector<int>>& matrix) {
-          int colm=1,rows=matrix.size(),col=matrix[0].size();
-        for(int i=0;i<rows;i++)
-        {
-        if(matrix[i][0]==0) colm=0;
-          for(int j=1;j<col;j++)
-         {
-            if(matrix[i][j]==0)
-                matrix[i][0]=matrix[0][j]=0;
+//       void setZeroes(vector<vector<int>>& matrix) {
+//           int colm=1,rows=matrix.size(),col=matrix[0].size();
+          
+          
+//         for(int i=0;i<rows;i++)
+//         {
+//         if(matrix[i][0]==0) colm=0;
+//           for(int j=1;j<col;j++)
+//          {
+//             if(matrix[i][j]==0)
+//                 matrix[i][0]=matrix[0][j]=0;
             
-         }
-        }
-        for(int i=rows-1;i>=0;i--)
-        {
-            for(int j=col-1;j>=1;j--)
-            {
-                if(matrix[i][0]==0 || matrix[0][j]==0)
-                    matrix[i][j]=0;
+//          }
+//         }
+//         for(int i=rows-1;i>=0;i--)
+//         {
+//             for(int j=col-1;j>=1;j--)
+//             {
+//                 if(matrix[i][0]==0 || matrix[0][j]==0)
+//                     matrix[i][j]=0;
                 
                 
-            }
-            if(colm==0) matrix[i][0]=0;
+//             }
+//             if(colm==0) matrix[i][0]=0;
             
         
-        }
+//         }
           
           
           
           
-      }
-};
+//       }
+// };
     
+    
+    
+    
+    
+    void setZeroes(vector<vector<int>>& mat){
+        
+        int m=mat.size();
+        int n=mat[0].size();
+        
+        
+        vector<int>r(m,-1);
+        vector<int>c(n,-1);
+        
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    r[i]=0;
+                    c[j]=0;
+                }
+                
+            }
+            
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(r[i]==0 || c[j]==0){
+                    mat[i][j]=0;
+                }
+                
+            }
+            
+        }
+   
+    }
+};
