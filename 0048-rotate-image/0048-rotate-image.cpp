5425cb4827228bd632f4d58667f273c3034d73e4
devansh sachan <65270644+devahub@users.noreply.github.com>
2024-04-29 23:23:34 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (61.11%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     
-    /// swap and reverse
+    
+    
     
     void rotate(vector<vector<int>>& matrix) {
-        
-        for(int i=0;i<matrix.size();i++){
-            for(int j=i+1;j<matrix.size();j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
-        }
-        
-        
+           for(int i=0;i<matrix.size();i++){
+               for(int j=i+1;j<matrix.size();j++){
+                     swap(matrix[i][j],matrix[j][i]);                          
+               }
+           } 
         
         for(int i=0;i<matrix.size();i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
+        
+        
         
     }
 };
